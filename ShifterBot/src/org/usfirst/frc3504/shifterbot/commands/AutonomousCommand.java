7be1f81d851623b156e6dd9975e2cccfd8df3d51
Alexa <Alexa@128.237.175.26>
2014-11-18 19:12:59 -0500

    Changed things

--- ShifterBot/src/org/usfirst/frc3504/shifterbot/commands/AutonomousCommand.java
+++ ShifterBot/src/org/usfirst/frc3504/shifterbot/commands/AutonomousCommand.java
@@ -26,7 +26,7 @@ public class  AutonomousCommand extends Command {
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
-	}
+	
 
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
