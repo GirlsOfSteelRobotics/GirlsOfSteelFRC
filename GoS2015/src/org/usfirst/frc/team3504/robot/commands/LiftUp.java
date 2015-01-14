22641144069fd4687c7b91b3ed38795d3bb266f2
arushibandi <bandia.gos@gmail.com>
2015-01-13 19:23:28 -0500

    added to index lift up & down, drive left & right

---
+++ GoS2015/src/org/usfirst/frc/team3504/robot/commands/LiftUp.java
@@ -0,0 +1,46 @@
+package org.usfirst.frc.team3504.robot.commands;
+
+import org.usfirst.frc.team3504.robot.Robot;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class LiftUp extends Command {
+	Timer upTime = new Timer();
+	public LiftUp() {
+		requires(Robot.forklift);
+		
+	}
+
+	@Override
+	protected void initialize() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+		upTime.delay(2);
+		Robot.forklift.up(0.5);
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void end() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+		end();
+	}
+	
+}
