a048b059f5f910eb942cbdbbd5fcd7a065c2d623
Corinne <chswim17@gmail.com>
2015-01-17 14:45:11 -0500

    added comments

--- GoS2015/src/org/usfirst/frc/team3504/robot/commands/autonomous/AutoDriveForward.java
+++ GoS2015/src/org/usfirst/frc/team3504/robot/commands/autonomous/AutoDriveForward.java
@@ -29,7 +29,10 @@ public class AutoDriveForward extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-    	if (Robot.chassis.getFrontLeftEncoderDistance() == 53.7)
+    	if (Robot.chassis.getFrontLeftEncoderDistance() == 53.7) /**Tote/Container Distances:
+																	Tote+Container(in): 26.9+18=44.9
+																	Tote(in): 26.9
+																	Distance Between w/o Container(in): 18+35.75=53.75 */
     		return true;
     	else 
     		return false;
