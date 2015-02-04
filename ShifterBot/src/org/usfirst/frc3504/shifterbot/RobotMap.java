be5a8d7de42cf99717a0a1568eba3279ae873ccf
Joseph Jackson <joseph.l.jackson@gmail.com>
2015-02-03 20:32:08 -0500

    Commented out the CANTalon PID calls for now
    Robot wasn't driving at all with them in place. ShifterBot is still not
    driving correctly in teleop now. (Forward joystick moves right wheels.
    Right joystick moves left wheels.)

--- ShifterBot/src/org/usfirst/frc3504/shifterbot/RobotMap.java
+++ ShifterBot/src/org/usfirst/frc3504/shifterbot/RobotMap.java
@@ -44,32 +44,32 @@ public class RobotMap {
 		driveSystemDriveLeft0 = new CANTalon(0);
 		// LiveWindow.addActuator("Drive System", "Drive Left0",
 		// driveSystemDriveLeft0);
-		driveSystemDriveLeft0.setPID(.0001, .0000001, .0000001);
+		//driveSystemDriveLeft0.setPID(.0001, .0000001, .0000001);
 
 		driveSystemDriveRight0 = new CANTalon(3);
 		// LiveWindow.addActuator("Drive System", "Drive Right0",
 		// driveSystemDriveRight0);
-		driveSystemDriveRight0.setPID(.0001, .0000001, .0000001);
+		//driveSystemDriveRight0.setPID(.0001, .0000001, .0000001);
 		
 		driveSystemDriveLeft1 = new CANTalon(1);
 		// LiveWindow.addActuator("Drive System", "Drive Left1",
 		// driveSystemDriveLeft1);
-		driveSystemDriveLeft1.setPID(.0001, .0000001, .0000001);
+		//driveSystemDriveLeft1.setPID(.0001, .0000001, .0000001);
 		
 		driveSystemDriveRight1 = new CANTalon(4);
 		// LiveWindow.addActuator("Drive System", "Drive Right1",
 		// driveSystemDriveRight1);
-		driveSystemDriveRight1.setPID(.0001, .0000001, .0000001);
+		//driveSystemDriveRight1.setPID(.0001, .0000001, .0000001);
 		
 		driveSystemDriveLeft2 = new CANTalon(2);
 		// LiveWindow.addActuator("Drive System", "Drive Left2",
 		// driveSystemDriveLeft2);
-		driveSystemDriveLeft2.setPID(.0001, .0000001, .0000001);
+		//driveSystemDriveLeft2.setPID(.0001, .0000001, .0000001);
 
 		driveSystemDriveRight2 = new CANTalon(5);
 		// LiveWindow.addActuator("Drive System", "Drive Right2",
 		// driveSystemDriveRight2);
-		driveSystemDriveRight2.setPID(.0001, .0000001, .0000001);
+		//driveSystemDriveRight2.setPID(.0001, .0000001, .0000001);
 
 		// Follower: The m_motor will run at the same throttle as the specified
 		// other talon.
