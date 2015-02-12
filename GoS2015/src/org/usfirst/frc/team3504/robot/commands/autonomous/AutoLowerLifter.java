e36e7d76def265c7efea5c1c337db5f7e42464cd
Sarah <smes3007@gmail.com>
2015-02-12 18:09:21 -0500

    added AutoLowerLifter

---
+++ GoS2015/src/org/usfirst/frc/team3504/robot/commands/autonomous/AutoLowerLifter.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team3504.robot.commands.autonomous;
+
+import org.usfirst.frc.team3504.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class AutoLowerLifter extends Command {
+
+    public AutoLowerLifter() {
+    	requires(Robot.forklift);
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	setTimeout(8);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.forklift.down(.5);
+    	//TODO speed
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return isTimedOut();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.forklift.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
