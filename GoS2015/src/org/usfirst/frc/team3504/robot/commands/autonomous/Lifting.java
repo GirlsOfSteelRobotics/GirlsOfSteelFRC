d3728d57a3d9803a21ce3372e419781b7f3c6524
soniaappasamy <appasamysm@gmail.com>
2015-02-14 12:33:43 -0500

    Fixed lifting commandgroup

--- GoS2015/src/org/usfirst/frc/team3504/robot/commands/autonomous/Lifting.java
+++ GoS2015/src/org/usfirst/frc/team3504/robot/commands/autonomous/Lifting.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team3504.robot.commands.autonomous;
 
 import org.usfirst.frc.team3504.robot.commands.collector.AngleCollectorIn;
+import org.usfirst.frc.team3504.robot.commands.collector.AngleCollectorOut;
 import org.usfirst.frc.team3504.robot.commands.doors.DoorsIn;
 import org.usfirst.frc.team3504.robot.commands.doors.DoorsOut;
 import org.usfirst.frc.team3504.robot.commands.fingers.FingerUp;
@@ -14,11 +15,13 @@ public class Lifting extends CommandGroup {
     
     public  Lifting() {
     	
-    	addSequential(new AngleCollectorIn());
+    	//addSequential(new AngleCollectorIn());
     	
-    	addSequential(new AutoCollector());
+    	//addSequential(new AutoCollector());
     	
     	addSequential(new DoorsOut());
+    	
+    	addSequential(new AngleCollectorOut());
     	
     	addSequential(new AutoBringInGrippers());
     	
