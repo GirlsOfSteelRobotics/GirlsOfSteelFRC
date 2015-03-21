440ee6701d10d204812551c6bbfb6512cd1d50fa
chswim17 <chswim17@gmail.com>
2015-03-21 11:19:22 -0400

    Removed AutoBringInGrippers and AutoReleaseGripper and fixed errors in Lifting

--- GoS2015/src/org/usfirst/frc/team3504/robot/commands/autonomous/Lifting.java
+++ GoS2015/src/org/usfirst/frc/team3504/robot/commands/autonomous/Lifting.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team3504.robot.commands.autonomous;
 import org.usfirst.frc.team3504.robot.commands.collector.AngleCollectorIn;
 import org.usfirst.frc.team3504.robot.commands.collector.AngleCollectorOut;
 import org.usfirst.frc.team3504.robot.commands.shack.ShackIn;
+import org.usfirst.frc.team3504.robot.commands.shack.ShackOut;
 import org.usfirst.frc.team3504.robot.subsystems.Lifter;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -15,14 +16,13 @@ public class Lifting extends CommandGroup {
 
 	public Lifting() {
 
-		addSequential(new AutoReleaseGripper());
+		addSequential(new ShackOut());
 		addSequential(new AngleCollectorIn());
 		addSequential(new AutoCollector());
 		addSequential(new ShackIn());
 		addSequential(new AngleCollectorOut());
 		addSequential(new AutoLift(Lifter.DISTANCE_ONE_TOTE));
-		// addSequential(new AutoReleaseGripper());
-
+		// addSequential(new ShackOut());
 		addSequential(new AutoLift(Lifter.DISTANCE_ZERO_TOTES));
 
 	}
