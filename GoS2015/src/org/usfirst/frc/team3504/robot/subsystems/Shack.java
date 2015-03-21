c23bacc4666f295b5b8617ce61cfde44654bd294
Alexa <selwooda.gos@gmail.com>
2015-03-21 11:11:40 -0400

    changed shack in

--- GoS2015/src/org/usfirst/frc/team3504/robot/subsystems/Shack.java
+++ GoS2015/src/org/usfirst/frc/team3504/robot/subsystems/Shack.java
@@ -6,29 +6,22 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Shack extends Subsystem {
 
-	private DoubleSolenoid leftShack;
-
-	// private DoubleSolenoid rightShack;
-
+	private DoubleSolenoid shack;
+	
 	public Shack() {
-		leftShack = new DoubleSolenoid(RobotMap.LEFT_SHACK_MODULE, RobotMap.LEFT_SHACK_CHANNEL_A, RobotMap.LEFT_SHACK_CHANNEL_B);
-		// rightShack = new DoubleSolenoid(RobotMap.RIGHT_SHACK_MODULE,
-		// RobotMap.RIGHT_SHACK_CHANNEL_A, RobotMap.RIGHT_SHACK_CHANNEL_B);
+		shack = new DoubleSolenoid(RobotMap.LEFT_SHACK_MODULE, RobotMap.LEFT_SHACK_CHANNEL_A, RobotMap.LEFT_SHACK_CHANNEL_B);
 	}
 
 	public void ShackIn() {
-		leftShack.set(DoubleSolenoid.Value.kForward);
-		// rightShack.set(DoubleSolenoid.Value.kForward);
+		shack.set(DoubleSolenoid.Value.kForward);
 	}
 
 	public void ShackOut() {
-		leftShack.set(DoubleSolenoid.Value.kReverse);
-		// rightShack.set(DoubleSolenoid.Value.kReverse);
+		shack.set(DoubleSolenoid.Value.kReverse);
 	}
 
 	@Override
 	protected void initDefaultCommand() {
-		// TODO Auto-generated method stub
 	}
 
 }
