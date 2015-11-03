0beda3703f669d78655a5f5d02c6a2df1ece4bce
Claire <claire.legoues@gmail.com>
2015-11-03 15:13:48 -0500

    Fix indentation.

--- 2016GitLesson/src/IceCreamExample.java
+++ 2016GitLesson/src/IceCreamExample.java
@@ -7,13 +7,12 @@ public class IceCreamExample {
 			System.out.println("Super Happy"); 
 		} else if (icecreamScoops==10) {
 			System.out.println("Happy"); 
-			}
+		}
 		else if (icecreamScoops<=9 && icecreamScoops>=5) {
 			System.out.println("Sad");
-			}
-		else	{
+		} else {
 			System.out.println("Super Sad");
 		}
-				
+
 	}
 }
