978b938041a641f46663e1b35a29c02f6d64aef8
Claire <claire.legoues@gmail.com>
2015-11-03 15:10:01 -0500

    First ice cream checkin.

---
+++ 2016GitLesson/src/IceCreamExample.java
@@ -0,0 +1,19 @@
+
+public class IceCreamExample {
+	public static void main(String[] args) {
+		int icecreamScoops = Integer.parseInt(args[0]);
+		if (icecreamScoops>10) 
+		{
+			System.out.println("Super Happy"); 
+		} else if (icecreamScoops==10) {
+			System.out.println("Happy"); 
+			}
+		else if (icecreamScoops<=9 && icecreamScoops>=5) {
+			System.out.println("Sad");
+			}
+		else	{
+			System.out.println("Super Sad");
+		}
+				
+	}
+}
