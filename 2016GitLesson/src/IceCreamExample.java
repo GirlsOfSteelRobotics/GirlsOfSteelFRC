a034dcfdf5af7e3cc8f82f47e9f13086059807eb
Alexa <selwooda.gos@gmail.com>
2015-11-03 19:30:49 -0500

    added condition to the else

--- 2016GitLesson/src/IceCreamExample.java
+++ 2016GitLesson/src/IceCreamExample.java
@@ -8,9 +8,13 @@ public class IceCreamExample {
 		} else if (icecreamScoops==10) {
 			System.out.println("Happy"); 
 		}
+		
 		else if (icecreamScoops<=9 && icecreamScoops>=5) {
 			System.out.println("Sad");
-		} else {
+		} else if (icecreamScoops<=15 && icecreamScoops>=11) {
+			System.out.println("Lovely");
+		}
+		else {
 			System.out.println("Super Sad");
 		}
 
