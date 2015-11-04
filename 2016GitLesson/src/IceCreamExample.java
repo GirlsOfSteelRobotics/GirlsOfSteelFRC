685fab93893341fba6b0212c4cdca754c03d3867
alex <alex@alex-PC>
2015-11-03 16:35:30 -0800

    all lowercase

--- 2016GitLesson/src/IceCreamExample.java
+++ 2016GitLesson/src/IceCreamExample.java
@@ -4,14 +4,14 @@ public class IceCreamExample {
 		int icecreamScoops = Integer.parseInt(args[0]);
 		if (icecreamScoops>10) 
 		{
-			System.out.println("Super Happy"); 
+			System.out.println("super happy"); 
 		} else if (icecreamScoops==10) {
-			System.out.println("Happy"); 
+			System.out.println("happy"); 
 		}
 		else if (icecreamScoops<=9 && icecreamScoops>=5) {
-			System.out.println("Sad");
+			System.out.println("sad");
 		} else {
-			System.out.println("Super Sad");
+			System.out.println("super sad");
 		}
 
 	}
