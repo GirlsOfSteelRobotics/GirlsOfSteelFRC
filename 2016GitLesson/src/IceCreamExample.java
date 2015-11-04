3a44426de3871136da2907d135d03b9d8a00d47f
kritishah2000 <shahk.gos@gmail.com>
2015-11-03 19:47:18 -0500

    Added a loop that prints out the feeling 10 times.

--- 2016GitLesson/src/IceCreamExample.java
+++ 2016GitLesson/src/IceCreamExample.java
@@ -2,6 +2,7 @@
 public class IceCreamExample {
 	public static void main(String[] args) {
 		int icecreamScoops = Integer.parseInt(args[0]);
+		
 		if (icecreamScoops>10) 
 		{
 			System.out.println("Super Happy"); 
@@ -9,9 +10,20 @@ public class IceCreamExample {
 			System.out.println("Happy"); 
 		}
 		else if (icecreamScoops<=9 && icecreamScoops>=5) {
-			System.out.println("Sad");
+			int i = 1;
+			if (i<=10) {
+				i++;
+				System.out.println("Sad");
+			}
+			else {
+				
+			}
 		} else {
+			int i = 1;
+			if (i<=10) {
+				i++;
 			System.out.println("Super Sad");
+			}
 		}
 
 	}
