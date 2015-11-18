d3c5c4c9357da6b149e7cbb8221d6c65dd4641bc
arushibandi <bandia.gos@gmail.com>
2015-11-17 19:58:55 -0500

    started driveByJoystick

--- TeamSquirtle/src/org/usfirst/frc/team3504/robot/subsystems/Chassis.java
+++ TeamSquirtle/src/org/usfirst/frc/team3504/robot/subsystems/Chassis.java
@@ -1,5 +1,33 @@
 package org.usfirst.frc.team3504.robot.subsystems;
 
+import org.usfirst.frc.team3504.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+
 public class Chassis {
-// Im rozie. can you see this???
+
+	CANTalon rt1 = RobotMap.rightTalon1;
+	CANTalon rt2 = RobotMap.rightTalon2;
+	CANTalon rt3 = RobotMap.rightTalon3;
+	CANTalon lt1 = RobotMap.leftTalon1;
+	CANTalon lt2 = RobotMap.leftTalon2;
+	CANTalon lt3 = RobotMap.leftTalon3;
+	RobotDrive driveSystem = RobotMap.driveSystem;
+	Joystick stick;
+	
+	public Chassis() {
+		
+	}
+	
+	public void driveByJoystick(Joystick stick) {
+		driveSystem.arcadeDrive(stick);
+	}
+	
+	public void initDefaultCommand() {
+		
+	}
+	
+	
 }
