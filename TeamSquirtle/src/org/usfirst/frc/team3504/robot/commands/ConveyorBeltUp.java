7bbcc3032fb5fb535015f3abb7f6054ad845cc0e
arushibandi <bandia.gos@gmail.com>
2015-11-24 20:21:29 -0500

    Created conveyorbelt commands

--- TeamSquirtle/src/org/usfirst/frc/team3504/robot/commands/ConveyorBeltUp.java
+++ TeamSquirtle/src/org/usfirst/frc/team3504/robot/commands/ConveyorBeltUp.java
@@ -1,20 +1,23 @@
 package org.usfirst.frc.team3504.robot.commands;
 
-import org.usfirst.frc.team3504.robot.Robot;
+import edu.wpi.first.wpilibj.command.Command;
 
-public class ControlConveyorBelt {
+/**
+ *
+ */
+public class ConveyorBeltUp extends Command {
 
-	public ControlConveyorBelt() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	// Called just before this Command runs the first time
+    public ConveyorBeltUp() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
     protected void initialize() {
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -30,5 +33,4 @@ public class ControlConveyorBelt {
     // subsystems is scheduled to run
     protected void interrupted() {
     }
-
 }
