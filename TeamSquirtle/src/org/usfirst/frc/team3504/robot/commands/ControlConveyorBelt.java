db11207dc7980744a76748eb6ed9bf519e0acbfa
ziyaxu <ziyaxu07@gmail.com>
2015-11-24 20:09:36 -0500

    deleted unfinished line

--- TeamSquirtle/src/org/usfirst/frc/team3504/robot/commands/ControlConveyorBelt.java
+++ TeamSquirtle/src/org/usfirst/frc/team3504/robot/commands/ControlConveyorBelt.java
@@ -14,7 +14,7 @@ public class ControlConveyorBelt {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.con;
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
