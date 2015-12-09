1966bcf7ad29fa1bd9133e5a9a3f990ca8e59e89
arushibandi <bandia.gos@gmail.com>
2015-12-08 19:01:36 -0500

    added USBCamera (in comments)

--- TeamSquirtle/src/org/usfirst/frc/team3504/robot/subsystems/Camera.java
+++ TeamSquirtle/src/org/usfirst/frc/team3504/robot/subsystems/Camera.java
@@ -2,16 +2,18 @@ package org.usfirst.frc.team3504.robot.subsystems;
 
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.vision.USBCamera;
 
 /**
  *
  */
 public class Camera extends Subsystem {
 	CameraServer server;
-
+	//USBCamera cam;
 	public Camera() {
 		server = CameraServer.getInstance();
 		server.setQuality(50);
+		//cam = new USBCamera();
 		// the camera name (ex "cam0") can be found through the roborio web
 		// interface
 		server.startAutomaticCapture("cam0");
@@ -19,6 +21,12 @@ public class Camera extends Subsystem {
 
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
+	/*
+	public void findTube() {
+		cam.getImageData(null);
+		
+	}
+	*/
 
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
