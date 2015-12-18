88ff00af64b00ff9cff5191f9aa6d7ff874acbe2
Sarah <smes3007@gmail.com>
2015-12-18 18:20:58 -0500

    Ramp Up

---
+++ TeamSquirtle/src/org/usfirst/frc/team3504/robot/subsystems/RampUp.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team3504.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class RampUp extends Subsystem {
+    public static DoubleSolenoid leftramp=RobotMap.
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	public void Up(){
+		
+		
+	}
+
+	public void Down(){
+		
+	}
+	
+    public void initDefaultCommand() {
+    	
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
