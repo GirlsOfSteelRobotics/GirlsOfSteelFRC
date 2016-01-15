c3a0db8026be3bb63d43e50444894f98b91205c8
sophiaslee <leesophia.gos@gmail.com>
2016-01-14 19:34:19 -0500

    fixed talons (?)

--- Neohuman_Assassination_Golem/src/org/usfirst/frc/team3504/robot/RobotMap.java
+++ Neohuman_Assassination_Golem/src/org/usfirst/frc/team3504/robot/RobotMap.java
@@ -17,12 +17,12 @@ public class RobotMap {
     // public static int rangefinderModule = 1;
 	
 	//Talons for Driving
-	public static final int DRIVE_LEFT_A = 1; //TODO: fix these numbers
-	public static final int DRIVE_LEFT_B = 2;
-	public static final int DRIVE_LEFT_C = 3;
+	public static final int DRIVE_LEFT_A = 4; //TODO: fix these numbers
+	public static final int DRIVE_LEFT_B = 5;
+	public static final int DRIVE_LEFT_C = 6;
 	
-	public static final int DRIVE_RIGHT_A = 4;
-	public static final int DRIVE_RIGHT_B = 5;
-	public static final int DRIVE_RIGHT_C = 6;
+	public static final int DRIVE_RIGHT_A = 1;
+	public static final int DRIVE_RIGHT_B = 2;
+	public static final int DRIVE_RIGHT_C = 3;
 
 }
