220da0dd995347fe28a7da37cb8fc8e5509e69af
chswim17 <chswim17@gmail.com>
2016-02-02 18:22:07 -0500

    added LEDLights subsystem

---
+++ Neohuman_Assassination_Golem/src/org/usfirst/frc/team3504/robot/subsystems/LEDLights.java
@@ -0,0 +1,23 @@
+package org.usfirst.frc.team3504.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class LEDLights extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+	public LEDLights() {
+		
+	}
+	
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
