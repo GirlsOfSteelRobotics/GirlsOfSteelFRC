e5391747436e280e49b7ba80bfc4d57ade1d6c93
soniaappasamy <appasamysm@gmail.com>
2016-02-11 20:24:05 -0500

    worked on autonomous & fixed encoder gear ratio

--- Neohuman_Assassination_Golem/src/org/usfirst/frc/team3504/robot/subsystems/LEDLights.java
+++ Neohuman_Assassination_Golem/src/org/usfirst/frc/team3504/robot/subsystems/LEDLights.java
@@ -8,13 +8,14 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class LEDLights extends Subsystem {
     
-	SerialPort serialPort = new SerialPort(9600, SerialPort.Port.kUSB);
+	//SerialPort serialPort;
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
 	public LEDLights() {
+		//serialPort = new SerialPort(9600, SerialPort.Port.kUSB); 
 	}
-	
+	/*
 	public void blueLight(){
 		serialPort.writeString("b"); 
 	}
@@ -37,10 +38,11 @@ public class LEDLights extends Subsystem {
 	public void dotLights(){
 		serialPort.writeString("p"); 
 	}
-	
+	*/
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
     }
+    
 }
 
