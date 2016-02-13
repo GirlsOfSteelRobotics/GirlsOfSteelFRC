b58cb667e52747cdbeee894124e304fe6feae0f6
arushibandi <bandia.gos@gmail.com>
2016-02-13 11:28:34 -0500

    changed speeds for testing

--- Neohuman_Assassination_Golem/src/org/usfirst/frc/team3504/robot/commands/PivotUp.java
+++ Neohuman_Assassination_Golem/src/org/usfirst/frc/team3504/robot/commands/PivotUp.java
@@ -34,7 +34,7 @@ public class PivotUp extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.pivot.tiltUpandDown(.1);
+    	Robot.pivot.tiltUpandDown(.4);
     }
 
     // Make this return true when this Command no longer needs to run execute()
