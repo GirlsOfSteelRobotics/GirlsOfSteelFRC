ceb66ba5ef914fc17ae369ab4dc05a0b1a587ca2
kritishah2000 <shahk.gos@gmail.com>
2016-02-15 18:49:21 -0500

    Added Test Flap

--- Neohuman_Assassination_Golem/src/org/usfirst/frc/team3504/robot/commands/TestFlapUp.java
+++ Neohuman_Assassination_Golem/src/org/usfirst/frc/team3504/robot/commands/TestFlapUp.java
@@ -20,7 +20,7 @@ public class TestFlapUp extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.flap.setTalon(0.1);
+    	Robot.flap.setTalon(0.3);
     }
 
     // Make this return true when this Command no longer needs to run execute()
