58859b33ac30223ca9b1d153f8da719ebd4d52cb
rfero <roslyn.fero@gmail.com>
2016-03-21 19:13:32 -0400

    Increased claw speed again

--- Neohuman_Assassination_Golem/src/org/usfirst/frc/team3504/robot/commands/CollectBall.java
+++ Neohuman_Assassination_Golem/src/org/usfirst/frc/team3504/robot/commands/CollectBall.java
@@ -24,9 +24,9 @@ public class CollectBall extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	if(RobotMap.USING_CLAW)
-    		Robot.claw.collectRelease(-1);
+    		Robot.claw.collectRelease(-1.1);
     	else
-    		Robot.shooter.spinWheels(.9);
+    		Robot.shooter.spinWheels(1);
     	
     }
 
