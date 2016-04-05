9a2088090a86594728475380107e5ccaf16ad9a1
Kriti Shah <kritishah2000@yahoo.com>
2016-04-05 18:57:05 -0400

    Added end to interrupted in StopShooterWheels.

--- Neohuman_Assassination_Golem/src/org/usfirst/frc/team3504/robot/commands/StopShooterWheels.java
+++ Neohuman_Assassination_Golem/src/org/usfirst/frc/team3504/robot/commands/StopShooterWheels.java
@@ -40,5 +40,6 @@ public class StopShooterWheels extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
