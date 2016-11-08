41d5b4b8d4cb975feacb6fe27e1a8a56455854fc
Kriti Shah <kritishah2000@yahoo.com>
2016-11-08 18:28:10 -0500

    worked on driveByJoystick command

--- ChassisBot/src/org/usfirst/frc/team3504/robot/commands/DriveByJoystick.java
+++ ChassisBot/src/org/usfirst/frc/team3504/robot/commands/DriveByJoystick.java
@@ -1,19 +1,26 @@
 package org.usfirst.frc.team3504.robot.commands;
 
+import org.usfirst.frc.team3504.robot.Robot;
+
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
 public class DriveByJoystick extends Command {
+	
+	Joystick stick;
 
     public DriveByJoystick() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	requires(Robot.driveSystem);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	stick = Robot.oi.driveStick;
     }
 
     // Called repeatedly when this Command is scheduled to run
