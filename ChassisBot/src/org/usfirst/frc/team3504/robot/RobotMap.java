e32edfd1564cdceaa945e7022292c787c5727a57
Kriti Shah <kritishah2000@yahoo.com>
2016-11-08 19:47:32 -0500

    Fixed chassisbot code. May drive backwards.

--- ChassisBot/src/org/usfirst/frc/team3504/robot/RobotMap.java
+++ ChassisBot/src/org/usfirst/frc/team3504/robot/RobotMap.java
@@ -11,13 +11,11 @@ import edu.wpi.first.wpilibj.RobotDrive;
  */
 public class RobotMap {
 	
-	public static final int DRIVE_LEFT_A_CAN_ID = 4; 
-	public static final int DRIVE_LEFT_B_CAN_ID = 5;
-	public static final int DRIVE_LEFT_C_CAN_ID = 6;
+	public static final int DRIVE_LEFT_A_CAN_ID = 3; 
+	public static final int DRIVE_LEFT_B_CAN_ID = 4;
 
 	public static final int DRIVE_RIGHT_A_CAN_ID = 1;
 	public static final int DRIVE_RIGHT_B_CAN_ID = 2;
-	public static final int DRIVE_RIGHT_C_CAN_ID = 3;
 	
 	public static final int DRIVE_LEFT_ENCODER_A = 0;
 	public static final int DRIVE_LEFT_ENCODER_B = 1;
