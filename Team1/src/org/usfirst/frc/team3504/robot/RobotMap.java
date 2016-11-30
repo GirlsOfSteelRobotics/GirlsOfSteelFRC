c1d21f8ddf38091cd3f2d89a5031e7480058ba69
maansi-dasari <mdbidda@gmail.com>
2016-11-29 19:27:02 -0500

    Finished Piston subsystem, need to fix solenoid values in RobotMap

--- Team1/src/org/usfirst/frc/team3504/robot/RobotMap.java
+++ Team1/src/org/usfirst/frc/team3504/robot/RobotMap.java
@@ -34,6 +34,10 @@ public class RobotMap {
 	public static final int DRIVE_RIGHT_ENCODER_A = 2;
 	public static final int DRIVE_RIGHT_ENCODER_B = 3;
 	
+	//Solenoids for piston/shooter
+	public static final int SHOOTER_PISTON_A = 1; //TODO: fix
+	public static final int SHOOTER_PISTON_B = 1; //TODO: fix		
+	
 	// Encoder-to-distance constants
 	// How many ticks are there on the encoder wheel?
 	private static final double pulsePerRevolution = 360;
--- Team1/src/org/usfirst/frc/team3504/robot/subsystems/Piston.java
+++ Team1/src/org/usfirst/frc/team3504/robot/subsystems/Piston.java
@@ -1,6 +1,9 @@
 package org.usfirst.frc.team3504.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+
+import org.usfirst.frc.team3504.robot.RobotMap;
+
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 /**
  *
@@ -11,14 +14,18 @@ public class Piston extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 	
+	public Piston()
+	{
+		shooterPiston = new DoubleSolenoid(RobotMap.SHOOTER_PISTON_A, RobotMap.SHOOTER_PISTON_B);
+	}
 	public void pistonOut()
 	{
-		
+		shooterPiston.set(DoubleSolenoid.Value.kForward);
 	}
 	
 	public void pistonIn()
 	{
-		
+		shooterPiston.set(DoubleSolenoid.Value.kReverse);
 	}
 
     public void initDefaultCommand() {
