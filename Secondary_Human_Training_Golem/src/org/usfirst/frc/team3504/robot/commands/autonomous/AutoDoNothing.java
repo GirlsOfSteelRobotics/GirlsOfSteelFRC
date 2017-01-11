0400e1f9d6eea9dbbd6d4376495f357b8f214e50
ziyaxu <ziyaxu07@gmail.com>
2017-01-10 20:14:18 -0500

    AutoDoNothing

--- Secondary_Human_Training_Golem/src/org/usfirst/frc/team3504/robot/commands/autonomous/AutoDoNothing.java
+++ Secondary_Human_Training_Golem/src/org/usfirst/frc/team3504/robot/commands/autonomous/AutoDoNothing.java
@@ -7,43 +7,33 @@ import edu.wpi.first.wpilibj.command.Command;
 /**
  *
  */
-public class AutoBaseLine extends Command {
-	
-	private double inches;
-	private double speed;
+public class AutoDoNothing extends Command {
 
-    public AutoBaseLine(double distance, double speed) {
-		// Use requires() here to declare subsystem dependencies
+    public AutoDoNothing() {
+        // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(Robot.chassis);
-    	inches = distance;
-    	this.speed = speed;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	System.out.println("Inches: " + inches);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.chassis.driveSpeed(speed);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.chassis.stop();
-    	System.out.println("Stopped");
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	end();
     }
 }
