2cb8e83b32cb3184d636e3a4d59db55829c615b9
Joseph Jackson <joseph.l.jackson@gmail.com>
2017-01-23 19:18:53 -0500

    added encoder stuff

--- ChassisBot/src/org/usfirst/frc/team3504/robot/commands/DriveByJoystick.java
+++ ChassisBot/src/org/usfirst/frc/team3504/robot/commands/DriveByJoystick.java
@@ -21,11 +21,13 @@ public class DriveByJoystick extends Command {
     // Called just before this Command runs the first time
     protected void initialize() {
     	stick = Robot.oi.driveStick;
+    	Robot.driveSystem.resetDistance();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	Robot.driveSystem.driveByJoystick(stick);
+    	Robot.driveSystem.getEncoderDistance();
     }
 
     // Make this return true when this Command no longer needs to run execute()
