fa24b19ee49c702667892f31f8f6248f659a1fcd
jzwang8 <jzwang8@gmail.com>
2017-02-07 18:45:17 -0500

    finished lights

--- CAN Lights/src/org/usfirst/frc/team3504/robot/subsystems/Lights.java
+++ CAN Lights/src/org/usfirst/frc/team3504/robot/subsystems/Lights.java
@@ -1,5 +1,9 @@
 package org.usfirst.frc.team3504.robot.subsystems;
 
+import org.usfirst.frc.team3504.robot.RobotMap;
+
+import com.mindsensors.CANLight;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -9,8 +13,15 @@ public class Lights extends Subsystem {
 	// Put methods for controlling this subsystem
 	// here. Call these from Commands.
 
+	private CANLight lights;
 	public void initDefaultCommand() {
 		// Set the default command for a subsystem here.
 		// setDefaultCommand(new MySpecialCommand());
+		lights = new CANLight(RobotMap.LIGHTS);
+	}
+	
+	public void changeColor(int R, int G, int B)
+	{
+		lights.showRGB(R, G, B);
 	}
 }
