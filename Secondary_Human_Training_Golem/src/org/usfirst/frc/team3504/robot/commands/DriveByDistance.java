71a589496fb0e66ef4dd8e2d7a17ba5c86fca87a
Kriti Shah <kritishah2000@yahoo.com>
2017-03-07 20:03:09 -0500

    Moved set methods to initialize. Also, changed return statement in isFinished

--- Secondary_Human_Training_Golem/src/org/usfirst/frc/team3504/robot/commands/DriveByDistance.java
+++ Secondary_Human_Training_Golem/src/org/usfirst/frc/team3504/robot/commands/DriveByDistance.java
@@ -11,6 +11,8 @@ import edu.wpi.first.wpilibj.command.Command;
 public class DriveByDistance extends Command {
 
 	private double rotations; 
+	//Desired error limit was half an inch. 1/2 inch in ticks is approximately 150.
+	private static final int ERROR_LIMIT = 150;
 	
     public DriveByDistance(double inches){
     	rotations = inches / (RobotMap.WHEEL_DIAMETER * Math.PI); 
@@ -27,17 +29,19 @@ public class DriveByDistance extends Command {
     	Robot.chassis.getLeftTalon().setF(0.0);
     	Robot.chassis.getRightTalon().setF(0.0);
     	
+    	Robot.chassis.getLeftTalon().set(-rotations); //Move this back to execute
+    	Robot.chassis.getRightTalon().set(rotations); //Move this back to execute
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.chassis.getLeftTalon().set(-rotations);
-    	Robot.chassis.getRightTalon().set(rotations); 
+
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+    	return Math.abs(Robot.chassis.getLeftTalon().getClosedLoopError()) < ERROR_LIMIT && 
+    			Math.abs(Robot.chassis.getRightTalon().getClosedLoopError()) < ERROR_LIMIT; //closedLoopError is in integers and represents ticks
     }
 
     // Called once after isFinished returns true
