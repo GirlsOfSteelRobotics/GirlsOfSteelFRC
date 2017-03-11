a0810fc3ee846451c72ca4771bc7bb4a77eb8c29
Joseph Jackson <joseph.l.jackson@gmail.com>
2017-03-11 13:08:18 -0500

    Climber: increase from 3/4 to full power

--- Secondary_Human_Training_Golem/src/org/usfirst/frc/team3504/robot/commands/Climb.java
+++ Secondary_Human_Training_Golem/src/org/usfirst/frc/team3504/robot/commands/Climb.java
@@ -25,7 +25,7 @@ public class Climb extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.climber.climb(-0.75);
+    	Robot.climber.climb(-1.0);
     }
 
     // Make this return true when this Command no longer needs to run execute()
