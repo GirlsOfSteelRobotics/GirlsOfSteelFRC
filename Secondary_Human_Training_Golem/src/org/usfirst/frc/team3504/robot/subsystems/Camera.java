2994334ebb7fb78c1cf80b959ebfbcccb332d451
Kriti Shah <kritishah2000@yahoo.com>
2017-11-30 18:55:14 -0500

    Changed Camera.java and RobotMap.java to match 2017 code.

--- Secondary_Human_Training_Golem/src/org/usfirst/frc/team3504/robot/subsystems/Camera.java
+++ Secondary_Human_Training_Golem/src/org/usfirst/frc/team3504/robot/subsystems/Camera.java
@@ -13,15 +13,22 @@ public class Camera extends Subsystem {
 
 	private UsbCamera camGear;
 	private UsbCamera camClimb;
+	public UsbCamera visionCam; 
 	private MjpegServer server;
 
 	public Camera() {
 		camGear = new UsbCamera("camGear", RobotMap.CAMERA_GEAR);
 		camGear.setResolution(320, 240);
+		camGear.setFPS(10);
 		camClimb = new UsbCamera("camClimb", RobotMap.CAMERA_CLIMB);
 		camClimb.setResolution(320, 240);   
+		camClimb.setFPS(10);
+		visionCam = new UsbCamera("visionCam", RobotMap.VISION_CAMERA);
+		visionCam.setResolution(320, 240);
+		visionCam.setFPS(10); 
 		CameraServer.getInstance().addCamera(camGear);
 		CameraServer.getInstance().addCamera(camClimb);
+		CameraServer.getInstance().addCamera(visionCam);
 		server = CameraServer.getInstance().addServer("CameraServer", 1181);
 		server.setSource(camGear);
 		
