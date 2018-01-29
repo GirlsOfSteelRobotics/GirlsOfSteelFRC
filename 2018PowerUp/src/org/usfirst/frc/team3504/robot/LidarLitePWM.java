edf8875676440eea8a3f84622a0d034104dcf37f
Joseph Jackson <joseph.l.jackson@gmail.com>
2018-01-29 16:46:54 -0500

    LidarLitePWM: Fixed endless loop when no LIDAR-Lite is plugged in

--- 2018PowerUp/src/org/usfirst/frc/team3504/robot/LidarLitePWM.java
+++ 2018PowerUp/src/org/usfirst/frc/team3504/robot/LidarLitePWM.java
@@ -4,8 +4,21 @@ import edu.wpi.first.wpilibj.Counter;
 import edu.wpi.first.wpilibj.DigitalSource;
 
 public class LidarLitePWM {
+	/*
+	 * Adjust the Calibration Offset to compensate for differences in each unit.
+	 * We've found this is a reasonably constant value for readings in the 25 cm to 600 cm range.
+	 * You can also use the offset to zero out the distance between the sensor and edge of the robot.
+	 */
+	private static final int CALIBRATION_OFFSET = -18;
+
 	private Counter counter;
+	private int printedWarningCount = 5;
 	
+	/**
+	 * Create an object for a LIDAR-Lite attached to some digital input on the roboRIO
+	 * 
+	 * @param source The DigitalInput or DigitalSource where the LIDAR-Lite is attached (ex: new DigitalInput(9))
+	 */
 	public LidarLitePWM (DigitalSource source) {
 		counter = new Counter(source);
 	    counter.setMaxPeriod(1.0);
@@ -21,13 +34,19 @@ public class LidarLitePWM {
 	 */
 	public double getDistance() {
 		double cm;
-		while (counter.get() < 1) {
-			System.out.println("LidarLitePWM: waiting for distance measurement");
+		/* If we haven't seen the first rising to falling pulse, then we have no measurement.
+		 * This happens when there is no LIDAR-Lite plugged in, btw.
+		 */
+		if (counter.get() < 1) {
+			if (printedWarningCount-- > 0) {
+				System.out.println("LidarLitePWM: waiting for distance measurement");
+			}
+			return 0;
 		}
 		/* getPeriod returns time in seconds. The hardware resolution is microseconds.
 		 * The LIDAR-Lite unit sends a high signal for 10 microseconds per cm of distance.
 		 */
-		cm = (counter.getPeriod() * 1000000.0 / 10.0) - 18;
+		cm = (counter.getPeriod() * 1000000.0 / 10.0) + CALIBRATION_OFFSET;
 		return cm;
 	}
 }
