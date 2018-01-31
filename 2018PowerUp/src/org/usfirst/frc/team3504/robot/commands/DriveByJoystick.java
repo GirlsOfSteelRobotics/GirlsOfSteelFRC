e81c79dc7531b115e89d4bd9c7c8ac20269d73fc
Joseph Jackson <joseph.l.jackson@gmail.com>
2018-01-30 21:54:16 -0500

    DriveByJoystick now has interrupted() call end()

--- 2018PowerUp/src/org/usfirst/frc/team3504/robot/commands/DriveByJoystick.java
+++ 2018PowerUp/src/org/usfirst/frc/team3504/robot/commands/DriveByJoystick.java
@@ -64,5 +64,6 @@ public class DriveByJoystick extends Command {
 	// Called when another command which requires one or more of the same
 	// subsystems is scheduled to run
 	protected void interrupted() {
+		end();
 	}
 }
