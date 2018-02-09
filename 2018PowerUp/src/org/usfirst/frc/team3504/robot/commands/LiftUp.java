0f3e999942af4601a8a13882458b69ad2daaae8c
Joseph Jackson <joseph.l.jackson@gmail.com>
2018-02-08 20:35:42 -0500

    made liftUp even slower for drive practice tomorrow

--- 2018PowerUp/src/org/usfirst/frc/team3504/robot/commands/LiftUp.java
+++ 2018PowerUp/src/org/usfirst/frc/team3504/robot/commands/LiftUp.java
@@ -26,7 +26,7 @@ public class LiftUp extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.lift.setLiftSpeed(-0.7);
+    	Robot.lift.setLiftSpeed(-0.4);
     }
 
     // Make this return true when this Command no longer needs to run execute()
