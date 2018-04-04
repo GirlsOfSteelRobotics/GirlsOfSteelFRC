0c6ad054794285649dca556661df86df258a62b5
maansi-dasari <dasari.maansi@gmail.com>
2018-04-04 19:27:27 -0400

    some two cube stuff

---
+++ 2018PowerUp/src/org/usfirst/frc/team3504/robot/commands/autonomous/AutoMiddleSwitchTwoCube.java
@@ -0,0 +1,77 @@
+package org.usfirst.frc.team3504.robot.commands.autonomous;
+
+import org.usfirst.frc.team3504.robot.GameData.FieldSide;
+import org.usfirst.frc.team3504.robot.commands.Collect;
+import org.usfirst.frc.team3504.robot.commands.CollectPosition;
+import org.usfirst.frc.team3504.robot.commands.CollectorStop;
+import org.usfirst.frc.team3504.robot.commands.DriveByMotionMagic;
+import org.usfirst.frc.team3504.robot.commands.LiftHold;
+import org.usfirst.frc.team3504.robot.commands.LiftToSwitch;
+import org.usfirst.frc.team3504.robot.commands.ReleaseSlow;
+import org.usfirst.frc.team3504.robot.commands.TimeDelay;
+import org.usfirst.frc.team3504.robot.commands.WristHold;
+import org.usfirst.frc.team3504.robot.commands.WristToCollect;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class AutoMiddleSwitchTwoCube extends CommandGroup {
+	public final static double RIGHT_ANGLE = 50.0;
+	public final static double RIGHT_DISTANCE = 63.0;
+	public final static double LEFT_ANGLE = 65.0;
+	public final static double LEFT_DISTANCE = 70.0;
+	public final static double BACK_UP = -70.0;
+	
+	public final static double TURN_RADIUS_2 = 100.0;
+	public final static double TURN_DEGREES_2 = 70.0;
+
+    public AutoMiddleSwitchTwoCube(FieldSide switchSide) {
+    	
+    	addSequential(new WristToCollect());
+    	addSequential(new LiftToSwitch());
+    	addParallel(new WristHold());
+    	addParallel(new LiftHold());
+    	
+    	if(switchSide == FieldSide.right)
+    	{
+    		addSequential(new DriveByMotionMagic(RIGHT_DISTANCE, -RIGHT_ANGLE));
+    		addSequential(new DriveByMotionMagic(RIGHT_DISTANCE, RIGHT_ANGLE));
+    	}
+    	else if (switchSide == FieldSide.left)
+    	{
+    		addSequential(new DriveByMotionMagic(LEFT_DISTANCE, LEFT_ANGLE));
+    		addSequential(new DriveByMotionMagic(LEFT_DISTANCE, -LEFT_ANGLE));
+    	}
+    	else System.out.println("AutoMiddleSwitch: invalid switch side");
+    	
+    	//Release and back up
+    	addParallel(new ReleaseSlow());
+    	addSequential(new TimeDelay(1.0));
+    	addSequential(new DriveByMotionMagic(BACK_UP, 0));
+    	
+    	//Put lift down and stop collector
+    	addSequential(new CollectPosition());
+    	addSequential(new CollectorStop());
+    	addParallel(new WristHold());
+    	addParallel(new LiftHold());
+    	
+    	//Start Collecting
+    	addParallel(new Collect());
+    	
+    	//Grab second cube and come back
+    	if(switchSide == FieldSide.right)
+    	{
+    		addSequential(new DriveByMotionMagic(TURN_RADIUS_2, TURN_DEGREES_2));
+    		addSequential(new DriveByMotionMagic(-TURN_RADIUS_2, 0, false));
+    	}
+    	else if (switchSide == FieldSide.left)
+    	{
+    		addSequential(new DriveByMotionMagic(-TURN_RADIUS_2, TURN_DEGREES_2));
+    		addSequential(new DriveByMotionMagic(TURN_RADIUS_2, 0, false));
+    	}
+    	else System.out.println("AutoMiddleSwitch: invalid switch side");
+    	addSequential(new DriveByMotionMagic(TURN_RADIUS_2, TURN_DEGREES_2));
+    }
+}
