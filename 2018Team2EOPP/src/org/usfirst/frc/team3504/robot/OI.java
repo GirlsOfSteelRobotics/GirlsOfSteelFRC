c8dd0b2cabb359117cdce5ff6a2002e10381692c
meghnabehari <meghnabehari@meghnas-air.wv.cc.cmu.edu>
2018-11-29 20:20:57 -0500

    OI.java Tentative Changes

--- 2018Team2EOPP/src/org/usfirst/frc/team3504/robot/OI.java
+++ 2018Team2EOPP/src/org/usfirst/frc/team3504/robot/OI.java
@@ -7,18 +7,57 @@
 
 package org.usfirst.frc.team3504.robot;
 
+//import org.usfirst.frc.team3504.robot.commands.DriveByArcade;
+
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-
+	public Joystick tankDrive;
+    public Joystick arcadeDrive;
 	private Joystick joystick1;
+
+    
+    public Button button1; 
+	public Button button2;
+	public Button buttonGo;
+
+	
 	
 	public OI() {
 		joystick1 = new Joystick(RobotMap.JOYSTICK_1);
+
+	joystick1 = new Joystick (2);
+	arcadeDrive = new Joystick(1);
+    //make sure that tank uses the logitech dual controller (basically the dual controller)
+    tankDrive = new Joystick(0);
+    button1 = new JoystickButton(joystick1, 1); //Open Pneumatic
+    button2 = new JoystickButton(joystick1, 2); //Close Pneumatic
+    buttonGo = new JoystickButton(joystick1, 3);
+    
+    //button1 = new JoystickButton(tankDrive, 1); //Open Pneumatic 
+	//button2 = new JoystickButton(tankDrive, 2); //Close Pneumatic
+
+    //button1.whenPressed(new OpenPneumatic());
+	
+    // SmartDashboard Buttons
+    //SmartDashboard.putData("DriveByArcade", new DriveByArcade());
+    
+    
+}
+
+	public Joystick getTankDrive() {
+	    return tankDrive;
+	}
+	
+	public Joystick getArcadeDrive() {
+	    return arcadeDrive;
 	}
 	
 	public double getDrivingJoystickY () {
@@ -29,6 +68,7 @@ public class OI {
 		return joystick1.getX();
 	}
 	}
+
 	//// CREATING BUTTONS
 	// One type of button is a joystick button which is any button on a
 	//// joystick.
