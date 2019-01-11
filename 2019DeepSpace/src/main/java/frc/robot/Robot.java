354815d57db874c75fb3d5f8e2ad8400d36e2ff4
Meghna Behari <meghnabehari@Meghnas-Air.wv.cc.cmu.edu>
2019-01-10 19:22:01 -0500

    Robot drive

--- 2019DeepSpace/src/main/java/frc/robot/Robot.java
+++ 2019DeepSpace/src/main/java/frc/robot/Robot.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -14,13 +7,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.Chassis;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
- */
+
 public class Robot extends TimedRobot {
   public static Chassis chassis = new Chassis();
   public static OI oi;
--- 2019DeepSpace/src/main/java/frc/robot/subsystems/Chassis.java
+++ 2019DeepSpace/src/main/java/frc/robot/subsystems/Chassis.java
@@ -14,122 +14,63 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Chassis extends Subsystem {
 	
-	private final WPI_TalonSRX talonSRX1 = new WPI_TalonSRX(RobotMap.DRIVE_RIGHT_FRONT);
-	private final WPI_TalonSRX talonSRX2 = new WPI_TalonSRX(RobotMap.DRIVE_RIGHT_MIDDLE);
-	private final WPI_TalonSRX talonSRX3 = new WPI_TalonSRX(RobotMap.DRIVE_RIGHT_BACK);
-	private final WPI_TalonSRX talonSRX4 = new WPI_TalonSRX(RobotMap.DRIVE_LEFT_FRONT);
-	private final WPI_TalonSRX talonSRX5 = new WPI_TalonSRX(RobotMap.DRIVE_LEFT_MIDDLE);
-	private final WPI_TalonSRX talonSRX6 = new WPI_TalonSRX(RobotMap.DRIVE_LEFT_BACK);
+	private WPI_TalonSRX masterLeft;
+	private WPI_TalonSRX slaveLeft_A;
+	private WPI_TalonSRX slaveLeft_B;
+
+	private WPI_TalonSRX masterRight;
+	private WPI_TalonSRX slaveRight_A;
+	private WPI_TalonSRX slaveRight_B;
 	
-
 	private DifferentialDrive drive;
-
-	public Chassis() {
-		// This is the constructor
-		// Safety and brakes ----------------------------------
-		talonSRX1.setNeutralMode(NeutralMode.Brake);
-		talonSRX4.setNeutralMode(NeutralMode.Brake);
+	
+	public Chassis () {
+		masterLeft = new WPI_TalonSRX(RobotMap.LEFT_MASTER_PORT);
+		slaveLeft_A = new WPI_TalonSRX(RobotMap.LEFT_SLAVE_A_PORT);
+		slaveLeft_B = new WPI_TalonSRX(RobotMap.LEFT_SLAVE_B_PORT);
 		
-
-		talonSRX2.set(ControlMode.Follower, talonSRX1.getDeviceID());
-		talonSRX3.set(ControlMode.Follower, talonSRX1.getDeviceID());
-		talonSRX5.set(ControlMode.Follower, talonSRX2.getDeviceID());
-		talonSRX6.set(ControlMode.Follower, talonSRX2.getDeviceID());
+		masterRight = new WPI_TalonSRX(RobotMap.RIGHT_MASTER_PORT); 
+		slaveRight_A = new WPI_TalonSRX(RobotMap.RIGHT_SLAVE_A_PORT); 
+		slaveRight_B = new WPI_TalonSRX(RobotMap.RIGHT_SLAVE_B_PORT); 
 		
+		masterLeft.setNeutralMode(NeutralMode.Brake);
+		slaveLeft_A.setNeutralMode(NeutralMode.Brake);
+		slaveLeft_B.setNeutralMode(NeutralMode.Brake);
 
-	    	talonSRX1.setSafetyEnabled(false);
-	    	talonSRX2.setSafetyEnabled(false);
-	    	talonSRX3.setSafetyEnabled(false);
-	    	
-	    	talonSRX4.setSafetyEnabled(false);
-	    	talonSRX5.setSafetyEnabled(false);
-	    	talonSRX6.setSafetyEnabled(false);
-		// -----------------------------------------------------
+		masterRight.setNeutralMode(NeutralMode.Brake);
+		slaveRight_A.setNeutralMode(NeutralMode.Brake);
+		slaveRight_B.setNeutralMode(NeutralMode.Brake);
 		
-
-		drive = new DifferentialDrive(talonSRX1, talonSRX4);
+		slaveLeft_A.follow(masterLeft, FollowerType.PercentOutput); 
+		slaveLeft_B.follow(masterLeft, FollowerType.PercentOutput);
+		
+		slaveRight_A.follow(masterRight, FollowerType.PercentOutput);
+		slaveRight_B.follow(masterRight, FollowerType.PercentOutput); 
+		
+		drive = new DifferentialDrive(masterLeft, masterRight);
 		drive.setSafetyEnabled(true);
 		drive.setExpiration(0.1);
 		drive.setMaxOutput(1.0);
 
-		// reverse();
 
-		setFollowerMode();
-
-		// invert(true); //uncomment if needed
-		// if needed to invert the talons, do before putting into the drive
-		drive = new DifferentialDrive(talonSRX4, talonSRX1);
-
-		drive.setSafetyEnabled(false);
-		// this line is to set it so the joystick isn't too sensitive to input
-		drive.setDeadband(0.02);
 	}
 
-	public void driveByJoystick(double yDir, double xDir) {
-		SmartDashboard.putString("driveByJoystick?", yDir + "," + xDir);
-		drive.arcadeDrive(yDir, xDir);
-	}
+	// Put methods for controlling this subsystem
+    // here. Call these from Commands.
 
-	public void driveForward() {
-		talonSRX4.set(0.4);
-		talonSRX1.set(0.4);
-	}
-
-	@Override
-	public void initDefaultCommand() {
-		setDefaultCommand(new DriveByJoystick());
-
-		// Set the default command for a subsystem here.
-		// setDefaultCommand(new MySpecialCommand());
-	}
-
-	@Override
-	public void periodic() {
-		// if you had something that needed to executed on a periodic sort of time
-		// instead of tiem, this is where you would put the commands
-	}
-
-	public void driveByTank(double left, double right) {
-		drive.tankDrive(left, right);
-	}
-
-	public void driveByArcade(double mag, double rotate) {
-		// speed and how tight the rotation is [-1,1]
-		drive.arcadeDrive(mag, rotate);
-	}
-
-	public void invert(boolean x) {
-		// if the moters are spinning in opposite directions
-		talonSRX1.setInverted(false);
-		talonSRX2.setInverted(false);
-		talonSRX3.setInverted(false);
-
-		talonSRX4.setInverted(x);
-		talonSRX5.setInverted(x);
-		talonSRX6.setInverted(x);
-	}
-
-	public void reverse() {
-		// if the motors are together but completely reversed (do not pair with invert)
-		talonSRX1.setInverted(true);
-		talonSRX3.setInverted(true);
-		talonSRX5.setInverted(true);
-
-		talonSRX2.setInverted(true);
-		talonSRX4.setInverted(true);
-		talonSRX6.setInverted(true);
-	}
-
-	public void setFollowerMode() {
-		// follower code
-		talonSRX2.follow(talonSRX1, FollowerType.PercentOutput);
-		talonSRX3.follow(talonSRX1, FollowerType.PercentOutput);
-
-		talonSRX5.follow(talonSRX4, FollowerType.PercentOutput);
-		talonSRX6.follow(talonSRX4, FollowerType.PercentOutput);
-	}
-
-	public void stop() {
-		drive.stopMotor();
-	}
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    	setDefaultCommand(new DriveByJoystick()); 
+    }
+    
+    public void driveByJoystick(double yDir, double xDir) {
+    	SmartDashboard.putString("driveByJoystick?", yDir + "," + xDir); 
+    	drive.arcadeDrive(yDir, xDir);
+    }
+    
+    public void stop() {
+    	drive.stopMotor(); 
+    }
 }
+
