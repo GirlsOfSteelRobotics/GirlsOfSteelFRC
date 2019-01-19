8119fcdb216e6c3e4263aee32899ad8be4247421
Anna Nesbitt <annanesbitt@Annas-MacBook-Air-2.local>
2019-01-19 13:17:26 -0500

    climber commands now return true when finished

--- 2019DeepSpace/src/main/java/frc/robot/commands/AllUp.java
+++ 2019DeepSpace/src/main/java/frc/robot/commands/AllUp.java
@@ -34,7 +34,7 @@ public class AllUp extends Command {
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return true;
   }
 
   // Called once after isFinished returns true
