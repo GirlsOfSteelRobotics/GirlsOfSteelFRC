bf52024f84b78e51d2357ea5e8890652b8a5d68d
Anna Nesbitt <annanesbitt@Annas-MacBook-Air-2.local>
2019-01-26 10:49:29 -0500

    screw climber

--- 2019DeepSpace/src/main/java/frc/robot/RobotMap.java
+++ 2019DeepSpace/src/main/java/frc/robot/RobotMap.java
@@ -39,6 +39,9 @@ public class RobotMap {
 	public static final int PISTON_BACK_A2 = 1; 
 	public static final int PISTON_BACK_B1 = 2; 
 	public static final int PISTON_BACK_B2 = 3; 
+	
+	public static final int SCREW_FRONT = 0;
+	public static final int SCREW_BACK = 0;
 
 	public static final int LIGHT_SENSOR_PORT = 0;
 
