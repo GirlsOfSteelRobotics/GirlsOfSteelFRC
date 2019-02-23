f583cf41c971cc3afbd012f21a5f6af62526f53a
Joseph Jackson <joseph.l.jackson@gmail.com>
2019-02-23 12:23:22 -0500

    Re-merged Climber MotionMagic changes

--- 2019DeepSpace/src/main/java/frc/robot/subsystems/Climber.java
+++ 2019DeepSpace/src/main/java/frc/robot/subsystems/Climber.java
@@ -40,10 +40,12 @@ public class Climber extends Subsystem {
   public static final double BACK_POSITION = 0;
 
   public static final double FIRST_GOAL_POS = 0.0; // Robot is powered on in fully retracted state
-  public static final double SECOND_GOAL_POS = -33000.0; //TODO; first estimate from quick measurement
-  public static final double THIRD_GOAL_POS = -78000.0; //TODO; first estimate 
-  //THIRD_GOAL_POS should be around -83000
+  public static final double SECOND_GOAL_POS = -33000.0; //tested and good
+  public static final double THIRD_GOAL_POS = -78000.0; //THIRD_GOAL_POS should be around -83000
   public static final double ALL_TO_ZERO = 0.0;
+
+  public static final int MAX_CRUISE_VELOCITY = 15000;
+  public static final int MAX_ACCELERATION = 6000;
 
   public double goalFrontPosition;
   public double goalBackPosition;
@@ -55,24 +57,32 @@ public class Climber extends Subsystem {
     climberFront.setSensorPhase(true);
     climberBack.setSensorPhase(true);
 
+    // PID
     climberFront.config_kF(0, 0, 10);
-    climberFront.config_kP(0, 1.0, 10); //1.0 works for manual control
+    climberFront.config_kP(0, 1.0, 10);
     climberFront.config_kI(0, 0, 10);
     climberFront.config_kD(0, 0, 10);
 
     climberBack.config_kF(0, 0, 10);
-    climberBack.config_kP(0, 0.85, 10); //.85
+    climberBack.config_kP(0, 0.85, 10);
     climberBack.config_kI(0, 0, 10);
     climberBack.config_kD(0, 0, 10);
+
+    // Motion Magic
+    climberFront.configMotionAcceleration(MAX_ACCELERATION);
+    climberFront.configMotionCruiseVelocity(MAX_CRUISE_VELOCITY);
+
+    climberBack.configMotionAcceleration(MAX_ACCELERATION);
+    climberBack.configMotionCruiseVelocity(MAX_CRUISE_VELOCITY);
 
     climberFront.setNeutralMode(NeutralMode.Brake);
     climberBack.setNeutralMode(NeutralMode.Brake);
 
     // Limit Switches On
-    // climberFront.configReverseLimitSwitchSource(RemoteLimitSwitchSource.RemoteTalonSRX, LimitSwitchNormal.NormallyOpen,
-    //     RobotMap.DRIVE_LEFT_MASTER_TALON);
-    // climberBack.configReverseLimitSwitchSource(RemoteLimitSwitchSource.RemoteTalonSRX, LimitSwitchNormal.NormallyOpen,
-    //     RobotMap.DRIVE_RIGHT_MASTER_TALON);
+//     climberFront.configReverseLimitSwitchSource(RemoteLimitSwitchSource.RemoteTalonSRX, LimitSwitchNormal.NormallyClosed,
+//         RobotMap.DRIVE_LEFT_MASTER_TALON);
+//     climberBack.configReverseLimitSwitchSource(RemoteLimitSwitchSource.RemoteTalonSRX, LimitSwitchNormal.NormallyClosed,
+//         RobotMap.DRIVE_RIGHT_MASTER_TALON);
 
     // Limit Switches Off
     climberFront.configReverseLimitSwitchSource(RemoteLimitSwitchSource.Deactivated, LimitSwitchNormal.NormallyClosed,
@@ -132,16 +142,11 @@ public class Climber extends Subsystem {
   }
 
   public void holdClimberFrontPosition() {
-    climberFront.set(ControlMode.Position, goalFrontPosition);
+    climberFront.set(ControlMode.MotionMagic, goalFrontPosition);
   }
 
   public void holdClimberBackPosition() {
-    climberBack.set(ControlMode.Position, goalBackPosition);
-  }
-
-  public void holdClimberAllPosition(){
-    climberBack.set(ControlMode.Position, goalBackPosition);
-    climberFront.set(ControlMode.Position, goalFrontPosition);
+    climberBack.set(ControlMode.MotionMagic, goalFrontPosition);
   }
   
   public void incrementFrontClimber() {
