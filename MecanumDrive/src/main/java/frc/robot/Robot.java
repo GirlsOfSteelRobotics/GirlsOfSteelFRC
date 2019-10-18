5d1fde104dbc878c99c33d571916608660951798
Emma Prokop <emma@Emmas-MacBook-Air.local>
2019-10-17 20:01:14 -0400

    Worked on Chassis subsytem.

--- MecanumDrive/src/main/java/frc/robot/Robot.java
+++ MecanumDrive/src/main/java/frc/robot/Robot.java
@@ -12,8 +12,8 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.commands.DriveByJoystick;
+import frc.robot.subsystems.Chassis;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -23,7 +23,7 @@ import frc.robot.subsystems.ExampleSubsystem;
  * project.
  */
 public class Robot extends TimedRobot {
-  public static ExampleSubsystem m_subsystem = new ExampleSubsystem();
+  public static Chassis m_subsystem = new Chassis();
   public static OI m_oi;
 
   Command m_autonomousCommand;
@@ -36,7 +36,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     m_oi = new OI();
-    m_chooser.setDefaultOption("Default Auto", new ExampleCommand());
+    m_chooser.setDefaultOption("Default Auto", new DriveByJoystick());
     // chooser.addOption("My Auto", new MyAutoCommand());
     SmartDashboard.putData("Auto mode", m_chooser);
   }
--- MecanumDrive/src/main/java/frc/robot/subsystems/Chassis.java
+++ MecanumDrive/src/main/java/frc/robot/subsystems/Chassis.java
@@ -7,18 +7,67 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.SparkMax;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import frc.robot.commands.DriveByJoystick;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 /**
  * An example subsystem.  You can replace me with your own Subsystem.
  */
-public class ExampleSubsystem extends Subsystem {
+public class Chassis extends Subsystem {
+
+  private CANSparkMax frontLeft;
+  private CANSparkMax frontRight;
+  private CANSparkMax rearLeft;
+  private CANSparkMax rearRight;
+
+  private MecanumDrive drive;
+
+  public Chassis() {
+   frontLeft = new CANSparkMax(1, MotorType.kBrushless);
+   frontRight = new CANSparkMax(2, MotorType.kBrushless);
+   rearLeft = new CANSparkMax(3, MotorType.kBrushless);
+   rearRight = new CANSparkMax(4, MotorType.kBrushless);
+
+   frontLeft.setIdleMode(IdleMode.kBrake);
+   frontRight.setIdleMode(IdleMode.kBrake);
+   rearLeft.setIdleMode(IdleMode.kBrake);
+   rearRight.setIdleMode(IdleMode.kBrake);
+
+   frontLeft.setInverted(false);
+   frontRight.setInverted(false);
+   rearLeft.setInverted(false);
+   rearRight.setInverted(false);
+
+   drive = new MecanumDrive(frontLeft, frontRight, rearLeft, rearRight);
+  }
+  
+
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
   @Override
   public void initDefaultCommand() {
+    setDefaultCommand(new DriveByJoystick());
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
+  }
+
+  public void driveByJoystick(double yDir, double xDir){
+    drive.arcadeDrive(yDir, xDir);
+  }
+
+  public void setSpeed(double speed) {
+    drive.arcadeDrive(speed, 0);
+  }
+
+  public void stop() {
+    drive.stopMotor();
   }
 }
