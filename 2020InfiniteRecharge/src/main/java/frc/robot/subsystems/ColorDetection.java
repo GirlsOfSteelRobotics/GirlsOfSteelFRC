5e9f9a0d646fd42e45e78f9aa11df1fe3d378cc7
Anna Nesbitt <annanesbitt@annas-air-2.wifi.cmu.edu>
2020-01-11 12:56:54 -0500

    added color matching code

--- 2020InfiniteRecharge/src/main/java/frc/robot/subsystems/ColorDetection.java
+++ 2020InfiniteRecharge/src/main/java/frc/robot/subsystems/ColorDetection.java
@@ -6,7 +6,11 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.subsystems;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
 import com.revrobotics.ColorSensorV3;
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -19,11 +23,24 @@ public class ColorDetection extends SubsystemBase {
 
   private final I2C.Port i2cPort = I2C.Port.kOnboard;
   private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+  private final ColorMatch m_colorMatcher = new ColorMatch();
+
+  private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+  private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+  private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+  private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
 
   public ColorDetection() {
     
     System.out.println("ColorDetection"); 
 
+  }
+
+  public void init(){
+    m_colorMatcher.addColorMatch(kBlueTarget);
+    m_colorMatcher.addColorMatch(kGreenTarget);
+    m_colorMatcher.addColorMatch(kRedTarget);
+    m_colorMatcher.addColorMatch(kYellowTarget);
   }
 
   @Override
@@ -33,10 +50,27 @@ public class ColorDetection extends SubsystemBase {
 
     double IR = m_colorSensor.getIR();
 
+    String colorString;
+    
+    ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+
+    if (match.color == kBlueTarget) {
+      colorString = "Blue";
+    } else if (match.color == kRedTarget) {
+      colorString = "Red";
+    } else if (match.color == kGreenTarget) {
+      colorString = "Green";
+    } else if (match.color == kYellowTarget) {
+      colorString = "Yellow";
+    } else {
+      colorString = "Unknown";
+    }
+
     System.out.println("Red " + detectedColor.red);
     System.out.println("Green " + detectedColor.green); 
     System.out.println("Blue " + detectedColor.blue); 
     System.out.println("IR " + IR); 
+    System.out.println("Detected Color" + colorString);
 
     int proximity = m_colorSensor.getProximity();
 
