193d6e6bfa4d7270d8017bf60bddf2465f25981b
Ariella <ariella.avigad@gmail.com>
2020-01-16 17:52:22 -0500

    Removed button assignments from robot container

--- 2020InfiniteRecharge/src/main/java/frc/robot/RobotContainer.java
+++ 2020InfiniteRecharge/src/main/java/frc/robot/RobotContainer.java
@@ -41,19 +41,7 @@ public class RobotContainer {
     oi = new OI();
 
     chassis.setDefaultCommand(new DriveByJoystick(chassis, oi));
-    // Configure the button bindings
-    configureButtonBindings();
   }
-
-  /**
-   * Use this method to define your button->command mappings.  Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a
-   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {
-  }
-
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
