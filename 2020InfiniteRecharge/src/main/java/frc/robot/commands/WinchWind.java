efcfe7c086382d4e0660f4c9717d9ed15a1c41ed
Sreyashi Mondal <sreyashi1m@gmail.com>
2020-01-25 14:10:24 -0500

    We added the winch and lift commands.

---
+++ 2020InfiniteRecharge/src/main/java/frc/robot/commands/WinchWind.java
@@ -0,0 +1,53 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Winch;
+
+public class WinchWind extends CommandBase {
+  /**
+   * Creates a new RotationControl.
+   */
+
+  Winch WinchWind;
+  boolean wind;
+
+  public WinchWind(Winch WinchWind, boolean wind) {
+    this.WinchWind = WinchWind;
+    this.wind = wind;
+    super.addRequirements(WinchWind);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if (wind)
+      WinchWind.wind();
+    else
+      WinchWind.unwind();
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    WinchWind.stop();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
