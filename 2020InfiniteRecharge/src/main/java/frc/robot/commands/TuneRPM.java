9aaa94e1030d16a49aea8a0661925da528dfcb3c
Ariella <ariella.avigad@gmail.com>
2020-01-30 20:11:10 -0500

    Added a command that can change the RPM

---
+++ 2020InfiniteRecharge/src/main/java/frc/robot/commands/TuneRPM.java
@@ -0,0 +1,32 @@
+package frc.robot.commands;
+
+public class TuneRPM {
+    /**
+ * This function sets the speed for the RPM
+ * @param setSpeed speed in RPM
+ */
+    public void setRPM(double setSpeed) {
+        return;
+    }
+    /**
+ * This function tests the speed, will make the robot move
+ * @param testSpeed speed in RPM
+ */
+
+    public void testRPM(double testSpeed){
+
+    }
+
+    public TuneRPM() {
+
+    }
+
+    public static void main(String... args) {
+
+        TuneRPM tuneRPM = new TuneRPM();
+        tuneRPM.setRPM(400);
+    }
+}
+
+
+
