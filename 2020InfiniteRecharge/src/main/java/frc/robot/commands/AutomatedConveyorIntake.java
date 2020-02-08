4bc8001f06330f2a6d7c1f15e1784f928625bf92
Meghna Behari <meghnabehari@meghnas-air.wifi.cmu.edu>
2020-02-08 15:12:35 -0500

    conveyor and break sensor integration cont

--- 2020InfiniteRecharge/src/main/java/frc/robot/commands/AutomatedConveyorIntake.java
+++ 2020InfiniteRecharge/src/main/java/frc/robot/commands/AutomatedConveyorIntake.java
@@ -1,8 +1,8 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                                                         */
+/* Open Source Software - may be modified and shared by FRC teams. The code     */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
+/* the project.                                                                                                                             */
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.commands;
@@ -12,24 +12,34 @@ import frc.robot.subsystems.*;
 
 public class AutomatedConveyorIntake extends SequentialCommandGroup {
 
-  private final ShooterIntake m_shooterIntake; 
-  private final ShooterConveyor m_shooterConveyor; 
+    private final ShooterIntake m_shooterIntake; 
+    private final ShooterConveyor m_shooterConveyor; 
 
-  /**
-   * Creates a new AutomatedConveyorIntake.
-   */
-  public AutomatedConveyorIntake(ShooterIntake shooterIntake, ShooterConveyor shooterConveyor) {
+    /**
+     * Creates a new AutomatedConveyorIntake.
+     */
+    public AutomatedConveyorIntake(ShooterIntake shooterIntake, ShooterConveyor shooterConveyor) {
 
-    m_shooterIntake = shooterIntake; 
-    m_shooterConveyor = shooterConveyor; 
+        m_shooterIntake = shooterIntake; 
+        m_shooterConveyor = shooterConveyor; 
+
+        
+        IntakeCells intakeCell = new IntakeCells(m_shooterIntake, true);
+        Conveyor runConveyor = new Conveyor(shooterConveyor, true); 
+
+        //cell intake runs until handoff break sensor is true (a ball has been collected)
+        addCommands(intakeCell.withInterrupt(m_shooterConveyor::getHandoff)); 
+
+        //conveyor belt runs until secondary break sensor is true (collected ball has been positioned at bottom of conveyor)
+        addCommands(runConveyor.withInterrupt(() -> {
+            if (m_shooterConveyor.getSecondary() || m_shooterConveyor.getTop()) {
+                return true; 
+            } else {
+                return false; 
+            }
+        }));
 
     
-    IntakeCells intakeCell = new IntakeCells(m_shooterIntake, true);
-    Conveyor runConveyor = new Conveyor(shooterConveyor, true); 
-
-    addCommands(intakeCell.withInterrupt(m_shooterConveyor::getHandoff));
-    addCommands(runConveyor.withInterrupt(m_shooterConveyor::getSecondary)); 
-  
-    // Use addRequirements() here to declare subsystem dependencies.
-  }
+        // Use addRequirements() here to declare subsystem dependencies.
+    }
 }
--- 2020InfiniteRecharge/src/main/java/frc/robot/subsystems/Camera.java
+++ 2020InfiniteRecharge/src/main/java/frc/robot/subsystems/Camera.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import edu.wpi.cscore.MjpegServer;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -17,6 +18,10 @@ public class Camera extends SubsystemBase {
 	
 	
 	public Camera() {
+
+		if(RobotBase.isSimulation()){
+			return; 
+		}
 		camIntake = new UsbCamera("camIntake", Constants.CAMERA_INTAKE);
 		camIntake.setResolution(320, 240);
 		camIntake.setFPS(10);
@@ -35,16 +40,29 @@ public class Camera extends SubsystemBase {
 	}
 
 	public void switchToCamClimb() {
+
+		if(RobotBase.isSimulation()){
+			return; 
+		}
 		server.setSource(camClimb);
 		System.out.println("Cam Climb!");
 	}
 
 	public void switchToCamIntake() {
+
+		if(RobotBase.isSimulation()){
+			return; 
+		}
+
 		server.setSource(camIntake);
 		System.out.println("Cam Intake!");
 	}
 
 	public void initDefaultCommand() {
+
+		if(RobotBase.isSimulation()){
+			return; 
+		}
 		// Set the default command for a subsystem here.
 		// setDefaultCommand(new MySpecialCommand());
 	}
