037ee7b515b5fae1a1325474299313ea51ec6678
PJ Reiniger <pj.reiniger@gmail.com>
2020-02-12 18:31:41 -0500

    Disabling pigeon, camera. Trying to clean up the rio logs and debug the shooter

--- 2020InfiniteRecharge/src/main/java/frc/robot/subsystems/Camera.java
+++ 2020InfiniteRecharge/src/main/java/frc/robot/subsystems/Camera.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.cscore.VideoSink;
 import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -12,50 +11,55 @@ import frc.robot.Constants;
  */
 public class Camera extends SubsystemBase {
 
-	private UsbCamera camIntake;
-	private UsbCamera camClimb;
-	private VideoSink server;
+    private final boolean m_useCamera;
 
-	public Camera() {
-		if(RobotBase.isSimulation()){
-			return; 
-		}
-		camIntake = new UsbCamera("camIntake", Constants.CAMERA_INTAKE);
-		camIntake.setResolution(320, 240);
-		camIntake.setFPS(20);
-		camClimb = new UsbCamera("camClimb", Constants.CAMERA_CLIMB);
-		camClimb.setResolution(320, 240);
-		camClimb.setFPS(20);
-		server = CameraServer.getInstance().addSwitchedCamera("Driver Cameras");
-		server.setSource(camIntake);
+    private UsbCamera m_camIntake;
+    private UsbCamera m_camClimb;
+    private VideoSink m_server;
 
-		// To see the stream in the Dashboard, add a CameraServer Stream Viewer
-	}
+    public Camera() {
+        // TODO(pj) - Undo when cameras are plugged in. Disabled now to clean up roboRio logs
+        m_useCamera = false;
+        // m_useCamera = !RobotBase.isSimulation();
+        if (m_useCamera) {
+            return; 
+        }
+        m_camIntake = new UsbCamera("camIntake", Constants.CAMERA_INTAKE);
+        m_camIntake.setResolution(320, 240);
+        m_camIntake.setFPS(20);
+        m_camClimb = new UsbCamera("camClimb", Constants.CAMERA_CLIMB);
+        m_camClimb.setResolution(320, 240);
+        m_camClimb.setFPS(20);
+        m_server = CameraServer.getInstance().addSwitchedCamera("Driver Cameras");
+        m_server.setSource(m_camIntake);
 
-	public void switchToCamClimb() {
-		if(RobotBase.isSimulation()){
-			return; 
-		}
-		server.setSource(camClimb);
-		System.out.println("Cam Climb!");
-	}
+        // To see the stream in the Dashboard, add a CameraServer Stream Viewer
+    }
 
-	public void switchToCamIntake() {
-		if(RobotBase.isSimulation()){
-			return; 
-		}
+    public void switchToCamClimb() {
+        if (m_useCamera) {
+            return; 
+        }
+        m_server.setSource(m_camClimb);
+        System.out.println("Cam Climb!");
+    }
 
-		server.setSource(camIntake);
-		System.out.println("Cam Intake!");
-	}
+    public void switchToCamIntake() {
+        if (m_useCamera) {
+            return; 
+        }
 
-	public void initDefaultCommand() {
-		if(RobotBase.isSimulation()){
-			return; 
-		}
-		// Set the default command for a subsystem here.
-		// setDefaultCommand(new MySpecialCommand());
-	}
+        m_server.setSource(m_camIntake);
+        System.out.println("Cam Intake!");
+    }
+
+    public void initDefaultCommand() {
+        if (m_useCamera) {
+            return; 
+        }
+        // Set the default command for a subsystem here.
+        // setDefaultCommand(new MySpecialCommand());
+    }
 
 
 }
