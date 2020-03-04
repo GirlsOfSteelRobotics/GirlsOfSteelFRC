390bb82a4672074b36de2136c43aceb01e23686f
Girls of Steel <girlsofsteelrobotics@gmail.com>
2020-03-03 20:32:17 -0500

    added turntoangleprofiled

--- 2020InfiniteRecharge/src/main/java/frc/robot/commands/autonomous/TurnToAngleProfiled.java
+++ 2020InfiniteRecharge/src/main/java/frc/robot/commands/autonomous/TurnToAngleProfiled.java
@@ -1,9 +1,10 @@
 package frc.robot.commands.autonomous;
 
-import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
+import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.ProfiledPIDCommand;
-
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
 import frc.robot.lib.PropertyManager;
 import frc.robot.subsystems.Chassis;
 
@@ -11,61 +12,38 @@ public class TurnToAngleProfiled extends ProfiledPIDCommand {
 
     private static final PropertyManager.IProperty<Double> AUTO_KP = new PropertyManager.DoubleProperty("TurnToAngleKp", 0.05);
 
-    private final Chassis m_chassis;
-    private final double m_angle;
-    private final double m_allowableError;
+    // private final Chassis m_chassis;
+    // private final double m_angle;
+    // private final double m_allowableError;
 
     private double m_error;
 
     public TurnToAngleProfiled(double targetAngleDegrees, Chassis chassis) {
         super(
-            new ProfiledPIDController(DriveConstants.kTurnP, DriveConstants.kTurnI, DriveConstants.kTurnD, new TrapezoidProfile.Constraints(
-            ChassisSpeeds.kMaxTurnRateDegPerS,
-            ChassisSpeeds.kMaxTurnAccelerationDegPerSSquared)),
-        // Close loop on heading
-        drive::getHeading,
-        // Set reference to target
-        targetAngleDegrees,
-        // Pipe output to turn robot
-        (output, setpoint) -> drive.arcadeDrive(0, output),
-        // Require the drive
-        drive);
-       
-       
-       
+            new ProfiledPIDController(Constants.kTurnP, Constants.kTurnI,
+            Constants.kTurnD, new TrapezoidProfile.Constraints(
+                Constants.kMaxTurnRateDegPerS,
+                Constants.kMaxTurnAccelerationDegPerSSquared)),
+            // Close loop on heading
+            chassis::getHeading,
+            // Set reference to target
+            targetAngleDegrees,
+            // Pipe output to turn robot
+            (output, setpoint) -> chassis.setSteer(output),
+            // Require the drive
+            chassis);
     
-        addRequirements(chassis);
+        // Set the controller to be continuous (because it is an angle controller)
+        getController().enableContinuousInput(-180, 180);
+        // Set the controller tolerance - the delta tolerance ensures the robot is stationary at the
+        // setpoint before it is considered as having reached the reference
+        getController()
+            .setTolerance(Constants.kTurnToleranceDeg, Constants.kTurnRateToleranceDegPerS);
+      }
+    
+      @Override
+      public boolean isFinished() {
+        // End when the controller is at the reference.
+        return getController().atGoal();
+      }
     }
-
-    @Override
-    public void initialize(){
-    }
-
-    @Override
-    public void execute() {
-        double currentAngle = m_chassis.getHeading();
-        m_error = m_angle - currentAngle;
-
-        double turnSpeed = m_error * AUTO_KP.getValue();
-        m_chassis.setSpeedAndSteer(0, turnSpeed);
-
-        //System.out.println("error:" + m_error + "speed:" + speed);
-    }
-
-    @Override
-    public boolean isFinished() {
-        if (Math.abs(m_error) < m_allowableError) {
-            System.out.println("Done!");
-            return true;
-        }
-        else {
-            System.out.println("Turn to angle" + "error:" + m_error + "allowableError" + m_allowableError);
-            return false;
-        }
-    }
-
-    @Override
-    public void end(boolean interrupted) {
-        m_chassis.setSpeed(0);
-    }
-}
