5276c30eb3eefd9f1df37b2930f5417858f3472f
ashley-wei <72471243+ashley-wei@users.noreply.github.com>
2021-08-29 16:50:37 -0400

    Limelight shooter pid (#45)
    * merge conflict resolve
    
    * made hood angle correlate with distance using limelight
    
    * made hood angle correlate with distance using limelight

---
+++ OutreachBot/src/main/java/frc/robot/commands/LimelightHoodControlCommand.java
@@ -0,0 +1,51 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.util.Units;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.LimelightSubsystem;
+import frc.robot.subsystems.Shooter;
+
+
+public class LimelightHoodControlCommand extends CommandBase {
+    private final LimelightSubsystem m_limelightSubsystem;
+    private final Shooter m_shooter;
+    private static final double HOOD_MAX = 85;
+    private static final double HOOD_MIN = 5;
+    private static final double ROBOT_DISTANCE_MAX = Units.feetToMeters(27);
+    private static final double ROBOT_DISTANCE_MIN = Units.feetToMeters(0);
+
+    public LimelightHoodControlCommand(LimelightSubsystem limelightSubsystem, Shooter shooter) {
+        this.m_limelightSubsystem = limelightSubsystem;
+        this.m_shooter = shooter;
+        // each subsystem used by the command must be passed into the addRequirements() method (which takes a vararg of Subsystem)
+        addRequirements(this.m_limelightSubsystem, this.m_shooter);
+    }
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void execute() {
+        double distanceFromGoal = m_limelightSubsystem.limelightDistance();
+        if (distanceFromGoal >= ROBOT_DISTANCE_MAX) {
+            m_shooter.setHoodAngle(HOOD_MAX);
+        } else if (distanceFromGoal <= ROBOT_DISTANCE_MIN) {
+            m_shooter.setHoodAngle(HOOD_MIN);
+        } else if (m_limelightSubsystem.targetExists()) {
+            double hoodDifference = HOOD_MAX - HOOD_MIN;
+            double distanceDifference = ROBOT_DISTANCE_MAX - ROBOT_DISTANCE_MIN;
+            m_shooter.setHoodAngle((hoodDifference / distanceDifference) * distanceFromGoal + HOOD_MIN);
+        }
+        //doesn't do anything if it doesn't see the limelight
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+    }
+}
