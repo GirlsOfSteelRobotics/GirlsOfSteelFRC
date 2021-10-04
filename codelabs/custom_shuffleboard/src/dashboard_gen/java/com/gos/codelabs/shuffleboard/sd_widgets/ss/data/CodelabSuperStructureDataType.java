2e6a5d3f0d8935c01031f9ccf853aeacea0771b3
PJ Reiniger <pj.reiniger@gmail.com>
2021-10-04 00:56:39 -0400

    Fixing up some other packages

---
+++ codelabs/custom_shuffleboard/src/dashboard_gen/java/com/gos/codelabs/shuffleboard/sd_widgets/ss/data/CodelabSuperStructureDataType.java
@@ -0,0 +1,24 @@
+package com.gos.codelabs.shuffleboard.sd_widgets.ss.data;
+
+import com.gos.codelabs.shuffleboard.sd_widgets.SmartDashboardNames;
+import edu.wpi.first.shuffleboard.api.data.ComplexDataType;
+
+import java.util.Map;
+import java.util.function.Function;
+
+public class CodelabSuperStructureDataType  extends ComplexDataType<CodelabSuperStructureData> {
+
+    public CodelabSuperStructureDataType() {
+        super(SmartDashboardNames.SUPER_STRUCTURE_TABLE_NAME, CodelabSuperStructureData.class);
+    }
+
+    @Override
+    public Function<Map<String, Object>, CodelabSuperStructureData> fromMap() {
+        return CodelabSuperStructureData::new;
+    }
+
+    @Override
+    public CodelabSuperStructureData getDefaultValue() {
+        return new CodelabSuperStructureData();
+    }
+}
---
+++ y2020/Testboard/src/main/java/com/gos/testboard2020/commands/OuterShootAlign2.java
@@ -0,0 +1,38 @@
+package com.gos.testboard2020.commands;
+
+import com.gos.testboard2020.subsystems.Limelight;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class OuterShootAlign2 extends CommandBase {
+
+    private final Limelight m_limelight;
+    //private final Chassis m_chassis;
+
+    public OuterShootAlign2(Limelight limelight) {
+        this.m_limelight = limelight;
+        //this.m_chassis = chassis;
+
+        addRequirements(limelight);
+
+    }
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void execute() {
+        m_limelight.getDriveCommand(m_limelight.estimateDistance());
+        //m_limelight.getSteerCommand();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+}
