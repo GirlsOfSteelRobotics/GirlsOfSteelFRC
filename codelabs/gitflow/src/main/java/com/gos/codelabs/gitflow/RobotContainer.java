5ddcf3efae8776c06bdb05e7c6705d0f9f561864
AshleyWei7106 <weiashley.gos@gmail.com>
2021-10-12 19:26:43 -0400

    first codelab 2021

--- codelabs/gitflow/src/main/java/com/gos/codelabs/gitflow/RobotContainer.java
+++ codelabs/gitflow/src/main/java/com/gos/codelabs/gitflow/RobotContainer.java
@@ -1,6 +1,7 @@
 package com.gos.codelabs.gitflow;
 
 import com.gos.codelabs.gitflow.commands.TeleopDriveCommand;
+import com.gos.codelabs.gitflow.subsystems.AshleyCodelab2021Part1SubsystemsSubsystem;
 import com.gos.codelabs.gitflow.subsystems.ChassisSubsystem;
 import com.gos.codelabs.gitflow.subsystems.JacksonCodelab2020Part1Part1;
 import edu.wpi.first.wpilibj.GenericHID;
@@ -27,6 +28,7 @@ public class RobotContainer {
     private PJCodelab2020Part1Subsystem m_pjCodelab2020Part1;
     private PJCodelab2020Part2Subsystem m_pjCodelab2020Part2;
     private JacksonCodelab2020Part1Part1 m_jacksonCodeLab2020Part1;
+    private AshleyCodelab2021Part1SubsystemsSubsystem m_ashley2021CodelabPart1;
 
     ///////////////////////////////////////
     // Don't touch things below here
