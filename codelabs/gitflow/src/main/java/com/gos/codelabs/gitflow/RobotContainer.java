3f5299d3affab26ae52faa9b2acba19038b1b949
ggoslin <68612840+ggoslin@users.noreply.github.com>
2021-10-26 18:59:35 -0400

    Grace's 2021 GitFlow CodeLab (#17)
    * Grace's 2021 GitFlow CodeLab
    
    * Grace's 2021 GitFlow CodeLab variable change

--- codelabs/gitflow/src/main/java/com/gos/codelabs/gitflow/RobotContainer.java
+++ codelabs/gitflow/src/main/java/com/gos/codelabs/gitflow/RobotContainer.java
@@ -2,6 +2,7 @@ package com.gos.codelabs.gitflow;
 
 import com.gos.codelabs.gitflow.commands.TeleopDriveCommand;
 import com.gos.codelabs.gitflow.subsystems.ChassisSubsystem;
+import com.gos.codelabs.gitflow.subsystems.GraceGCodeLab2020Subsystem;
 import com.gos.codelabs.gitflow.subsystems.JacksonCodelab2020Part1Part1;
 import com.gos.codelabs.gitflow.subsystems.PJCodelab2021Part1Subsystem;
 import edu.wpi.first.wpilibj.GenericHID;
@@ -29,6 +30,7 @@ public class RobotContainer {
     private PJCodelab2020Part2Subsystem m_pjCodelab2020Part2;
     private JacksonCodelab2020Part1Part1 m_jacksonCodeLab2020Part1;
     private PJCodelab2021Part1Subsystem m_pjCodelab2021Part1;
+    private GraceGCodeLab2020Subsystem m_gracecodeLab2020pt1;
 
     ///////////////////////////////////////
     // Don't touch things below here
---
+++ codelabs/gitflow/src/main/java/com/gos/codelabs/gitflow/subsystems/GraceGCodeLab2020Subsystem.java
@@ -0,0 +1,12 @@
+package com.gos.codelabs.gitflow.subsystems;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class GraceGCodeLab2020Subsystem extends SubsystemBase {
+    public GraceGCodeLab2020Subsystem() {
+        System.out.println("Grace Goslin says HELLO WORLD!!!!!");
+
+    }
+}
+
