2c398bd1946a76a807d4f15a9023940d4a9507a8
ahulver <91995507+ahulver@users.noreply.github.com>
2021-10-26 20:04:02 -0400

    created codelab part 1 (#23)

--- codelabs/gitflow/src/main/java/com/gos/codelabs/gitflow/RobotContainer.java
+++ codelabs/gitflow/src/main/java/com/gos/codelabs/gitflow/RobotContainer.java
@@ -1,6 +1,7 @@
 package com.gos.codelabs.gitflow;
 
 import com.gos.codelabs.gitflow.commands.TeleopDriveCommand;
+import com.gos.codelabs.gitflow.subsystems.AmandaCodelab2021Part1;
 import com.gos.codelabs.gitflow.subsystems.AshleyCodelab2021Part1SubsystemsSubsystem;
 import com.gos.codelabs.gitflow.subsystems.ChassisSubsystem;
 import com.gos.codelabs.gitflow.subsystems.GraceGCodeLab2020Subsystem;
@@ -34,6 +35,7 @@ public class RobotContainer {
     private AshleyCodelab2021Part1SubsystemsSubsystem m_ashley2021CodelabPart1;
     private PJCodelab2021Part1Subsystem m_pjCodelab2021Part1;
     private GraceGCodeLab2020Subsystem m_gracecodeLab2020pt1;
+    private AmandaCodelab2021Part1 m_amandaCodeLab;
     private JustinaCodelab2021Part1Subsystem m_justinaCodeLab2021Part1;
 
 
