4961b42028a387d7267b380a98a6ad74b157ae75
jskim671 <81633973+jskim671@users.noreply.github.com>
2021-10-26 20:29:55 -0400

    change (#25)
    Co-authored-by: jskim671 <janiseskim@gmail.com>

--- codelabs/gitflow/src/main/java/com/gos/codelabs/gitflow/RobotContainer.java
+++ codelabs/gitflow/src/main/java/com/gos/codelabs/gitflow/RobotContainer.java
@@ -7,6 +7,7 @@ import com.gos.codelabs.gitflow.subsystems.AshleyCodelab2021Part1SubsystemsSubsy
 import com.gos.codelabs.gitflow.subsystems.ChassisSubsystem;
 import com.gos.codelabs.gitflow.subsystems.GraceGCodeLab2020Subsystem;
 import com.gos.codelabs.gitflow.subsystems.JacksonCodelab2020Part1Part1;
+import com.gos.codelabs.gitflow.subsystems.JaniseCodelab2021Pt1Subsystem;
 import com.gos.codelabs.gitflow.subsystems.JustinaCodelab2021Part1Subsystem;
 import com.gos.codelabs.gitflow.subsystems.PJCodelab2021Part1Subsystem;
 import edu.wpi.first.wpilibj.GenericHID;
@@ -39,6 +40,8 @@ public class RobotContainer {
     private AriellaCodelab2021Part1Subsystem m_ariellaCodelab2021Part1;
     private AmandaCodelab2021Part1 m_amandaCodeLab;
     private JustinaCodelab2021Part1Subsystem m_justinaCodeLab2021Part1;
+
+    private JaniseCodelab2021Pt1Subsystem m_janiseCodelab2021Pt1;
 
     ///////////////////////////////////////
     // Don't touch things below here
