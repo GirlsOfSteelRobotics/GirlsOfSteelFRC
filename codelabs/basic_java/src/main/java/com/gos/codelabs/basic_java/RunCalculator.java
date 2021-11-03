5316d67445cb2c335bac84803d7b1e782d11ae59
justina <jwang3@nastudents.org>
2021-11-02 20:08:48 -0400

    Justina code lab

--- codelabs/basic_java/src/main/java/com/gos/codelabs/basic_java/RunCalculator.java
+++ codelabs/basic_java/src/main/java/com/gos/codelabs/basic_java/RunCalculator.java
@@ -8,8 +8,17 @@ public class RunCalculator {
         System.out.println("Input a number: ");
         // creates a scanner that waits and processes keyboard input
         Scanner input = new Scanner(System.in);
+        Calculator myCalculator = new Calculator();
+
         double number = input.nextDouble();
-        System.out.println("The number typed was " + number);
+        System.out.println("Jaccording Jo Jenius Justina Java Jalculator, " +
+            "Jhe Jumber Jyped Jin Jas " + number);
+        System.out.println(myCalculator.add(5, 6));
+        System.out.println(myCalculator.subtract(5, 6));
+        System.out.println(myCalculator.multiply(5, 6));
+        System.out.println(myCalculator.divide(5, 6));
+        System.out.println("Hello World!");
+
         input.close();
         // TODO Task 1: Ask for 2 numbers and output the sum using a myCalculator
         // TODO Bonus Task 2: Ask which operation the user would like to use (use input.nextLine())
