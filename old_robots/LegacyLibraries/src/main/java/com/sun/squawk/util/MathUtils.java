f6716ac28d081157af1d2861608a776dfd6d24c8
PJ Reiniger <pj.reiniger@gmail.com>
2021-12-03 21:25:06 -0500

    [monorepo] Adding gradle build files for old robots (#43)

---
+++ old_robots/LegacyLibraries/src/main/java/com/sun/squawk/util/MathUtils.java
@@ -0,0 +1,19 @@
+package com.sun.squawk.util;
+
+public class MathUtils {
+    public static double atan(double a) {
+        return Math.atan(a);
+    }
+
+    public static double round(double a) {
+        return Math.round(a);
+    }
+
+    public static double atan2(double y, double x) {
+        return Math.atan2(y, x);
+    }
+
+    public static double acos(double a) {
+        return Math.acos(a);
+    }
+}
