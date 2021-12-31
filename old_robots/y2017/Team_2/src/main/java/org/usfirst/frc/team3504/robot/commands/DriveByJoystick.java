359202f3e7766d12f1cd8c5d809e8718c97b4dac
PJ Reiniger <pj.reiniger@gmail.com>
2021-12-31 13:31:18 -0500

    2017 robot 3 directory

---
+++ old_robots/y2017/Team_2/src/main/java/org/usfirst/frc/team3504/robot/commands/DriveByJoystick.java
@@ -0,0 +1,53 @@
+package org.usfirst.frc.team3504.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import org.usfirst.frc.team3504.robot.OI;
+import org.usfirst.frc.team3504.robot.subsystems.Chassis;
+
+/**
+ *
+ */
+public class DriveByJoystick extends Command {
+
+    private final OI m_oi;
+    private final Chassis m_chassis;
+
+    public DriveByJoystick(OI oi, Chassis chassis) {
+        m_chassis = chassis;
+        m_oi = oi;
+        requires(m_chassis);
+    }
+
+    // Called just before this Command runs the first time
+    @Override
+    protected void initialize() {
+        SmartDashboard.putBoolean("Drive by Joystick", true);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    protected void execute() {
+        m_chassis.driveByJoystick(m_oi.getStick());
+        //Robot.chassis.printEncoderValues();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    @Override
+    protected void end() {
+        m_chassis.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    @Override
+    protected void interrupted() {
+        end();
+    }
+}
