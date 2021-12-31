5e3d0e97d868850020f3673d04bef58b6f29757b
PJ Reiniger <pj.reiniger@gmail.com>
2021-12-31 13:18:10 -0500

    Moving 2017 robot 1

---
+++ old_robots/y2017/Secondary_Human_Training_Golem/src/main/java/org/usfirst/frc/team3504/robot/commands/autonomous/AutoRedHopper.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team3504.robot.commands.autonomous;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+@SuppressWarnings("PMD")
+public class AutoRedHopper extends CommandGroup {
+
+    public AutoRedHopper() {
+        // addSequential(new AutoDriveForward(156, .6)); //TODO: change //cross
+        // baseline
+        //// addSequential(new AutoTurn(21, 0.1)); //TODO: change
+        // addSequential(new AutoDriveForward(156, .6)); //TODO: change //hit
+        // hopper 1
+        // addSequential(new AutoDriveBackward(156, .6)); //TODO: change
+        //// addSequential(new AutoTurn(156, .6)); //TODO: change
+        // addSequential(new AutoDriveForward(156, .6)); //TODO: change
+        // // addSequential(new AutoTurn(156, .6)); //TODO: change
+        // addSequential(new AutoDriveForward(156, .6)); //TODO: change //hit
+        // hopper 2
+        // addSequential(new AutoDriveBackward(156, .6)); //TODO: change //hit
+        // hopper 2
+    }
+}
