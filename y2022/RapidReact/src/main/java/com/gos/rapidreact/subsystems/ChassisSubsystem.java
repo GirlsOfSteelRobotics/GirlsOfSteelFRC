bcd757ff464f8ff37e824c3e597ca391af43cec0
ashley-wei <weiashley.gos@gmail.com>
2022-01-22 13:25:04 -0500

    started odometry

--- y2022/RapidReact/src/main/java/com/gos/rapidreact/subsystems/ChassisSubsystem.java
+++ y2022/RapidReact/src/main/java/com/gos/rapidreact/subsystems/ChassisSubsystem.java
@@ -1,10 +1,15 @@
 package com.gos.rapidreact.subsystems;
 
 
+import com.gos.lib.ctre.PigeonGyro;
 import com.gos.rapidreact.Constants;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SimableCANSparkMax;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -18,6 +23,14 @@ public class ChassisSubsystem extends SubsystemBase {
 
     private final DifferentialDrive m_drive;
 
+    //odometry
+    private final DifferentialDriveOdometry m_odometry;
+    private final PigeonGyro m_gyro;
+    private final RelativeEncoder m_rightEncoder;
+    private final RelativeEncoder m_leftEncoder;
+
+
+
     public ChassisSubsystem() {
         m_leaderLeft = new SimableCANSparkMax(Constants.DRIVE_LEFT_LEADER_SPARK, CANSparkMaxLowLevel.MotorType.kBrushless);
         m_followerLeft = new SimableCANSparkMax(Constants.DRIVE_LEFT_FOLLOWER_SPARK, CANSparkMaxLowLevel.MotorType.kBrushless);
@@ -25,6 +38,13 @@ public class ChassisSubsystem extends SubsystemBase {
         m_followerRight = new SimableCANSparkMax(Constants.DRIVE_RIGHT_FOLLOWER_SPARK, CANSparkMaxLowLevel.MotorType.kBrushless);
 
         m_drive = new DifferentialDrive(m_leaderLeft, m_leaderRight);
+
+        m_rightEncoder = m_leaderRight.getEncoder();
+        m_leftEncoder = m_leaderLeft.getEncoder();
+
+        m_gyro = new PigeonGyro(Constants.PIGEON_PORT);
+
+        m_odometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(0));
 
         CANSparkMax.IdleMode idleMode = CANSparkMax.IdleMode.kCoast;
         m_leaderLeft.setIdleMode(idleMode);
@@ -37,6 +57,16 @@ public class ChassisSubsystem extends SubsystemBase {
 
         m_followerLeft.follow(m_leaderLeft, false);
         m_followerRight.follow(m_leaderRight, false);
+
+    }
+
+
+
+    public void resetInitialOdometry(Pose2d pose) {
+        m_leftEncoder.setPosition(0);
+        m_rightEncoder.setPosition(0);
+        m_odometry.resetPosition(pose, Rotation2d.fromDegrees(getHeading()));
+        m_simulator.resetOdometry(pose);
 
     }
 
