7f772ec11b45703b6e38d3b66c400dc0e7b04a62
ktai <lerantai777@gmail.com>
2022-02-05 15:01:32 -0500

    created super structure for rapid react created model

---
+++ y2022/RapidReactDashboard/src/main/java/com/gos/rapidreact/shuffleboard/super_structure/SuperStructureController.java
@@ -0,0 +1,49 @@
+package com.gos.rapidreact.shuffleboard.super_structure;
+
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.DoubleBinding;
+import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.layout.Pane;
+import javafx.scene.transform.Scale;
+
+public class SuperStructureController {
+
+    private static final double MAX_WIDTH = 1; // TODO figure out real value
+    private static final double MAX_HEIGHT = 1; // TODO figure out real value
+
+    @FXML
+    private Group m_group;
+
+    @FXML
+    private Pane m_pane;
+
+    @FXML
+    public void initialize() {
+
+        ///////////////////////////////////////////////////////////
+        // Controls the inches <-> pixels conversion. Don't touch
+        double minWidthMultiplier = 1;
+        m_pane.setMinHeight(MAX_HEIGHT * minWidthMultiplier);
+        m_pane.setMinWidth(MAX_WIDTH * minWidthMultiplier);
+
+        DoubleBinding scaleBinding = Bindings.createDoubleBinding(() -> {
+            double output = Math.min(m_pane.getWidth() / MAX_WIDTH, m_pane.getHeight() / MAX_HEIGHT);
+            return output;
+        }, m_pane.widthProperty(), m_pane.heightProperty());
+
+        Scale scale = new Scale();
+        scale.xProperty().bind(scaleBinding);
+        scale.yProperty().bind(scaleBinding);
+
+        m_group.getTransforms().add(scale);
+        ///////////////////////////////////////////////////////////
+    }
+
+
+    public void updateSuperStructure(SuperStructureData superStructureData) {
+        // TODO implement
+    }
+
+
+}
