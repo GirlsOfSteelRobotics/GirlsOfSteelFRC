16d8d8e2e34acd66b2de39ffa0e984782e75f019
ashley-wei <72471243+ashley-wei@users.noreply.github.com>
2022-02-07 18:42:55 -0500

    create shooter (#89)
    * Created a shooter subsystem and motor speed command.
    
    * added pid to shooter
    
    * fixed github comments for pid for shooter
    
    * fixed github comments for pid for shooter
    
    * fixed github comments for pid for shooter
    
    Co-authored-by: Grace <gosling.gos@gmail.com>

---
+++ y2022/RapidReact/src/main/java/com/gos/rapidreact/commands/ShooterRpmPIDCommand.java
@@ -0,0 +1,38 @@
+package com.gos.rapidreact.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import com.gos.rapidreact.subsystems.ShooterSubsystem;
+
+
+public class ShooterRpmPIDCommand extends CommandBase {
+    private final ShooterSubsystem m_shooter;
+    private final double m_goalRPM;
+
+    public ShooterRpmPIDCommand(ShooterSubsystem shooterSubsystem, double goalRPM) {
+        this.m_shooter = shooterSubsystem;
+        m_goalRPM = goalRPM;
+
+        addRequirements(this.m_shooter);
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+        m_shooter.setShooterRpmPIDSpeed(m_goalRPM);
+    }
+
+    @Override
+    public boolean isFinished() {
+        double error = Math.abs(m_goalRPM - m_shooter.getEncoderVelocity());
+        return error < ShooterSubsystem.ALLOWABLE_ERROR;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+}
