e14e9dcbda1d126628171072d648a13a18966992
ashley-wei <weiashley.gos@gmail.com>
2022-02-08 20:26:22 -0500

    fixed limelight subsystem

--- y2022/RapidReact/src/main/java/com/gos/rapidreact/subsystems/IntakeLimelightSubsystem.java
+++ y2022/RapidReact/src/main/java/com/gos/rapidreact/subsystems/IntakeLimelightSubsystem.java
@@ -7,20 +7,20 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import java.sql.Driver;
-
 public class IntakeLimelightSubsystem extends SubsystemBase {
     public static final double MOUNTING_ANGLE = 0;
     public static final double LIMELIGHT_HEIGHT = Units.inchesToMeters(35);
     public static final double BLUE_CARGO = 0;
     public static final double RED_CARGO = 1;
-    private final NetworkTableEntry m_angleToCargo;
+    private final NetworkTableEntry m_horizontalAngle;
+    private final NetworkTableEntry m_verticalAngle;
     private final NetworkTableEntry m_isVisible;
     private final NetworkTableEntry m_pipeline; //which camera (color or cargo) to use
     private final DriverStation.Alliance m_alliance;
 
     public IntakeLimelightSubsystem() {
-        m_angleToCargo = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx");
+        m_horizontalAngle = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx");
+        m_verticalAngle = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty");
         m_isVisible = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv");
         m_pipeline = NetworkTableInstance.getDefault().getTable("limelight").getEntry("pipeline");
         m_alliance = DriverStation.getAlliance();
@@ -28,12 +28,12 @@ public class IntakeLimelightSubsystem extends SubsystemBase {
 
     public double distanceToCargo() {
         double distance;
-        distance = (LIMELIGHT_HEIGHT) / Math.tan(MOUNTING_ANGLE + m_angleToCargo.getDouble(0));
+        distance = (LIMELIGHT_HEIGHT) / Math.tan(MOUNTING_ANGLE + m_verticalAngle.getDouble(0));
         return distance;
     }
 
     public double getAngle() {
-        return m_angleToCargo.getDouble(0);
+        return m_horizontalAngle.getDouble(0);
     }
 
     public boolean isVisible() {
