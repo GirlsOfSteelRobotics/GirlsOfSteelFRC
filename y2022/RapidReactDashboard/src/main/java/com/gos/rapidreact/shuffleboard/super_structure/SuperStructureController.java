2c0b95527f1f20f158c3a48ef15c72a94c7f4d8c
ktai <lerantai777@gmail.com>
2022-02-08 20:28:59 -0500

    created and set shapes/constants for all robot parts in super structure colored shapes/robot components in super structure - when motors move, colors change

--- y2022/RapidReactDashboard/src/main/java/com/gos/rapidreact/shuffleboard/super_structure/SuperStructureController.java
+++ y2022/RapidReactDashboard/src/main/java/com/gos/rapidreact/shuffleboard/super_structure/SuperStructureController.java
@@ -1,22 +1,77 @@
 package com.gos.rapidreact.shuffleboard.super_structure;
 
+import com.gos.rapidreact.shuffleboard.Utils;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.DoubleBinding;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.transform.Scale;
 
 public class SuperStructureController {
 
-    private static final double MAX_WIDTH = 1; // TODO figure out real value
-    private static final double MAX_HEIGHT = 1; // TODO figure out real value
+    private static final double MAX_WIDTH = 36;
+    private static final double MAX_HEIGHT = 44;
+    private static final double CHASSIS_HEIGHT = 4;
+    private static final double CHASSIS_WIDTH = 36;
+    private static final double CHASSIS_X = 0;
+    private static final double CHASSIS_Y = 40;
+    private static final double INTAKE_WIDTH = 10;
+    private static final double INTAKE_HEIGHT = 16;
+    private static final double INTAKE_X = 26;
+    private static final double INTAKE_Y = 20;
+    private static final double INTAKE_START_ANGLE = 0;
+    private static final double INTAKE_WHEEL_WIDTH = 10;
+    private static final double INTAKE_WHEEL_HEIGHT = 4;
+    private static final double INTAKE_WHEEL_X = 26;
+    private static final double INTAKE_WHEEL_Y = 20;
+    private static final double HORI_CONVEYOR_WIDTH = 36;
+    private static final double HORI_CONVEYOR_HEIGHT = 4;
+    private static final double HORI_CONVEYOR_X = 0;
+    private static final double HORI_CONVEYOR_Y = 36;
+    private static final double VERT_CONVEYOR_WIDTH = 2;
+    private static final double VERT_CONVEYOR_HEIGHT = 30;
+    private static final double VERT_CONVEYOR_X = 2;
+    private static final double VERT_CONVEYOR_Y = 6;
+    private static final double HANGER_WIDTH = 2;
+    private static final double HANGER_HEIGHT = 32;
+    private static final double HANGER_X = 10;
+    private static final double HANGER_Y = 4;
+    private static final double SHOOTER_WIDTH = 8;
+    private static final double SHOOTER_HEIGHT = 6;
+    private static final double SHOOTER_X = 2;
+    private static final double SHOOTER_Y = 0;
+
+
 
     @FXML
     private Group m_group;
 
     @FXML
     private Pane m_pane;
+
+    @FXML
+    private Rectangle m_chassis;
+
+    @FXML
+    private Rectangle m_intake;
+
+    @FXML
+    private Rectangle m_intakeWheel;
+
+    @FXML
+    private Rectangle m_horizontalConveyor;
+
+    @FXML
+    private Rectangle m_verticalConveyor;
+
+    @FXML
+    private Rectangle m_hanger;
+
+    @FXML
+    private Rectangle m_shooter;
+
 
     @FXML
     public void initialize() {
@@ -38,11 +93,53 @@ public class SuperStructureController {
 
         m_group.getTransforms().add(scale);
         ///////////////////////////////////////////////////////////
+
+        m_chassis.setX(CHASSIS_X);
+        m_chassis.setY(CHASSIS_Y);
+        m_chassis.setHeight(CHASSIS_HEIGHT);
+        m_chassis.setWidth(CHASSIS_WIDTH);
+
+        m_intake.setX(INTAKE_X);
+        m_intake.setY(INTAKE_Y);
+        m_intake.setHeight(INTAKE_HEIGHT);
+        m_intake.setWidth(INTAKE_WIDTH);
+
+        m_intakeWheel.setX(INTAKE_WHEEL_X);
+        m_intakeWheel.setY(INTAKE_WHEEL_Y);
+        m_intakeWheel.setHeight(INTAKE_WHEEL_HEIGHT);
+        m_intakeWheel.setWidth(INTAKE_WHEEL_WIDTH);
+
+        m_horizontalConveyor.setX(HORI_CONVEYOR_X);
+        m_horizontalConveyor.setY(HORI_CONVEYOR_Y);
+        m_horizontalConveyor.setHeight(HORI_CONVEYOR_HEIGHT);
+        m_horizontalConveyor.setWidth(HORI_CONVEYOR_WIDTH);
+
+        m_verticalConveyor.setX(VERT_CONVEYOR_X);
+        m_verticalConveyor.setY(VERT_CONVEYOR_Y);
+        m_verticalConveyor.setHeight(VERT_CONVEYOR_HEIGHT);
+        m_verticalConveyor.setWidth(VERT_CONVEYOR_WIDTH);
+
+        m_hanger.setX(HANGER_X);
+        m_hanger.setY(HANGER_Y);
+        m_hanger.setHeight(HANGER_HEIGHT);
+        m_hanger.setWidth(HANGER_WIDTH);
+
+        m_shooter.setX(SHOOTER_X);
+        m_shooter.setY(SHOOTER_Y);
+        m_shooter.setHeight(SHOOTER_HEIGHT);
+        m_shooter.setWidth(SHOOTER_WIDTH);
+
     }
 
 
     public void updateSuperStructure(SuperStructureData superStructureData) {
-        // TODO implement
+
+        m_intakeWheel.setFill(Utils.getMotorColor(superStructureData.getIntakeSpeed()));
+        m_hanger.setFill(Utils.getMotorColor(superStructureData.getHangerSpeed()));
+        m_shooter.setFill(Utils.getMotorColor(superStructureData.getShooterSpeed()));
+        m_horizontalConveyor.setFill(Utils.getMotorColor(superStructureData.getHorizontalConveyorSpeed()));
+        m_verticalConveyor.setFill(Utils.getMotorColor(superStructureData.getVerticalConveyorSpeed()));
+
     }
 
 
