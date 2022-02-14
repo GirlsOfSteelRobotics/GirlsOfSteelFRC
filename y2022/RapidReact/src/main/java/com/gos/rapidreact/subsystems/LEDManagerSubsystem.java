63b19c4acf58f05505db9062fdd7a7101e747c29
ashley-wei <weiashley.gos@gmail.com>
2022-02-14 18:30:25 -0500

    added to led lights

--- y2022/RapidReact/src/main/java/com/gos/rapidreact/subsystems/LEDManagerSubsystem.java
+++ y2022/RapidReact/src/main/java/com/gos/rapidreact/subsystems/LEDManagerSubsystem.java
@@ -5,6 +5,7 @@ import com.gos.rapidreact.LED.LEDFlash;
 import com.gos.rapidreact.LED.LEDMovingPixel;
 import com.gos.rapidreact.LED.LEDPolkaDots;
 import com.gos.rapidreact.LED.LEDRainbow;
+import com.gos.rapidreact.LED.LEDBoolean;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.util.Color;
@@ -20,6 +21,8 @@ public class LEDManagerSubsystem extends SubsystemBase {
     private LEDRainbow m_ledRainbow;
     private LEDPolkaDots m_ledPolkaDots;
     private LEDMovingPixel m_movingPixel;
+    private LEDBoolean m_boolean;
+
 
     public LEDManagerSubsystem() {
         m_led = new AddressableLED(PORT);
@@ -29,7 +32,7 @@ public class LEDManagerSubsystem extends SubsystemBase {
         m_ledRainbow = new LEDRainbow(MAX_INDEX_LED - 2, m_buffer, 3);
         m_ledPolkaDots = new LEDPolkaDots(m_buffer, 0, MAX_INDEX_LED);
         m_movingPixel = new LEDMovingPixel(m_buffer, 0, MAX_INDEX_LED, Color.kPurple);
-
+        m_boolean = new LEDBoolean(m_buffer, 0, MAX_INDEX_LED / 6, Color.kDarkGreen, Color.kBlack);
 
         m_led.setLength(m_buffer.getLength());
 
@@ -53,6 +56,8 @@ public class LEDManagerSubsystem extends SubsystemBase {
         // m_ledFlash.flash();
         // m_ledRainbow.rainbow();
         //m_movingPixel.movingPixel();
+        //m_boolean.checkBoolean(false);
+
         m_led.setData(m_buffer);
     }
 
