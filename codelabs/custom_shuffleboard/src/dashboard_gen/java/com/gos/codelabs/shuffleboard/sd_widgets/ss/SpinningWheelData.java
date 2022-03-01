884c5b7ed6c87c786240da4abacbb5b0ec01bb14
PJ Reiniger <pj.reiniger@gmail.com>
2022-02-28 22:31:11 -0500

    [monorepo] CI, dashboard gen updates, building this year with bazel (#129)

--- codelabs/custom_shuffleboard/src/dashboard_gen/java/com/gos/codelabs/shuffleboard/sd_widgets/ss/SpinningWheelData.java
+++ codelabs/custom_shuffleboard/src/dashboard_gen/java/com/gos/codelabs/shuffleboard/sd_widgets/ss/SpinningWheelData.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.StringJoiner;
 
-@SuppressWarnings("PMD.DataClass")
+@SuppressWarnings({"PMD.DataClass", "PMD.ExcessiveParameterList"})
 public class SpinningWheelData extends ComplexData<SpinningWheelData> {
 
     private final double m_speed;
