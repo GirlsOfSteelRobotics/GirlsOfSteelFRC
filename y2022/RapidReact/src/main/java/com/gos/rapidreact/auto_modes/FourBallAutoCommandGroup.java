1fa1b01710037904a0caaa87e7d49abce60b9032
Girls of Steel <girlsofsteelrobotics@gmail.com>
2022-03-12 14:35:02 -0500

    4 ball

--- y2022/RapidReact/src/main/java/com/gos/rapidreact/auto_modes/FourBallAutoCommandGroup.java
+++ y2022/RapidReact/src/main/java/com/gos/rapidreact/auto_modes/FourBallAutoCommandGroup.java
@@ -10,28 +10,42 @@ import com.gos.rapidreact.subsystems.CollectorSubsystem;
 import com.gos.rapidreact.subsystems.HorizontalConveyorSubsystem;
 import com.gos.rapidreact.subsystems.ShooterSubsystem;
 import com.gos.rapidreact.subsystems.VerticalConveyorSubsystem;
+import com.gos.rapidreact.trajectory.FourBallTrajectory;
 import com.gos.rapidreact.trajectory.Trajectory54;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import com.gos.rapidreact.trajectory.TrajectoryB5;
 
 import static com.gos.rapidreact.subsystems.ShooterSubsystem.DEFAULT_SHOOTER_RPM;
 
-public class ThreeBallAuto extends SequentialCommandGroup {
+public class FourBallAutoCommandGroup extends SequentialCommandGroup {
     private static final double FIRST_SHOT_RPM = DEFAULT_SHOOTER_RPM;
     private static final double SECOND_SHOT_RPM = DEFAULT_SHOOTER_RPM;
 
-    public ThreeBallAuto(ChassisSubsystem chassis, ShooterSubsystem shooter, VerticalConveyorSubsystem verticalConveyor, HorizontalConveyorSubsystem horizontalConveyor, CollectorSubsystem collector) {
+    public FourBallAutoCommandGroup(ChassisSubsystem chassis, ShooterSubsystem shooter, VerticalConveyorSubsystem verticalConveyor, HorizontalConveyorSubsystem horizontalConveyor, CollectorSubsystem collector) {
         super(
             new ShooterRpmPIDCommand(shooter, FIRST_SHOT_RPM),
             new FeederVerticalConveyorForwardCommand(verticalConveyor)
                 .alongWith(new ShooterRpmPIDCommand(shooter, FIRST_SHOT_RPM))
                 .alongWith(new CollectorDownCommand(collector)).withTimeout(1),
-            TrajectoryB5.fromBto5(chassis).alongWith(new RollerInCommand(collector).withTimeout(2)),
+            FourBallTrajectory.fourBallPart1(chassis)
+                .alongWith(new RollerInCommand(collector).withTimeout(2)),
             new HorizontalConveyorForwardCommand(horizontalConveyor),
-            Trajectory54.from5to4(chassis).alongWith(new RollerInCommand(collector).withTimeout(2)),
+            FourBallTrajectory.fourBallPart2(chassis),
             new HorizontalConveyorForwardCommand(horizontalConveyor),
-            new ShooterRpmPIDCommand(shooter, SECOND_SHOT_RPM),
+            new ShooterRpmPIDCommand(shooter, FIRST_SHOT_RPM),
             new FeederVerticalConveyorForwardCommand(verticalConveyor)
-                .alongWith(new ShooterRpmPIDCommand(shooter, SECOND_SHOT_RPM)));
+                .alongWith(new ShooterRpmPIDCommand(shooter, FIRST_SHOT_RPM)),
+            FourBallTrajectory.fourBallPart3(chassis)
+                .alongWith(new RollerInCommand(collector).withTimeout(2)),
+            new HorizontalConveyorForwardCommand(horizontalConveyor),
+            FourBallTrajectory.fourBallPart4(chassis),
+            new HorizontalConveyorForwardCommand(horizontalConveyor),
+            new ShooterRpmPIDCommand(shooter, FIRST_SHOT_RPM)
+//            new FeederVerticalConveyorForwardCommand(verticalConveyor)
+//                .alongWith(new ShooterRpmPIDCommand(shooter, FIRST_SHOT_RPM)
+                );
+
+
+
     }
 }
---
+++ y2022/RapidReact/src/main/java/com/gos/rapidreact/trajectory/FourBallTrajectory.java
@@ -0,0 +1,24 @@
+package com.gos.rapidreact.trajectory;
+
+import com.gos.rapidreact.subsystems.ChassisSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class FourBallTrajectory {
+
+    public static Command fourBallPart1(ChassisSubsystem chassis) {
+        return TrajectoryUtils.startTrajectory("PathWeaver/Paths/4Ball_Part1", TrajectoryUtils.getTrajectoryConfig(), chassis);
+    }
+
+    public static Command fourBallPart2(ChassisSubsystem chassis) {
+        return TrajectoryUtils.startTrajectory("PathWeaver/Paths/4Ball_Part2.path", TrajectoryUtils.getTrajectoryConfig(), chassis);
+    }
+
+    public static Command fourBallPart3(ChassisSubsystem chassis) {
+        return TrajectoryUtils.startTrajectory("PathWeaver/Paths/4Ball_Part3path", TrajectoryUtils.getTrajectoryConfig(), chassis);
+    }
+
+    public static Command fourBallPart4(ChassisSubsystem chassis) {
+        return TrajectoryUtils.startTrajectory("PathWeaver/Paths/4Ball_Part4path", TrajectoryUtils.getTrajectoryConfig(), chassis);
+    }
+
+}
