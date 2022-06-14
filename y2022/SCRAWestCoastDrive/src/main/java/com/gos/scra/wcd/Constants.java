e4b4f6799055222dc4289b774ac6d15a629e5f8c
Girls of Steel <girlsofsteelrobotics@gmail.com>
2022-06-14 18:34:13 -0400

    west coast drive chassis

---
+++ y2022/SCRAWestCoastDrive/src/main/java/com/gos/scra/wcd/Constants.java
@@ -0,0 +1,22 @@
+// Copyright (c) FIRST and other WPILib contributors.
+
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package com.gos.scra.wcd;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+
+    public static final int DRIVE_LEFT_LEADER_SPARK = 1;
+    public static final int DRIVE_LEFT_FOLLOWER_SPARK = 2;
+    public static final int DRIVE_RIGHT_LEADER_SPARK = 3;
+    public static final int DRIVE_RIGHT_FOLLOWER_SPARK = 4;
+}
