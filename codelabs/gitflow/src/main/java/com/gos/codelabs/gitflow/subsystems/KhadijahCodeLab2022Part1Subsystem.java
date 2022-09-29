37b7af094da532811798aea29b26254fd540e3b6
kbajwa <kobajwa@gmail.com>
2022-09-29 19:38:34 -0400

    completed github code lab part 1

--- codelabs/gitflow/src/main/java/com/gos/codelabs/gitflow/subsystems/KhadijahCodeLab2022Part1Subsystem.java
+++ codelabs/gitflow/src/main/java/com/gos/codelabs/gitflow/subsystems/KhadijahCodeLab2022Part1Subsystem.java
@@ -4,7 +4,7 @@ package com.gos.codelabs.gitflow.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class KhadijahCodeLab2022Part1Subsystem extends SubsystemBase {
-    private KhadijahCodeLab2022Part1Subsystem() {
+    public KhadijahCodeLab2022Part1Subsystem() {
         // TODO: Set the default command, if any, for this subsystem by calling setDefaultCommand(command)
         //       in the constructor or in the robot coordination class, such as RobotContainer.
         //       Also, you can call addChild(name, sendableChild) to associate sendables with the subsystem
