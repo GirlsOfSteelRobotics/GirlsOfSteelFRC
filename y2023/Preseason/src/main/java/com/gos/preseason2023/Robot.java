b3c303f221aabc2ca40865d21c04f8df681a83be
PJ Reiniger <pj.reiniger@gmail.com>
2022-10-05 23:23:49 -0400

    Fix package name

--- y2023/Preseason/src/main/java/com/gos/preseason2023/Robot.java
+++ y2023/Preseason/src/main/java/com/gos/preseason2023/Robot.java
@@ -3,7 +3,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+package com.gos.preseason2023;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
--- y2023/Preseason/src/main/java/com/gos/preseason2023/RobotContainer.java
+++ y2023/Preseason/src/main/java/com/gos/preseason2023/RobotContainer.java
@@ -3,13 +3,13 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
+package com.gos.preseason2023;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
+import com.gos.preseason2023.commands.ExampleCommand;
+import com.gos.preseason2023.subsystems.ExampleSubsystem;
 
 
 /**
