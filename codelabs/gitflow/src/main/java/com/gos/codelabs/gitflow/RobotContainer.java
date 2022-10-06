64bfd88950a36a9aaf04d75f0b9a7b76602ea8c4
sienna-li <sieddy0116@gmail.com>
2022-10-06 19:08:52 -0400

    completed gitflow codelab

--- codelabs/gitflow/src/main/java/com/gos/codelabs/gitflow/RobotContainer.java
+++ codelabs/gitflow/src/main/java/com/gos/codelabs/gitflow/RobotContainer.java
@@ -13,6 +13,7 @@ import com.gos.codelabs.gitflow.subsystems.JaniseCodelab2021Pt1Subsystem;
 import com.gos.codelabs.gitflow.subsystems.JustinaCodelab2021Part1Subsystem;
 import com.gos.codelabs.gitflow.subsystems.KatherineCodeLabe2022Part1Subsystem;
 import com.gos.codelabs.gitflow.subsystems.PJCodelab2021Part1Subsystem;
+import com.gos.codelabs.gitflow.subsystems.SiennaCodeLab2022Part1Subsystem;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -45,6 +46,8 @@ public class RobotContainer {
     private JustinaCodelab2021Part1Subsystem m_justinaCodeLab2021Part1;
     private JaniseCodelab2021Pt1Subsystem m_janiseCodelab2021Pt1;
     private EllieSubsystem m_ellieSubsystem;
+
+    private SiennaCodeLab2022Part1Subsystem m_siennaSubsystem;
 
     private AshleyCodelab2022Part1Subsystem m_ashleyCodelab2022Part1;
 
---
+++ codelabs/gitflow/src/main/java/com/gos/codelabs/gitflow/subsystems/SiennaCodeLab2022Part1Subsystem.java
@@ -0,0 +1,14 @@
+package com.gos.codelabs.gitflow.subsystems;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SiennaCodeLab2022Part1Subsystem extends SubsystemBase {
+    public SiennaCodeLab2022Part1Subsystem() {
+        // TODO: Set the default command, if any, for this subsystem by calling setDefaultCommand(command)
+        //       in the constructor or in the robot coordination class, such as RobotContainer.
+        //       Also, you can call addChild(name, sendableChild) to associate sendables with the subsystem
+        //       such as SpeedControllers, Encoders, DigitalInputs, etc.
+    }
+}
+
