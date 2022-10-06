ae1b2c203019fb0a4ce226b69604a37966bfcb32
PJ Reiniger <pj.reiniger@gmail.com>
2022-10-05 23:21:09 -0400

    Add project to gradle, move into a subfolder of 2023

--- y2023/Preseason/src/main/java/frc/robot/commands/ExampleCommand.java
+++ y2023/Preseason/src/main/java/frc/robot/commands/ExampleCommand.java
@@ -9,46 +9,47 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ExampleSubsystem;
 
 
-
-/** An example command that uses an example subsystem. */
-public class ExampleCommand extends CommandBase
-{
+/**
+ * An example command that uses an example subsystem.
+ */
+public class ExampleCommand extends CommandBase {
     @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-    private final ExampleSubsystem subsystem;
-    
-    
+    private final ExampleSubsystem m_subsystem;
+
+
     /**
      * Creates a new ExampleCommand.
      *
      * @param subsystem The subsystem used by this command.
      */
-    public ExampleCommand(ExampleSubsystem subsystem)
-    {
-        this.subsystem = subsystem;
+    public ExampleCommand(ExampleSubsystem subsystem) {
+        this.m_subsystem = subsystem;
         // Use addRequirements() here to declare subsystem dependencies.
         addRequirements(subsystem);
     }
-    
-    
+
+
     // Called when the command is initially scheduled.
     @Override
-    public void initialize() {}
-    
-    
+    public void initialize() {
+    }
+
+
     // Called every time the scheduler runs while the command is scheduled.
     @Override
-    public void execute() {}
-    
-    
+    public void execute() {
+    }
+
+
     // Called once the command ends or is interrupted.
     @Override
-    public void end(boolean interrupted) {}
-    
-    
+    public void end(boolean interrupted) {
+    }
+
+
     // Returns true when the command should end.
     @Override
-    public boolean isFinished()
-    {
+    public boolean isFinished() {
         return false;
     }
 }
