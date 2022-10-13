4843addd6e1e8029180d08c61a801f9055b687d5
minkrage <yangster2007@gmail.com>
2022-10-13 18:42:28 -0400

    completed gitflow codelab B)

--- codelabs/gitflow/src/main/java/com/gos/codelabs/gitflow/subsystems/HannahCodeLab2022Part1Subsystem.java
+++ codelabs/gitflow/src/main/java/com/gos/codelabs/gitflow/subsystems/HannahCodeLab2022Part1Subsystem.java
@@ -5,10 +5,6 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class HannahCodeLab2022Part1Subsystem extends SubsystemBase {
     public HannahCodeLab2022Part1Subsystem() {
-        // TODO: Set the default command, if any, for this subsystem by calling setDefaultCommand(command)
-        //       in the constructor or in the robot coordination class, such as RobotContainer.
-        //       Also, you can call addChild(name, sendableChild) to associate sendables with the subsystem
-        //       such as SpeedControllers, Encoders, DigitalInputs, etc.
         System.out.println("Hannah says hello world in year 2022 part 1");
     }
 }
