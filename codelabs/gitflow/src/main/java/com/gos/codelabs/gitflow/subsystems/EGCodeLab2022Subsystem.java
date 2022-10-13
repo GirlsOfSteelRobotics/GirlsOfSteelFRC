6787b2fa66599a0b459531540ba8c474217cf63e
Grace_Goslin <gosling.gos@gmail.com>
2022-10-13 19:12:07 -0400

    Add "Hi"

---
+++ codelabs/gitflow/src/main/java/com/gos/codelabs/gitflow/subsystems/EGCodeLab2022Subsystem.java
@@ -0,0 +1,11 @@
+package com.gos.codelabs.gitflow.subsystems;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class EGCodeLab2022Subsystem extends SubsystemBase {
+    public EGCodeLab2022Subsystem() {
+        System.out.println("Hi");
+    }
+}
+
