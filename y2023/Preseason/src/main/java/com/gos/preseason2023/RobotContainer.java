9526e4f12489f935cdd9ec7bcfc2a1379b1c6071
Girls of Steel <girlsofsteelrobotics@gmail.com>
2022-10-27 19:55:43 -0400

    code for basic collector subsystem + command demo

--- y2023/Preseason/src/main/java/com/gos/preseason2023/RobotContainer.java
+++ y2023/Preseason/src/main/java/com/gos/preseason2023/RobotContainer.java
@@ -5,11 +5,15 @@
 
 package com.gos.preseason2023;
 
+import com.gos.preseason2023.commands.CollectorPivotCommand;
+import com.gos.preseason2023.commands.CollectorRollerCommand;
+import com.gos.preseason2023.subsystems.CollectorExampleSubsystem;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import com.gos.preseason2023.commands.ExampleCommand;
 import com.gos.preseason2023.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 
 /**
@@ -23,6 +27,10 @@ public class RobotContainer {
     private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
     private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+
+    private final CollectorExampleSubsystem m_collectorSubsystem = new CollectorExampleSubsystem();
+
+    private final XboxController m_driverController = new XboxController(0);
 
 
     /**
@@ -43,6 +51,15 @@ public class RobotContainer {
     private void configureButtonBindings() {
         // Add button to command mappings here.
         // See https://docs.wpilib.org/en/stable/docs/software/commandbased/binding-commands-to-triggers.html
+
+        final JoystickButton pivotUp = new JoystickButton(m_driverController, XboxController.Button.kRightBumper.value);
+        pivotUp.whileHeld(new CollectorPivotCommand(m_collectorSubsystem, 0.5));
+        final JoystickButton pivotDown = new JoystickButton(m_driverController, XboxController.Button.kLeftBumper.value);
+        pivotDown.whileHeld(new CollectorPivotCommand(m_collectorSubsystem, -0.5));
+        final JoystickButton rollerIn = new JoystickButton(m_driverController, XboxController.Button.kA.value);
+        rollerIn.whileHeld(new CollectorRollerCommand(m_collectorSubsystem, 0.5));
+        final JoystickButton rollerOut = new JoystickButton(m_driverController, XboxController.Button.kX.value);
+        rollerOut.whileHeld(new CollectorRollerCommand(m_collectorSubsystem, -0.5));
     }
 
 
