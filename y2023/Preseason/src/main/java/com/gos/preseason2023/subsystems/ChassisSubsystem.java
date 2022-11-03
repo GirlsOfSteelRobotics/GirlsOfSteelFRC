f64ea5375779b7971840b6ed6d5249eba75307fc
Anuva <anuva.ghosalkar@gmail.com>
2022-11-03 19:56:52 -0400

    Added functionality to the swerve drive

--- y2023/Preseason/src/main/java/com/gos/preseason2023/subsystems/ChassisSubsystem.java
+++ y2023/Preseason/src/main/java/com/gos/preseason2023/subsystems/ChassisSubsystem.java
@@ -1,18 +1,37 @@
 package com.gos.preseason2023.subsystems;
 
 import com.gos.preseason2023.Constants;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.snobotv2.module_wrappers.wpi.ADXRS450GyroWrapper;
+import org.snobotv2.sim_wrappers.SwerveModuleSimWrapper;
+import org.snobotv2.sim_wrappers.SwerveSimWrapper;
+
+import java.util.List;
 
 public class ChassisSubsystem extends SubsystemBase {
+
 
     private final SwerveModule m_swerveFrontLeft;
     private final SwerveModule m_swerveFrontRight;
     private final SwerveModule m_swerveBackLeft;
     private final SwerveModule m_swerveBackRight;
+
+    private static final double kWheelBase = Units.inchesToMeters(20.733);
+    private static final double kTrackWidth = Units.inchesToMeters(20.733);
+
+    private SwerveSimWrapper m_simWrapper;
 
     // Locations for the swerve drive modules relative to the robot center.
     //TODO: fake numbers lol get our actual swerve numbers
@@ -21,18 +40,57 @@ public class ChassisSubsystem extends SubsystemBase {
     private final Translation2d m_backLeftLocation = new Translation2d(-0.381, 0.381);
     private final Translation2d m_backRightLocation = new Translation2d(-0.381, -0.381);
 
+    private final ADXRS450_Gyro m_gyro = new ADXRS450_Gyro();
+
+    private final Field2d m_field;
+
     // Creating my kinematics object using the module locations
-    private final SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(
-        m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation, m_backRightLocation
-    );
-
-
-
     public ChassisSubsystem() {
+        m_field = new Field2d();
+        SmartDashboard.putData(m_field);
         m_swerveFrontLeft = new SwerveModule(Constants.SWERVE_FRONT_LEFT_SPIN_ID, Constants.SWERVE_FRONT_LEFT_POWER_ID);
         m_swerveFrontRight = new SwerveModule(Constants.SWERVE_FRONT_RIGHT_SPIN_ID, Constants.SWERVE_FRONT_RIGHT_POWER_ID);
         m_swerveBackLeft = new SwerveModule(Constants.SWERVE_BACK_LEFT_SPIN_ID, Constants.SWERVE_BACK_LEFT_POWER_ID);
         m_swerveBackRight = new SwerveModule(Constants.SWERVE_BACK_RIGHT_SPIN_ID, Constants.SWERVE_BACK_RIGHT_POWER_ID);
+
+        if (RobotBase.isSimulation()) {
+            List<SwerveModuleSimWrapper> moduleSims = List.of(
+                m_swerveFrontLeft.getSimWrapper(),
+                m_swerveFrontRight.getSimWrapper(),
+                m_swerveBackLeft.getSimWrapper(),
+                m_swerveBackRight.getSimWrapper());
+            m_simWrapper = new SwerveSimWrapper(kWheelBase, kTrackWidth, 64.0, 1.0, moduleSims, new ADXRS450GyroWrapper(m_gyro));
+        }
+    }
+
+
+    private final SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(
+        m_frontLeftLocation, m_frontRightLocation, m_backLeftLocation, m_backRightLocation
+    );
+
+    SwerveDriveOdometry m_odometry = new SwerveDriveOdometry(m_kinematics,
+        getGyroHeading(), new Pose2d(5, 6, new Rotation2d()));
+
+    private Rotation2d getGyroHeading() {
+        return m_gyro.getRotation2d();
+    }
+
+    @Override
+    public void periodic() {
+        // Get my gyro angle. We are negating the value because gyros return positive
+        // values as the robot turns clockwise. This is not standard convention that is
+        // used by the WPILib classes.
+        var gyroAngle = Rotation2d.fromDegrees(-m_gyro.getAngle());
+        m_field.setRobotPose(getPose());
+
+        // Update the pose
+        m_odometry.update(gyroAngle, m_swerveFrontLeft.getState(), m_swerveFrontRight.getState(),
+            m_swerveBackLeft.getState(), m_swerveBackRight.getState());
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        m_simWrapper.update();
     }
 
     public void setJoystickDrive(double xAxis, double yAxis, double steer) {
@@ -57,5 +115,10 @@ public class ChassisSubsystem extends SubsystemBase {
         m_swerveBackLeft.goToState(backLeft);
         m_swerveBackRight.goToState(backRight);
     }
+
+    public Pose2d getPose() {
+        return m_odometry.getPoseMeters();
+    }
+
 
 }
--- y2023/Preseason/src/main/java/com/gos/preseason2023/subsystems/SwerveModule.java
+++ y2023/Preseason/src/main/java/com/gos/preseason2023/subsystems/SwerveModule.java
@@ -1,24 +1,60 @@
 package com.gos.preseason2023.subsystems;
 
-import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SimableCANSparkMax;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.simulation.swerve.SwerveModuleSim;
+import org.snobotv2.module_wrappers.rev.RevEncoderSimWrapper;
+import org.snobotv2.module_wrappers.rev.RevMotorControllerSimWrapper;
+import org.snobotv2.sim_wrappers.SwerveModuleSimWrapper;
 
 public class SwerveModule {
 
-    private final CANSparkMax m_spinMotor;
-    private final CANSparkMax m_powerMotor;
+    private static final double TURNING_GEAR_RATION = (50.0 / 14.0) * (60.0 / 10.0);
+    private static final double DRIVE_GEAR_RATION = (50.0 / 14.0) * (19.0 / 25.0) * (45.0 / 15.0);
+
+    private static final double WHEEL_RADIUS = Units.inchesToMeters(4.0 / 2);
+
+    private final SimableCANSparkMax m_spinMotor;
+    private final SimableCANSparkMax m_powerMotor;
 
     private final RelativeEncoder m_spinEncoder;
     private final RelativeEncoder m_powerEncoder; // NOPMD
 
+    private SwerveModuleSimWrapper m_simWrapper;
+
     public SwerveModule(int spinID, int powerID) {
-        m_spinMotor = new CANSparkMax(spinID, CANSparkMaxLowLevel.MotorType.kBrushless);
-        m_powerMotor = new CANSparkMax(powerID, CANSparkMaxLowLevel.MotorType.kBrushless);
+        m_spinMotor = new SimableCANSparkMax(spinID, CANSparkMaxLowLevel.MotorType.kBrushless);
+        m_powerMotor = new SimableCANSparkMax(powerID, CANSparkMaxLowLevel.MotorType.kBrushless);
 
         m_spinEncoder = m_spinMotor.getEncoder();
         m_powerEncoder = m_powerMotor.getEncoder();
+
+        if (RobotBase.isSimulation()) {
+            SwerveModuleSim moduleSim = new SwerveModuleSim(
+                DCMotor.getNEO(1),
+                DCMotor.getNEO(1),
+                WHEEL_RADIUS,
+                TURNING_GEAR_RATION,
+                DRIVE_GEAR_RATION
+            );
+
+            m_simWrapper = new SwerveModuleSimWrapper(
+                moduleSim,
+                new RevMotorControllerSimWrapper(m_powerMotor),
+                new RevMotorControllerSimWrapper(m_spinMotor),
+                RevEncoderSimWrapper.create(m_powerMotor),
+                RevEncoderSimWrapper.create(m_spinMotor));
+        }
+    }
+
+    public SwerveModuleSimWrapper getSimWrapper() {
+        return m_simWrapper;
     }
 
     public void goToState(SwerveModuleState state) {
@@ -32,4 +68,9 @@ public class SwerveModule {
 
         m_powerMotor.set(state.speedMetersPerSecond);
     }
+
+    public SwerveModuleState getState() {
+        return new SwerveModuleState(m_powerEncoder.getVelocity(), Rotation2d.fromDegrees(m_spinEncoder.getPosition()));
+    }
 }
+
