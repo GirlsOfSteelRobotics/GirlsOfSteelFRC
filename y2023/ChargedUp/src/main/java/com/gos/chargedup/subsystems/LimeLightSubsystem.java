3664ac367cc2061e301171cbce0160694c42a548
Anuva <anuva.ghosalkar@gmail.com>
2023-01-12 18:01:24 -0500

    Added Limelight Subsystem 1/12

---
+++ y2023/ChargedUp/src/main/java/com/gos/chargedup/subsystems/LimeLightSubsystem.java
@@ -0,0 +1,12 @@
+package com.gos.chargedup.subsystems;
+
+
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
+public class LimeLightSubsystem implements Subsystem {
+    public LimeLightSubsystem() {
+        // TODO: Set the default command, if any, for this subsystem by calling setDefaultCommand(command)
+        //       in the constructor or in the robot coordination class, such as RobotContainer.
+    }
+}
+
