e6b02692717838866c3d770d4ca58e668621603f
egreenbaum <egreenbaum86@gmail.com>
2023-01-13 12:41:31 -0500

    Limelight2023 subsystem: Added getResult(), getTargets(), and working on targetData2D

--- y2023/ChargedUp/src/main/java/com/gos/chargedup/subsystems/LimelightSubsystem.java
+++ y2023/ChargedUp/src/main/java/com/gos/chargedup/subsystems/LimelightSubsystem.java
@@ -1,41 +1,43 @@
 package com.gos.chargedup.subsystems;
 
+import java.util.List;
+//import edu.wpi.first.math.util.Units;
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonTrackedTarget;
+import org.photonvision.targeting.PhotonPipelineResult;
+//import org.photonvision.targeting.TargetCorner;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LimelightSubsystem extends SubsystemBase {
+    PhotonCamera m_camera;
 
-    // With eager singleton initialization, any static variables/fields used in the 
-    // constructor must appear before the "INSTANCE" variable so that they are initialized 
-    // before the constructor is called when the "INSTANCE" variable initializes.
-
-    /**
-     * The Singleton instance of this LimelightSubsystem. Code should use
-     * the {@link #getInstance()} method to get the single instance (rather
-     * than trying to construct an instance of this class.)
-     */
-    private static final LimelightSubsystem limelight = new LimelightSubsystem();
-
-    /**
-     * Returns the Singleton instance of this LimelightSubsystem. This static method
-     * should be used, rather than the constructor, to get the single instance
-     * of this class. For example: {@code LimelightSubsystem.getInstance();}
-     */
-    @SuppressWarnings("WeakerAccess")
-    public static LimelightSubsystem getInstance() {
-        return limelight;
+    public LimelightSubsystem() {
+        m_camera = new PhotonCamera("photonvision");
     }
 
-    /**
-     * Creates a new instance of this LimelightSubsystem. This constructor
-     * is private since this class is a Singleton. Code should use
-     * the {@link #getInstance()} method to get the singleton instance.
-     */
-    private LimelightSubsystem() {
-        // TODO: Set the default command, if any, for this subsystem by calling setDefaultCommand(command)
-        //       in the constructor or in the robot coordination class, such as RobotContainer.
-        //       Also, you can call addChild(name, sendableChild) to associate sendables with the subsystem
-        //       such as SpeedControllers, Encoders, DigitalInputs, etc.
+    public PhotonPipelineResult getResult() {
+        return m_camera.getLatestResult();
     }
+
+    public List<PhotonTrackedTarget> getTargets(PhotonPipelineResult result) {
+        if (result.hasTargets()) {
+            return result.getTargets();
+        } else {
+            return null;
+        }
+    }
+
+    public void targetData2D(PhotonTrackedTarget target) {
+        double yaw = target.getYaw();
+        double pitch = target.getPitch();
+        double area = target.getArea();
+        double skew = target.getSkew();
+        //Transform2d pose = target.getCameraToTarget();
+        //List<TargetCorner> corners = target.getCorners();
+    }
+
+
+
 }
 
