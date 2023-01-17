d51d4c959fe85a53da435b723ea4a6070f70d050
Anuva <anuva.ghosalkar@gmail.com>
2023-01-16 20:28:24 -0500

    Separated Vision code into its own branch

--- y2023/ChargedUp/src/main/java/com/gos/chargedup/subsystems/ChassisSubsystem.java
+++ y2023/ChargedUp/src/main/java/com/gos/chargedup/subsystems/ChassisSubsystem.java
@@ -4,13 +4,28 @@ import com.ctre.phoenix.sensors.WPI_PigeonIMU;
 import com.gos.chargedup.Constants;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SimableCANSparkMax;
+import edu.wpi.first.math.Pair;
+import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.snobotv2.module_wrappers.ctre.CtrePigeonImuWrapper;
+import org.snobotv2.module_wrappers.rev.RevEncoderSimWrapper;
+import org.snobotv2.module_wrappers.rev.RevMotorControllerSimWrapper;
+import org.snobotv2.sim_wrappers.DifferentialDrivetrainSimWrapper;
+
 
 public class ChassisSubsystem extends SubsystemBase {
+    //Chassis and motors
     private final SimableCANSparkMax m_leaderLeft;
     private final SimableCANSparkMax m_followerLeft;
     private final SimableCANSparkMax m_leaderRight;
@@ -18,10 +33,33 @@ public class ChassisSubsystem extends SubsystemBase {
 
     private final DifferentialDrive m_drive;
 
-    private final DifferentialDriveOdometry m_odometry; // NOPMD
-    private final WPI_PigeonIMU m_gyro; //NOPMD
+    //Odometry
+    private final DifferentialDriveOdometry m_odometry;
+    private final WPI_PigeonIMU m_gyro;
+
+    private final RelativeEncoder m_rightEncoder;
+
+    private final RelativeEncoder m_leftEncoder;
+
+    //Field
+    private final Field2d m_field;
+
+    //SIM
+    private DifferentialDrivetrainSimWrapper m_simulator;
+
+    private VisionSubsystem m_pcw;
+
+    private static final double TRACK_WIDTH = 0.381 * 2; //set this to the actual
+    private final DifferentialDriveKinematics m_kinematics =
+        new DifferentialDriveKinematics(TRACK_WIDTH);
+
+    private final DifferentialDrivePoseEstimator m_poseEstimator;
+
+
+
 
     public ChassisSubsystem() {
+
         m_leaderLeft = new SimableCANSparkMax(Constants.DRIVE_LEFT_LEADER_SPARK, CANSparkMaxLowLevel.MotorType.kBrushless);
         m_followerLeft = new SimableCANSparkMax(Constants.DRIVE_LEFT_FOLLOWER_SPARK, CANSparkMaxLowLevel.MotorType.kBrushless);
         m_leaderRight = new SimableCANSparkMax(Constants.DRIVE_RIGHT_LEADER_SPARK, CANSparkMaxLowLevel.MotorType.kBrushless);
@@ -47,6 +85,33 @@ public class ChassisSubsystem extends SubsystemBase {
 
         m_gyro = new WPI_PigeonIMU(Constants.PIGEON_PORT);
         m_odometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(0), 0, 0);
+        m_rightEncoder = m_leaderRight.getEncoder();
+        m_leftEncoder = m_leaderLeft.getEncoder();
+
+        m_field = new Field2d();
+
+        m_poseEstimator = new DifferentialDrivePoseEstimator(
+            m_kinematics, m_gyro.getRotation2d(), 0.0, 0.0, new Pose2d());
+
+        m_pcw = new VisionSubsystem();
+
+        if (RobotBase.isSimulation()) {
+            DifferentialDrivetrainSim drivetrainSim = DifferentialDrivetrainSim.createKitbotSim(
+                DifferentialDrivetrainSim.KitbotMotor.kDoubleNEOPerSide,
+                DifferentialDrivetrainSim.KitbotGearing.k5p95,
+                DifferentialDrivetrainSim.KitbotWheelSize.kSixInch,
+                null);
+            m_simulator = new DifferentialDrivetrainSimWrapper(
+                drivetrainSim,
+                new RevMotorControllerSimWrapper(m_leaderLeft),
+                new RevMotorControllerSimWrapper(m_leaderRight),
+                RevEncoderSimWrapper.create(m_leaderLeft),
+                RevEncoderSimWrapper.create(m_leaderRight),
+                new CtrePigeonImuWrapper(m_gyro));
+            m_simulator.setRightInverted(false);
+
+            SmartDashboard.putData(m_field);
+        }
     }
 
     public void setArcadeDrive(double speed, double steer) {
@@ -57,5 +122,43 @@ public class ChassisSubsystem extends SubsystemBase {
         m_drive.curvatureDrive(speed, steer, speed < 0.05);
 
     }
+
+    @Override
+    public void periodic() {
+        m_odometry.update(Rotation2d.fromDegrees(m_gyro.getYaw()), m_leftEncoder.getPosition(), m_rightEncoder.getPosition());
+        m_field.setRobotPose(m_odometry.getPoseMeters());
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        m_simulator.update();
+
+    }
+
+    //NEW ODOMETRY
+    public void updateOdometry() {
+        m_poseEstimator.update(
+            m_gyro.getRotation2d(), m_leftEncoder.getPosition(), m_rightEncoder.getPosition());
+
+        // Also apply vision measurements. We use 0.3 seconds in the past as an example
+        // -- on
+        // a real robot, this must be calculated based either on latency or timestamps.
+        Pair<Pose2d, Double> result =
+            m_pcw.getEstimatedGlobalPose(m_poseEstimator.getEstimatedPosition());
+        var camPose = result.getFirst();
+        var camPoseObsTime = result.getSecond();
+        if (camPose != null) {
+            m_poseEstimator.addVisionMeasurement(camPose, camPoseObsTime);
+            m_field.getObject("Camera Estimated Position").setPose(camPose);
+        } else {
+            // move it way off the screen to make it disappear
+            m_field.getObject("Camera Estimated Position").setPose(new Pose2d(-100, -100, new Rotation2d()));
+        }
+
+        m_field.setRobotPose(m_poseEstimator.getEstimatedPosition());
+    }
+
+
+
 
 }
