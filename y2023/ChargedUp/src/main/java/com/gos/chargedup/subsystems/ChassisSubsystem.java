10b4480f67f764d2ee10dd65c49e59a5671f8f14
Girls of Steel <girlsofsteelrobotics@gmail.com>
2023-01-17 19:23:57 -0500

    Added PID to the forward driving on the Chassis

--- y2023/ChargedUp/src/main/java/com/gos/chargedup/subsystems/ChassisSubsystem.java
+++ y2023/ChargedUp/src/main/java/com/gos/chargedup/subsystems/ChassisSubsystem.java
@@ -2,11 +2,15 @@ package com.gos.chargedup.subsystems;
 
 import com.ctre.phoenix.sensors.WPI_PigeonIMU;
 import com.gos.chargedup.Constants;
+import com.gos.lib.properties.PidProperty;
+import com.gos.lib.rev.RevPidPropertyBuilder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import com.revrobotics.SimableCANSparkMax;
+import com.revrobotics.SparkMaxPIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -20,6 +24,12 @@ public class ChassisSubsystem extends SubsystemBase {
 
     private final DifferentialDriveOdometry m_odometry; // NOPMD
     private final WPI_PigeonIMU m_gyro; //NOPMD
+
+    private final SparkMaxPIDController m_leftPIDcontroller;
+    private final SparkMaxPIDController m_rightPIDcontroller;
+
+    private final PidProperty m_leftProperties;
+    private final PidProperty m_rightProperties;
 
     public ChassisSubsystem() {
         m_leaderLeft = new SimableCANSparkMax(Constants.DRIVE_LEFT_LEADER_SPARK, CANSparkMaxLowLevel.MotorType.kBrushless);
@@ -47,6 +57,36 @@ public class ChassisSubsystem extends SubsystemBase {
 
         m_gyro = new WPI_PigeonIMU(Constants.PIGEON_PORT);
         m_odometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(0), 0, 0);
+
+        m_leftPIDcontroller = m_leaderLeft.getPIDController();
+        m_rightPIDcontroller = m_leaderRight.getPIDController();
+
+        m_leftProperties = setupPidValues(m_leftPIDcontroller);
+        m_rightProperties = setupPidValues(m_rightPIDcontroller);
+
+    }
+
+    private PidProperty setupPidValues(SparkMaxPIDController pidController) {
+        return new RevPidPropertyBuilder("Chassis", false, pidController, 0)
+            .addP(.00003) //this needs to be tuned!
+            .addI(0)
+            .addD(0)
+            .addFF(0)
+            .addMaxVelocity((Units.inchesToMeters(0)))
+            .addMaxAcceleration((Units.inchesToMeters(0)))
+            .build();
+    }
+
+    public void smartVelocityControl(double leftVelocity, double rightVelocity, double leftAcceleration, double rightAcceleration) {
+        m_leftPIDcontroller.setReference(leftVelocity, CANSparkMax.ControlType.kVelocity, 0);
+        m_rightPIDcontroller.setReference(rightVelocity, CANSparkMax.ControlType.kVelocity, 0);
+
+    }
+
+    public void trapezoidMotionControl(double leftDistance, double rightDistance) {
+        m_leftPIDcontroller.setReference(leftDistance, CANSparkMax.ControlType.kSmartMotion, 0);
+        m_rightPIDcontroller.setReference(rightDistance, CANSparkMax.ControlType.kSmartMotion, 0);
+
     }
 
     public void setArcadeDrive(double speed, double steer) {
