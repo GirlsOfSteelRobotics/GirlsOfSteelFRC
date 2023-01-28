974c64fcf0d6c26693637200f72aad610ffcdc92
Grace_Goslin <gosling.gos@gmail.com>
2023-01-28 11:24:40 -0500

    fixed merge conflict

--- y2023/ChargedUp/src/main/java/com/gos/chargedup/Constants.java
+++ y2023/ChargedUp/src/main/java/com/gos/chargedup/Constants.java
@@ -14,38 +14,16 @@ package com.gos.chargedup;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+    public static final int PIGEON_PORT = 1;
 
     //motors
     public static final int DRIVE_LEFT_LEADER_SPARK = 4;
     public static final int DRIVE_LEFT_FOLLOWER_SPARK = 5;
     public static final int DRIVE_RIGHT_LEADER_SPARK = 9;
     public static final int DRIVE_RIGHT_FOLLOWER_SPARK = 10;
-    public static final int PIGEON_PORT = 1;
     public static final int PIVOT_SPARK = 2;
     public static final int TURRET_SPARK = 3;
-    public static final int PIGEON_PORT = 20;
-
-    //motors
-    public static final int CLAW_INTAKE_RIGHT = 0;
-
-    public static final int CLAW_INTAKE_LEFT = 1;
-
-    public static final int HOPPER_MOTOR = 10;
-
-    public static final int PIVOT_MOTOR = 2;
-
-    public static final int DRIVER_CONTROLLER_PORT = 0;
-
-    public static final int INTAKE_LEFT_PISTON = 2;
-    public static final int INTAKE_RIGHT_PISTON = 5;
-
-    public static final int TURRET_MOTOR = 3;
-
-    public static final int LEFT_LIMIT_SWITCH = 2;
-    public static final int INTAKE_LIMIT_SWITCH = 2;
-    public static final int RIGHT_LIMIT_SWITCH = 2;
-
-    public static final int SOLENOID_RIGHT_INTAKE = 0;
+    public static final int HOPPER_MOTOR = 11;
 
     //DIO
     public static final int INTAKE_LOWER_LIMIT_SWITCH = 7;
@@ -59,6 +37,8 @@ public final class Constants {
     public static final int ARM_INNER_PISTON = 2;
     public static final int RIGHT_CLAW_PISTON = 0;
     public static final int LEFT_INTAKE_PISTON = 1;
+    public static final int INTAKE_LEFT_PISTON = 3;
+    public static final int INTAKE_RIGHT_PISTON = 5;
 
     //Controllers
     public static final int DRIVER_CONTROLLER_PORT = 0;
