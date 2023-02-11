8302fbf1b888f0539b7974461fd6a8f0f34421dc
sienna-li <sieddy0116@gmail.com>
2023-02-11 13:59:46 -0500

    turret PID

--- y2023/ChargedUp/src/main/java/com/gos/chargedup/commands/TurretArmToIntakeCommandGroup.java
+++ y2023/ChargedUp/src/main/java/com/gos/chargedup/commands/TurretArmToIntakeCommandGroup.java
@@ -3,15 +3,15 @@ package com.gos.chargedup.commands;
 
 import com.gos.chargedup.subsystems.ArmSubsystem;
 import com.gos.chargedup.subsystems.ClawSubsystem;
+import com.gos.chargedup.subsystems.IntakeSubsystem;
 import com.gos.chargedup.subsystems.TurretSubsystem;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class TurretArmToIntakeCommandGroup extends SequentialCommandGroup {
-    public TurretArmToIntakeCommandGroup(ArmSubsystem arm, TurretSubsystem turret, ClawSubsystem claw) {
-        addCommands(arm.commandFullRetract());
-        addCommands(turret.);
+    public TurretArmToIntakeCommandGroup(ArmSubsystem arm, IntakeSubsystem intake, TurretSubsystem turret, ClawSubsystem claw) {
+        addCommands(intake.createExtendSolenoidCommand());
+        addCommands(turretPID(0.0));
         addCommands(arm.commandPivotArmDown());
-        addCommands(claw.);
-        addCommands(arm.commandMiddleRetract());
+        addCommands(intake.createRetractSolenoidCommand());
     }
 }
