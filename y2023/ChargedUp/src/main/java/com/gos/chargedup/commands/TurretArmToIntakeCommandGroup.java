45d8dd6cadc47faf9f4aebfef1dfae2983e7efcd
sienna-li <sieddy0116@gmail.com>
2023-02-11 14:09:12 -0500

    Turret arm to intake command

--- y2023/ChargedUp/src/main/java/com/gos/chargedup/commands/TurretArmToIntakeCommandGroup.java
+++ y2023/ChargedUp/src/main/java/com/gos/chargedup/commands/TurretArmToIntakeCommandGroup.java
@@ -2,16 +2,15 @@ package com.gos.chargedup.commands;
 
 
 import com.gos.chargedup.subsystems.ArmSubsystem;
-import com.gos.chargedup.subsystems.ClawSubsystem;
 import com.gos.chargedup.subsystems.IntakeSubsystem;
 import com.gos.chargedup.subsystems.TurretSubsystem;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class TurretArmToIntakeCommandGroup extends SequentialCommandGroup {
-    public TurretArmToIntakeCommandGroup(ArmSubsystem arm, IntakeSubsystem intake, TurretSubsystem turret, ClawSubsystem claw) {
-        addCommands(intake.createExtendSolenoidCommand());
-        addCommands(turretPID(0.0));
+    public TurretArmToIntakeCommandGroup(ArmSubsystem arm, IntakeSubsystem intake, TurretSubsystem turret) {
+        addCommands(intake.createIntakeOutCommand());
+        addCommands(turret.commandTurretPID(0.0));
         addCommands(arm.commandPivotArmDown());
-        addCommands(intake.createRetractSolenoidCommand());
+        addCommands(intake.createIntakeInCommand());
     }
 }
