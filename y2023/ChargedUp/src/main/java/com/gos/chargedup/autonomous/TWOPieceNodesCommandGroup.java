e37b756b4d296da3c4eaf14ff9ce72aef067ba9a
egreenbaum <egreenbaum86@gmail.com>
2023-02-16 20:37:57 -0500

    Began fixing TWOPieceNodesCommandGroup, stopped at setArmAndTurretToScore marker

--- y2023/ChargedUp/src/main/java/com/gos/chargedup/autonomous/TWOPieceNodesCommandGroup.java
+++ y2023/ChargedUp/src/main/java/com/gos/chargedup/autonomous/TWOPieceNodesCommandGroup.java
@@ -11,6 +11,7 @@ import com.gos.chargedup.subsystems.TurretSubsystem;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 import java.util.HashMap;
@@ -27,6 +28,18 @@ public class TWOPieceNodesCommandGroup extends SequentialCommandGroup {
             claw.createMoveClawIntakeCloseCommand() //piece is firmly in the claw? finish + tune soon
         ));
 
+        eventMap.put("resetArmAndTurret", new ParallelCommandGroup(
+            turret.commandTurretPID(0.0),
+            arm.commandPivotArmToAngle(ArmSubsystem.MIN_ANGLE_DEG)
+
+        ));
+
+        eventMap.put("setArmAndTurretToScore", new ParallelCommandGroup(
+            turret.commandTurretPID(180),
+            arm.commandPivotArmToAngle(ArmSubsystem.ARM_CONE_HIGH_DEG)
+
+        ));
+
         List<PathPlannerTrajectory> twoPieceNodes0And1 = PathPlanner.loadPathGroup(autoName, Constants.DEFAULT_PATH_CONSTRAINTS);
         Command fullAuto = chassis.ramseteAutoBuilder(eventMap).fullAuto(twoPieceNodes0And1);
 
@@ -39,7 +52,7 @@ public class TWOPieceNodesCommandGroup extends SequentialCommandGroup {
 
         //score piece:
         //addCommands(turret.commandTurretPID(180));
-
+        addCommands(turret.commandTurretPID(69));
 
         addCommands(new ScorePieceCommandGroup(turret, arm, claw, pivotHeightType, GamePieceType.CUBE));
 
