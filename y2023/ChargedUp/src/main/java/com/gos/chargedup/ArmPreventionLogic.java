de339ad54df7ab409f3176c0aa4057b109fa829c
egreenbaum <egreenbaum86@gmail.com>
2023-02-21 18:21:21 -0500

    continued on checks

--- y2023/ChargedUp/src/main/java/com/gos/chargedup/ArmPreventionLogic.java
+++ y2023/ChargedUp/src/main/java/com/gos/chargedup/ArmPreventionLogic.java
@@ -16,34 +16,29 @@ public class ArmPreventionLogic {
         this.m_arm = arm;
         this.m_turret = turret;
         this.m_intake = intake;
-
     }
 
 
     public boolean canArmExtend() {
         if (m_intake.getIntakeOut()) {
-            return m_arm.isArmAtAngle(m_arm.MIN_ANGLE_DEG);
-        } else if (m_arm.getArmAngleDeg() > m_arm.ARM_HIT_INTAKE_ANGLE) {
-            return true;
+            return m_arm.isArmAtAngle(m_arm.MIN_ANGLE_DEG);   //check if intake is down & if arm is at min angle
         } else {
-            m_arm.pivotArmStop();
-            return false;
+            return m_arm.getArmAngleDeg() > m_arm.ARM_HIT_INTAKE_ANGLE
+                   && m_arm.getArmAngleDeg() <= m_arm.MAX_ANGLE_DEG;    //check if arm is within allowable range
         }
     }
 
     public boolean canArmMoveUP() {
-        if (m_intake.getIntakeOut()) {
+        if (m_intake.getIntakeOut()) {    //check if intake is down
             return true;
         } else {
-            if (m_arm.getArmAngleDeg() > m_arm.ARM_HIT_INTAKE_ANGLE && m_arm.getArmAngleDeg() < m_arm.MAX_ANGLE_DEG) {
-                return true;
-            }
+            //check if arm is within allowable range
+            return (m_arm.getArmAngleDeg() > m_arm.ARM_HIT_INTAKE_ANGLE && m_arm.getArmAngleDeg() < m_arm.MAX_ANGLE_DEG);
         }
-        m_arm.pivotArmStop();
-        return false;
     }
 
     public boolean canArmPivot() {
+        //check if turret faces intake and arm is extended and at min angle
         if (m_turret.getTurretAngleDeg() == 0 && !m_arm.isBottomPistonIn() && m_arm.isArmAtAngle(m_arm.MIN_ANGLE_DEG)) {
             m_arm.pivotArmStop();
             return false;
