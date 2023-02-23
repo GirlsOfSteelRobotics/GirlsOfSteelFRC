f55c6460cbc8641a0a8c692f82f0fc9bd49c649b
ashley-wei <72471243+ashley-wei@users.noreply.github.com>
2023-02-22 22:41:07 -0500

    Testing intake (#294)
    Co-authored-by: Grace_Goslin <gosling.gos@gmail.com>

--- y2023/ChargedUp/src/main/java/com/gos/chargedup/Robot.java
+++ y2023/ChargedUp/src/main/java/com/gos/chargedup/Robot.java
@@ -47,6 +47,7 @@ public class Robot extends TimedRobot {
     public void robotInit() {
         // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
         // autonomous chooser on the dashboard.
+        m_pneumaticHub.enableCompressorAnalog(110, 120);
         m_robotContainer = new RobotContainer(() -> m_pneumaticHub.getPressure(Constants.PRESSURE_SENSOR_PORT));
     }
 
@@ -70,6 +71,7 @@ public class Robot extends TimedRobot {
 
         m_lowBatterVoltage.set(RobotController.getBatteryVoltage() < LOW_BATTERY_VOLTAGE);
         SmartDashboard.putNumber("Air Pressure", m_pneumaticHub.getPressure(Constants.PRESSURE_SENSOR_PORT));
+        SmartDashboard.putBoolean("Compressor On", m_pneumaticHub.getCompressor());
     }
 
 
