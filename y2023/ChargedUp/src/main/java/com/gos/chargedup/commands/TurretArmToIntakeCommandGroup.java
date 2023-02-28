3b607d29326bb6af8317325018c75a7446db944e
sienna-li <sieddy0116@gmail.com>
2023-02-28 18:57:19 -0500

    fixed errors in TurretArmToIntakeCommandGroup

--- y2023/ChargedUp/src/main/java/com/gos/chargedup/commands/TurretArmToIntakeCommandGroup.java
+++ y2023/ChargedUp/src/main/java/com/gos/chargedup/commands/TurretArmToIntakeCommandGroup.java
@@ -8,6 +8,6 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 public class TurretArmToIntakeCommandGroup extends SequentialCommandGroup {
     public TurretArmToIntakeCommandGroup(ArmSubsystem arm, TurretSubsystem turret) {
         addCommands(turret.commandTurretPID(0.0));
-        addCommands(arm.commandPivotArmDown());
+        addCommands(arm.commandPivotArmToAngleHold(ArmSubsystem.MIN_ANGLE_DEG));
     }
 }
