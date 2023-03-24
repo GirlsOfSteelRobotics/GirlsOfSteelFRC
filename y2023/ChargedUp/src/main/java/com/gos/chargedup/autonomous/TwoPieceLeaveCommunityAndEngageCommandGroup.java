d39a46e78e363e69353ae47db021869f9097edcb
sienna-li <sieddy0116@gmail.com>
2023-03-23 20:28:55 -0400

    new auto path

--- y2023/ChargedUp/src/main/java/com/gos/chargedup/autonomous/TwoPieceLeaveCommunityAndEngageCommandGroup.java
+++ y2023/ChargedUp/src/main/java/com/gos/chargedup/autonomous/TwoPieceLeaveCommunityAndEngageCommandGroup.java
@@ -10,7 +10,6 @@ import com.gos.chargedup.subsystems.ArmExtensionSubsystem;
 import com.gos.chargedup.subsystems.ArmPivotSubsystem;
 import com.gos.chargedup.subsystems.ChassisSubsystem;
 import com.gos.chargedup.subsystems.ClawSubsystem;
-import com.gos.chargedup.subsystems.TurretSubsystem;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -20,16 +19,30 @@ import java.util.HashMap;
 
 public class TwoPieceLeaveCommunityAndEngageCommandGroup extends SequentialCommandGroup {
     public TwoPieceLeaveCommunityAndEngageCommandGroup(ChassisSubsystem chassis, ArmPivotSubsystem armPivot, ArmExtensionSubsystem armExtension, ClawSubsystem claw, String path, AutoPivotHeight pivotHeightType, GamePieceType gamePieceType) {
-        PathPlannerTrajectory twoPieceLeaveAndEngage = PathPlanner.loadPath(path, Constants.DEFAULT_PATH_CONSTRAINTS, true);
-        Command driveAutoOnePieceAndLeave = chassis.ramseteAutoBuilder(new HashMap<>()).fullAuto(twoPieceLeaveAndEngage);
+        PathPlannerTrajectory twoPieceLeaveAndEngageBefore = PathPlanner.loadPath(path, Constants.DEFAULT_PATH_CONSTRAINTS, true);
+        Command driveAutoTwoPieceLeaveCommunityAndEngageBefore = chassis.ramseteAutoBuilder(new HashMap<>()).fullAuto(twoPieceLeaveAndEngageBefore);
+        PathPlannerTrajectory twoPieceLeaveAndEngageAfter = PathPlanner.loadPath(path, Constants.DEFAULT_PATH_CONSTRAINTS, true);
+        Command driveAutoTwoPieceLeaveCommunityAndEngageAfter = chassis.ramseteAutoBuilder(new HashMap<>()).fullAuto(twoPieceLeaveAndEngageAfter);
 
         //score
-        addCommands(new ScorePieceCommandGroup(armPivot, armExtension, claw, pivotHeightType, gamePieceType));
+        addCommands(new ScorePieceCommandGroup(armPivot, armExtension, claw, pivotHeightType, gamePieceType)
+            .andThen(CombinedCommandsUtil.goHome(armPivot, armExtension)));
+
+        //turn to start
+        addCommands(chassis.createTurnPID(twoPieceLeaveAndEngageBefore.getInitialPose().getRotation().getDegrees()));
 
         //drive out of community across engage
-        addCommands(driveAutoOnePieceAndLeave);
+        addCommands(driveAutoTwoPieceLeaveCommunityAndEngageBefore);
 
-        // TODO: Add your sequential commands in the super() call, e.g.
-        //           super(new OpenClawCommand(), new MoveArmCommand());
+        //grab piece
+        addCommands(armPivot.commandGoToGroundPickup());
+        addCommands(claw.createMoveClawIntakeInCommand());
+        addCommands(armPivot.commandHpPickupHold());
+
+        //turn to go back
+        addCommands(chassis.createTurnPID(twoPieceLeaveAndEngageAfter.getInitialPose().getRotation().getDegrees()));
+
+        //engage
+        addCommands(driveAutoTwoPieceLeaveCommunityAndEngageAfter);
     }
 }
