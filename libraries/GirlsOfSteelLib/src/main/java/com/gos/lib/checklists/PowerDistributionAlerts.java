8b488b6de265a51f014051907b0968c150daea1d
Grace_Goslin <gosling.gos@gmail.com>
2023-04-02 15:14:12 -0400

    Update alerts

--- libraries/GirlsOfSteelLib/src/main/java/com/gos/lib/checklists/PowerDistributionAlerts.java
+++ libraries/GirlsOfSteelLib/src/main/java/com/gos/lib/checklists/PowerDistributionAlerts.java
@@ -5,7 +5,12 @@ import edu.wpi.first.hal.PowerDistributionStickyFaults;
 import edu.wpi.first.wpilibj.PowerDistribution;
 import org.littletonrobotics.frc2023.util.Alert;
 
-@SuppressWarnings("PMD.AvoidDuplicateLiterals")
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@SuppressWarnings({"PMD.AvoidDuplicateLiterals", "PMD.CyclomaticComplexity"})
 public class PowerDistributionAlerts {
     private static final String ALERT_NAME = "power distribution";
     private static final String STICKY_ALERT_NAME = "power distribution (sticky) ";
@@ -14,10 +19,19 @@ public class PowerDistributionAlerts {
     private final Alert m_alert;
     private final Alert m_alertSticky;
 
+    private Set<Integer> m_ignoredBreakers;
+
     public PowerDistributionAlerts(PowerDistribution powerDistribution) {
+        this(powerDistribution, new ArrayList<>());
+    }
+
+    public PowerDistributionAlerts(PowerDistribution powerDistribution, List<Integer> ignoredBreakers) {
         m_powerDistribution = powerDistribution;
         m_alert = new Alert("power distribution", Alert.AlertType.ERROR);
         m_alertSticky = new Alert("power distribution (sticky)", Alert.AlertType.WARNING);
+
+        m_ignoredBreakers = new HashSet<>();
+        m_ignoredBreakers.addAll(ignoredBreakers);
     }
 
 
@@ -28,92 +42,92 @@ public class PowerDistributionAlerts {
     }
 
     @SuppressWarnings({"PMD.NcssCount", "PMD.CognitiveComplexity", "PMD.CyclomaticComplexity", "PMD.NPathComplexity"})
-    public void checkFaults() {
+    private void checkFaults() {
         StringBuilder alertMessageBuilder = new StringBuilder(700);
         alertMessageBuilder.append(ALERT_NAME);
 
         PowerDistributionFaults faults = m_powerDistribution.getFaults();
 
-        if (faults.Channel0BreakerFault) {
-            alertMessageBuilder.append(" channel 0 breaker error");
+        if (faults.Channel0BreakerFault && !m_ignoredBreakers.contains(0)) {
+            alertMessageBuilder.append("\nchannel 0 breaker error");
         }
-        if (faults.Channel1BreakerFault) {
-            alertMessageBuilder.append(" channel 1 breaker error");
+        if (faults.Channel1BreakerFault && !m_ignoredBreakers.contains(1)) {
+            alertMessageBuilder.append("\nchannel 1 breaker error");
         }
-        if (faults.Channel2BreakerFault) {
-            alertMessageBuilder.append(" channel 2 breaker error");
+        if (faults.Channel2BreakerFault && !m_ignoredBreakers.contains(2)) {
+            alertMessageBuilder.append("\nchannel 2 breaker error");
         }
-        if (faults.Channel3BreakerFault) {
-            alertMessageBuilder.append(" channel 3 breaker error");
+        if (faults.Channel3BreakerFault && !m_ignoredBreakers.contains(3)) {
+            alertMessageBuilder.append("\nchannel 3 breaker error");
         }
-        if (faults.Channel4BreakerFault) {
-            alertMessageBuilder.append(" channel 4 breaker error");
+        if (faults.Channel4BreakerFault && !m_ignoredBreakers.contains(4)) {
+            alertMessageBuilder.append("\nchannel 4 breaker error");
         }
-        if (faults.Channel5BreakerFault) {
-            alertMessageBuilder.append(" channel 5 breaker error");
+        if (faults.Channel5BreakerFault && !m_ignoredBreakers.contains(5)) {
+            alertMessageBuilder.append("\nchannel 5 breaker error");
         }
-        if (faults.Channel6BreakerFault) {
-            alertMessageBuilder.append(" channel 6 breaker error");
+        if (faults.Channel6BreakerFault && !m_ignoredBreakers.contains(6)) {
+            alertMessageBuilder.append("\nchannel 6 breaker error");
         }
-        if (faults.Channel7BreakerFault) {
-            alertMessageBuilder.append(" channel 7 breaker error");
+        if (faults.Channel7BreakerFault && !m_ignoredBreakers.contains(7)) {
+            alertMessageBuilder.append("\nchannel 7 breaker error");
         }
-        if (faults.Channel8BreakerFault) {
-            alertMessageBuilder.append(" channel 8 breaker error");
+        if (faults.Channel8BreakerFault && !m_ignoredBreakers.contains(8)) {
+            alertMessageBuilder.append("\nchannel 8 breaker error");
         }
-        if (faults.Channel9BreakerFault) {
-            alertMessageBuilder.append(" channel 9 breaker error");
+        if (faults.Channel9BreakerFault && !m_ignoredBreakers.contains(9)) {
+            alertMessageBuilder.append("\nchannel 9 breaker error");
         }
-        if (faults.Channel10BreakerFault) {
-            alertMessageBuilder.append(" channel 10 breaker error");
+        if (faults.Channel10BreakerFault && !m_ignoredBreakers.contains(10)) {
+            alertMessageBuilder.append("\nchannel 10 breaker error");
         }
-        if (faults.Channel11BreakerFault) {
-            alertMessageBuilder.append(" channel 11 breaker error");
+        if (faults.Channel11BreakerFault && !m_ignoredBreakers.contains(11)) {
+            alertMessageBuilder.append("\nchannel 11 breaker error");
         }
-        if (faults.Channel12BreakerFault) {
-            alertMessageBuilder.append(" channel 12 breaker error");
+        if (faults.Channel12BreakerFault && !m_ignoredBreakers.contains(12)) {
+            alertMessageBuilder.append("\nchannel 12 breaker error");
         }
-        if (faults.Channel13BreakerFault) {
-            alertMessageBuilder.append(" channel 13 breaker error");
+        if (faults.Channel13BreakerFault && !m_ignoredBreakers.contains(13)) {
+            alertMessageBuilder.append("\nchannel 13 breaker error");
         }
-        if (faults.Channel14BreakerFault) {
-            alertMessageBuilder.append(" channel 14 breaker error");
+        if (faults.Channel14BreakerFault && !m_ignoredBreakers.contains(14)) {
+            alertMessageBuilder.append("\nchannel 14 breaker error");
         }
-        if (faults.Channel15BreakerFault) {
-            alertMessageBuilder.append(" channel 15 breaker error");
+        if (faults.Channel15BreakerFault && !m_ignoredBreakers.contains(15)) {
+            alertMessageBuilder.append("\nchannel 15 breaker error");
         }
-        if (faults.Channel16BreakerFault) {
-            alertMessageBuilder.append(" channel 16 breaker error");
+        if (faults.Channel16BreakerFault && !m_ignoredBreakers.contains(16)) {
+            alertMessageBuilder.append("\nchannel 16 breaker error");
         }
-        if (faults.Channel17BreakerFault) {
-            alertMessageBuilder.append(" channel 17 breaker error");
+        if (faults.Channel17BreakerFault && !m_ignoredBreakers.contains(17)) {
+            alertMessageBuilder.append("\nchannel 17 breaker error");
         }
-        if (faults.Channel18BreakerFault) {
-            alertMessageBuilder.append(" channel 18 breaker error");
+        if (faults.Channel18BreakerFault && !m_ignoredBreakers.contains(18)) {
+            alertMessageBuilder.append("\nchannel 18 breaker error");
         }
-        if (faults.Channel19BreakerFault) {
-            alertMessageBuilder.append(" channel 19 breaker error");
+        if (faults.Channel19BreakerFault && !m_ignoredBreakers.contains(19)) {
+            alertMessageBuilder.append("\nchannel 19 breaker error");
         }
-        if (faults.Channel20BreakerFault) {
-            alertMessageBuilder.append(" channel 20 breaker error");
+        if (faults.Channel20BreakerFault && !m_ignoredBreakers.contains(20)) {
+            alertMessageBuilder.append("\nchannel 20 breaker error");
         }
-        if (faults.Channel21BreakerFault) {
-            alertMessageBuilder.append(" channel 21 breaker error");
+        if (faults.Channel21BreakerFault && !m_ignoredBreakers.contains(21)) {
+            alertMessageBuilder.append("\nchannel 21 breaker error");
         }
-        if (faults.Channel22BreakerFault) {
-            alertMessageBuilder.append(" channel 22 breaker error");
+        if (faults.Channel22BreakerFault && !m_ignoredBreakers.contains(22)) {
+            alertMessageBuilder.append("\nchannel 22 breaker error");
         }
-        if (faults.Channel23BreakerFault) {
-            alertMessageBuilder.append(" channel 23 breaker error");
+        if (faults.Channel23BreakerFault && !m_ignoredBreakers.contains(23)) {
+            alertMessageBuilder.append("\nchannel 23 breaker error");
         }
         if (faults.Brownout) {
-            alertMessageBuilder.append(" brownout error");
+            alertMessageBuilder.append("\nbrownout error");
         }
         if (faults.CanWarning) {
-            alertMessageBuilder.append("can warning error");
+            alertMessageBuilder.append("\ncan warning error");
         }
         if (faults.HardwareFault) {
-            alertMessageBuilder.append("Hardware fault error");
+            alertMessageBuilder.append("\nHardware fault error");
         }
         String alertMessage = alertMessageBuilder.toString();
         m_alert.setText(alertMessage);
@@ -122,95 +136,95 @@ public class PowerDistributionAlerts {
     }
 
     @SuppressWarnings({"PMD.NcssCount", "PMD.CognitiveComplexity", "PMD.CyclomaticComplexity", "PMD.NPathComplexity"})
-    public void checkStickyFaults() {
+    private void checkStickyFaults() {
         StringBuilder alertMessageBuilder = new StringBuilder(700);
         alertMessageBuilder.append(STICKY_ALERT_NAME);
 
         PowerDistributionStickyFaults stickyFaults = m_powerDistribution.getStickyFaults();
 
-        if (stickyFaults.Channel0BreakerFault) {
-            alertMessageBuilder.append("Channel 0 breaker error");
+        if (stickyFaults.Channel0BreakerFault && !m_ignoredBreakers.contains(0)) {
+            alertMessageBuilder.append("\nchannel 0 breaker error");
         }
-        if (stickyFaults.Channel1BreakerFault) {
-            alertMessageBuilder.append("Channel 1 breaker error");
+        if (stickyFaults.Channel1BreakerFault && !m_ignoredBreakers.contains(1)) {
+            alertMessageBuilder.append("\nchannel 1 breaker error");
         }
-        if (stickyFaults.Channel2BreakerFault) {
-            alertMessageBuilder.append("Channel 2 breaker error");
+        if (stickyFaults.Channel2BreakerFault && !m_ignoredBreakers.contains(2)) {
+            alertMessageBuilder.append("\nchannel 2 breaker error");
         }
-        if (stickyFaults.Channel3BreakerFault) {
-            alertMessageBuilder.append("Channel 3 breaker fault");
+        if (stickyFaults.Channel3BreakerFault && !m_ignoredBreakers.contains(3)) {
+            alertMessageBuilder.append("\nchannel 3 breaker error");
         }
-        if (stickyFaults.Channel4BreakerFault) {
-            alertMessageBuilder.append("Channel 4 breaker fault");
+        if (stickyFaults.Channel4BreakerFault && !m_ignoredBreakers.contains(4)) {
+            alertMessageBuilder.append("\nchannel 4 breaker error");
         }
-        if (stickyFaults.Channel5BreakerFault) {
-            alertMessageBuilder.append("Channel 5 breaker fault");
+        if (stickyFaults.Channel5BreakerFault && !m_ignoredBreakers.contains(5)) {
+            alertMessageBuilder.append("\nchannel 5 breaker error");
         }
-        if (stickyFaults.Channel6BreakerFault) {
-            alertMessageBuilder.append("Channel 6 breaker fault");
+        if (stickyFaults.Channel6BreakerFault && !m_ignoredBreakers.contains(6)) {
+            alertMessageBuilder.append("\nchannel 6 breaker error");
         }
-        if (stickyFaults.Channel7BreakerFault) {
-            alertMessageBuilder.append("Channel 7 breaker fault");
+        if (stickyFaults.Channel7BreakerFault && !m_ignoredBreakers.contains(7)) {
+            alertMessageBuilder.append("\nchannel 7 breaker error");
         }
-        if (stickyFaults.Channel8BreakerFault) {
-            alertMessageBuilder.append("Channel 8 breaker fault");
+        if (stickyFaults.Channel8BreakerFault && !m_ignoredBreakers.contains(8)) {
+            alertMessageBuilder.append("\nchannel 8 breaker error");
         }
-        if (stickyFaults.Channel9BreakerFault) {
-            alertMessageBuilder.append("Channel 9 breaker fault");
+        if (stickyFaults.Channel9BreakerFault && !m_ignoredBreakers.contains(9)) {
+            alertMessageBuilder.append("\nchannel 9 breaker error");
         }
-        if (stickyFaults.Channel10BreakerFault) {
-            alertMessageBuilder.append("Channel 10 breaker fault");
+        if (stickyFaults.Channel10BreakerFault && !m_ignoredBreakers.contains(10)) {
+            alertMessageBuilder.append("\nchannel 10 breaker error");
         }
-        if (stickyFaults.Channel11BreakerFault) {
-            alertMessageBuilder.append("Channel 11 breaker fault");
+        if (stickyFaults.Channel11BreakerFault && !m_ignoredBreakers.contains(11)) {
+            alertMessageBuilder.append("\nchannel 11 breaker error");
         }
-        if (stickyFaults.Channel12BreakerFault) {
-            alertMessageBuilder.append("Channel 12 breaker fault");
+        if (stickyFaults.Channel12BreakerFault && !m_ignoredBreakers.contains(12)) {
+            alertMessageBuilder.append("\nchannel 12 breaker error");
         }
-        if (stickyFaults.Channel13BreakerFault) {
-            alertMessageBuilder.append("Channel 13 breaker fault");
+        if (stickyFaults.Channel13BreakerFault && !m_ignoredBreakers.contains(13)) {
+            alertMessageBuilder.append("\nchannel 13 breaker error");
         }
-        if (stickyFaults.Channel14BreakerFault) {
-            alertMessageBuilder.append("Channel 14 breaker fault");
+        if (stickyFaults.Channel14BreakerFault && !m_ignoredBreakers.contains(14)) {
+            alertMessageBuilder.append("\nchannel 14 breaker error");
         }
-        if (stickyFaults.Channel15BreakerFault) {
-            alertMessageBuilder.append("Channel 15 breaker fault");
+        if (stickyFaults.Channel15BreakerFault && !m_ignoredBreakers.contains(15)) {
+            alertMessageBuilder.append("\nchannel 15 breaker error");
         }
-        if (stickyFaults.Channel16BreakerFault) {
-            alertMessageBuilder.append("Channel 16 breaker fault");
+        if (stickyFaults.Channel16BreakerFault && !m_ignoredBreakers.contains(16)) {
+            alertMessageBuilder.append("\nchannel 16 breaker error");
         }
-        if (stickyFaults.Channel17BreakerFault) {
-            alertMessageBuilder.append("Channel 17 breaker fault");
+        if (stickyFaults.Channel17BreakerFault && !m_ignoredBreakers.contains(17)) {
+            alertMessageBuilder.append("\nchannel 17 breaker error");
         }
-        if (stickyFaults.Channel18BreakerFault) {
-            alertMessageBuilder.append("Channel 18 breaker fault");
+        if (stickyFaults.Channel18BreakerFault && !m_ignoredBreakers.contains(18)) {
+            alertMessageBuilder.append("\nchannel 18 breaker error");
         }
-        if (stickyFaults.Channel19BreakerFault) {
-            alertMessageBuilder.append("Channel 19 breaker fault");
+        if (stickyFaults.Channel19BreakerFault && !m_ignoredBreakers.contains(19)) {
+            alertMessageBuilder.append("\nchannel 19 breaker error");
         }
-        if (stickyFaults.Channel20BreakerFault) {
-            alertMessageBuilder.append("Channel 20 breaker fault");
+        if (stickyFaults.Channel20BreakerFault && !m_ignoredBreakers.contains(20)) {
+            alertMessageBuilder.append("\nchannel 20 breaker error");
         }
-        if (stickyFaults.Channel21BreakerFault) {
-            alertMessageBuilder.append("Channel 21 breaker fault");
+        if (stickyFaults.Channel21BreakerFault && !m_ignoredBreakers.contains(21)) {
+            alertMessageBuilder.append("\nchannel 21 breaker error");
         }
-        if (stickyFaults.Channel22BreakerFault) {
-            alertMessageBuilder.append("Channel 22 breaker fault");
+        if (stickyFaults.Channel22BreakerFault && !m_ignoredBreakers.contains(22)) {
+            alertMessageBuilder.append("\nchannel 22 breaker error");
         }
-        if (stickyFaults.Channel23BreakerFault) {
-            alertMessageBuilder.append("Channel 23 breaker fault");
+        if (stickyFaults.Channel23BreakerFault && !m_ignoredBreakers.contains(23)) {
+            alertMessageBuilder.append("\nchannel 23 breaker error");
         }
         if (stickyFaults.Brownout) {
-            alertMessageBuilder.append("Brownout fault");
+            alertMessageBuilder.append("\nBrownout fault");
         }
         if (stickyFaults.CanWarning) {
-            alertMessageBuilder.append("Can Warning fault");
+            alertMessageBuilder.append("\nCan Warning fault");
         }
         if (stickyFaults.CanBusOff) {
-            alertMessageBuilder.append("Can Bus Off fault");
+            alertMessageBuilder.append("\nCan Bus Off fault");
         }
         if (stickyFaults.HasReset) {
-            alertMessageBuilder.append("Has Reset fault");
+            alertMessageBuilder.append("\nHas Reset fault");
         }
         String alertMessage = alertMessageBuilder.toString();
         m_alertSticky.setText(alertMessage);
