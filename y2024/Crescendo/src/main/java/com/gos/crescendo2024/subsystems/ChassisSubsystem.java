f6e23d625510b86d994acea052e779e4a361b27e
GosProgrammers <girlsofsteelrobotics@gmail.com>
2024-01-06 16:11:51 -0800

    Add robot project

---
+++ y2024/Crescendo/src/main/java/com/gos/crescendo2024/subsystems/ChassisSubsystem.java
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package com.gos.crescendo2024.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class ChassisSubsystem extends SubsystemBase {
+
+    @SuppressWarnings("PMD.UnnecessaryConstructor") // TODO remove
+    public ChassisSubsystem() {
+    }
+
+    @Override
+    public void periodic() {
+    }
+
+
+    @Override
+    public void simulationPeriodic() {
+    }
+
+    /////////////////////////////////////
+    // Checklists
+    /////////////////////////////////////
+
+
+    /////////////////////////////////////
+    // Command Factories
+    /////////////////////////////////////
+}
