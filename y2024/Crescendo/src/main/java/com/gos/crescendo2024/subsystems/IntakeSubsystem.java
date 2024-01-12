95e5eb1ae5f0ede223667ef683db704e9b1702e4
rachelkim <tinymousekim@gmail.com>
2024-01-11 19:14:25 -0500

    We began intake subsystem!

---
+++ y2024/Crescendo/src/main/java/com/gos/crescendo2024/subsystems/IntakeSubsystem.java
@@ -0,0 +1,16 @@
+package com.gos.crescendo2024.subsystems;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class IntakeSubsystem extends SubsystemBase {
+    double INTAKEANGLE;
+
+    public IntakeSubsystem() {
+        // TODO: Set the default command, if any, for this subsystem by calling setDefaultCommand(command)
+        //       in the constructor or in the robot coordination class, such as RobotContainer.
+        //       Also, you can call addChild(name, sendableChild) to associate sendables with the subsystem
+        //       such as SpeedControllers, Encoders, DigitalInputs, etc.
+    }
+}
+
