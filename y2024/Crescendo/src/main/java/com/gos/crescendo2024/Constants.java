3338ada15f1de115e2404302b223b98a0d8b73a5
sienna-li <114195125+sienna-li@users.noreply.github.com>
2024-01-11 19:58:32 -0500

    swerve subsystem + command (#384)

--- y2024/Crescendo/src/main/java/com/gos/crescendo2024/Constants.java
+++ y2024/Crescendo/src/main/java/com/gos/crescendo2024/Constants.java
@@ -18,4 +18,15 @@ public final class Constants {
     public static final int DRIVER_JOYSTICK = 0;
 
     // CAN ID's
+    public static final int PIGEON_PORT = 20;
+
+    public static final int FRONT_LEFT_WHEEL = 21;
+    public static final int FRONT_RIGHT_WHEEL = 22;
+    public static final int BACK_LEFT_WHEEL = 23;
+    public static final int BACK_RIGHT_WHEEL = 24;
+
+    public static final int FRONT_LEFT_AZIMUTH = 25;
+    public static final int FRONT_RIGHT_AZIMUTH = 26;
+    public static final int BACK_LEFT_AZIMUTH = 27;
+    public static final int BACK_RIGHT_AZIMUTH = 28;
 }
