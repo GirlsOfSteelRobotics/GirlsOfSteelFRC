07a0cdcbac507c46b56336760452d23fe4d2e47d
Anuva12 <40363420+Anuva12@users.noreply.github.com>
2024-01-20 15:22:27 -0500

    Arm Pivot Angle Superstructure fixed  (#414)
    * Unversioned Files
    
    * launch commits
    
    * began move arm to angle - not tuned nor tested
    
    * arm angle pid
    
    * arm angle pid again
    
    * arm angle pid again
    
    * super structure
    
    * Arm Pivot angles fixed
    checkstyled
    
    * checkstyled again
    
    ---------
    
    Co-authored-by: rachelkim <tinymousekim@gmail.com>
    Co-authored-by: egreenbaum <egreenbaum86@gmail.com>
    Co-authored-by: sienna-li <sieddy0116@gmail.com>

--- y2024/Crescendo/src/main/java/com/gos/crescendo2024/RobotContainer.java
+++ y2024/Crescendo/src/main/java/com/gos/crescendo2024/RobotContainer.java
@@ -13,10 +13,13 @@ import com.gos.crescendo2024.subsystems.ArmPivotSubsystem;
 import com.gos.crescendo2024.subsystems.ChassisSubsystem;
 import com.gos.crescendo2024.subsystems.IntakeSubsystem;
 import com.gos.crescendo2024.subsystems.ShooterSubsystem;
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.simulation.DriverStationSim;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -61,6 +64,8 @@ public class RobotContainer {
 
         createTestCommands();
 
+        SmartDashboard.putData("super structure", new SuperstructureSendable());
+
         if (RobotBase.isSimulation()) {
             DriverStationSim.setEnabled(true);
         }
@@ -80,7 +85,6 @@ public class RobotContainer {
         shuffleboardTab.add("Chassis to -180", m_chassisSubsystem.createTurnToAngleCommand(-180));
         shuffleboardTab.add("Chassis to -45", m_chassisSubsystem.createTurnToAngleCommand(-45));
 
-        //hard coded sorry will fix asap
         shuffleboardTab.add("David Drive", new DavidDriveSwerve(m_chassisSubsystem, m_driverController));
         shuffleboardTab.add("Turn to point drive", new TurnToPointSwerveDrive(m_chassisSubsystem, m_driverController, FieldConstants.Speaker.CENTER_SPEAKER_OPENING));
 
@@ -118,4 +122,27 @@ public class RobotContainer {
         // An example command will be run in autonomous
         return m_autonomousFactory.getSelectedAutonomous();
     }
+
+    private class SuperstructureSendable implements Sendable {
+
+        @Override
+        public void initSendable(SendableBuilder builder) {
+            builder.setSmartDashboardType(SmartDashboardNames.SUPER_STRUCTURE);
+
+            builder.addDoubleProperty(
+                SmartDashboardNames.PIVOT_MOTOR_ANGLE, m_armPivotSubsystem::getAngle, null);
+            builder.addDoubleProperty(
+                SmartDashboardNames.GOAL_ANGLE, m_armPivotSubsystem::getArmAngleGoal, null);
+            builder.addDoubleProperty(
+                SmartDashboardNames.SHOOTER_MOTOR_PERCENTAGE, m_shooterSubsystem::getShooterMotorPercentage, null);
+            builder.addDoubleProperty(
+                SmartDashboardNames.PIVOT_MOTOR_PERCENTAGE, m_armPivotSubsystem::getPivotMotorPercentage, null);
+            builder.addBooleanProperty(
+                SmartDashboardNames.HAS_GAME_PIECE, m_intakeSubsystem::hasGamePiece, null);
+            builder.addDoubleProperty(
+                SmartDashboardNames.INTAKE_MOTOR_PERCENTAGE, m_intakeSubsystem::getIntakeMotorPercentage, null);
+
+        }
+    }
+
 }
