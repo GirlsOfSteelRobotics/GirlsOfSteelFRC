c0fe85d8d1f8e2855689da5c13705c38f735d249
GosProgrammers <girlsofsteelrobotics@gmail.com>
2024-02-05 15:25:29 -0800

    Lint

--- y2024/Crescendo/src/main/java/com/gos/crescendo2024/commands/SpeakerAimAndShootCommand.java
+++ y2024/Crescendo/src/main/java/com/gos/crescendo2024/commands/SpeakerAimAndShootCommand.java
@@ -8,7 +8,6 @@ import com.gos.crescendo2024.subsystems.ShooterSubsystem;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 
 import java.util.function.DoubleSupplier;
