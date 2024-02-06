87d6b551e71ab478be940a1cb612bd242a4727e9
egreenbaum <egreenbaum86@gmail.com>
2024-02-05 19:37:40 -0500

    changed camera name

--- y2024/Crescendo/src/main/java/com/gos/crescendo2024/ObjectDetection.java
+++ y2024/Crescendo/src/main/java/com/gos/crescendo2024/ObjectDetection.java
@@ -34,7 +34,7 @@ public class ObjectDetection {
     private final PhotonCameraSim m_cameraSim;
 
     public ObjectDetection() {
-        m_photonCamera = new PhotonCamera("objectDetection");
+        m_photonCamera = new PhotonCamera("TestLimelight1");
 
         TargetModel targetModel = new TargetModel(Units.inchesToMeters(18));
 
