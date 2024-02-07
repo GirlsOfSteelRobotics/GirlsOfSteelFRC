b6eb11cd3e81123dfce0ec89e6b9f89f9d8bf8a7
egreenbaum <egreenbaum86@gmail.com>
2024-02-06 21:03:17 -0500

    "iTs dEgReEs nOt RoTatIoNs" changed arm sysid units

--- y2024/Crescendo/src/main/java/com/gos/crescendo2024/subsystems/sysid/ArmPivotSysId.java
+++ y2024/Crescendo/src/main/java/com/gos/crescendo2024/subsystems/sysid/ArmPivotSysId.java
@@ -13,8 +13,8 @@ import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import java.util.function.Consumer;
 
 import static edu.wpi.first.units.MutableMeasure.mutable;
-import static edu.wpi.first.units.Units.Rotations;
-import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.DegreesPerSecond;
 import static edu.wpi.first.units.Units.Volts;
 
 public class ArmPivotSysId {
@@ -22,8 +22,8 @@ public class ArmPivotSysId {
     private final ArmPivotSubsystem m_armPivot;
 
     private final MutableMeasure<Voltage> m_appliedVoltage = mutable(Volts.of(0));
-    private final MutableMeasure<Angle> m_rotations = mutable(Rotations.of(0));
-    private final MutableMeasure<Velocity<Angle>> m_velocity = mutable(RotationsPerSecond.of(0));
+    private final MutableMeasure<Angle> m_angle = mutable(Degrees.of(0));
+    private final MutableMeasure<Velocity<Angle>> m_velocity = mutable(DegreesPerSecond.of(0));
 
     public ArmPivotSysId(ArmPivotSubsystem armPivot) {
         m_armPivot = armPivot;
@@ -45,9 +45,9 @@ public class ArmPivotSysId {
                 .voltage(
                     m_appliedVoltage.mut_replace(
                         m_armPivot.replaceVoltage(), Volts))
-                .angularPosition(m_rotations.mut_replace(m_armPivot.getEncoderPos(), Rotations))
+                .angularPosition(m_angle.mut_replace(m_armPivot.getEncoderPos(), Degrees))
                 .angularVelocity(
-                    m_velocity.mut_replace(m_armPivot.getEncoderVel(), RotationsPerSecond));
+                    m_velocity.mut_replace(m_armPivot.getEncoderVel(), DegreesPerSecond));
         };
     }
 
