389fe7326b5c7126cbbcd6d6f47d2dd10f3ca2d8
PJ Reiniger <pj.reiniger@gmail.com>
2024-02-11 23:28:14 -0500

    Undo SB dashboard changes

--- y2024/CrescendoDashboard/src/main/java/com/gos/crescendo/shuffleboard/super_structure/SuperStructureController.java
+++ y2024/CrescendoDashboard/src/main/java/com/gos/crescendo/shuffleboard/super_structure/SuperStructureController.java
@@ -1,11 +1,12 @@
 package com.gos.crescendo.shuffleboard.super_structure;
 
+import com.gos.crescendo.shuffleboard.Utils;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.DoubleBinding;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Arc;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.transform.Rotate;
@@ -13,8 +14,8 @@ import javafx.scene.transform.Scale;
 
 public class SuperStructureController {
 
-    private static final double MAX_WIDTH = 1; // TODO figure out real value
-    private static final double MAX_HEIGHT = 1; // TODO figure out real value
+    private static final double MAX_WIDTH = 15; // TODO figure out real value
+    private static final double MAX_HEIGHT = 15; // TODO figure out real value
 
     private static final double CHASSIS_HEIGHT = 2;
     private static final double CHASSIS_WIDTH = 8;
@@ -30,17 +31,17 @@ public class SuperStructureController {
 
 
     private static final double CHASSIS_X = 3;
-    private static final double CHASSIS_Y = MAX_HEIGHT - CHASSIS_HEIGHT - 4;
-    private static final double PIVOT_MOTOR_X = CHASSIS_X + 0.5;
-    private static final double PIVOT_MOTOR_Y = CHASSIS_Y;
-    private static final double ARM_RECT_X = CHASSIS_X;
-    private static final double ARM_RECT_Y = CHASSIS_Y - ARM_RECT_HEIGHT;
-    private static final double SHOOTER_RECT_X = ARM_RECT_X - 1;
-    private static final double SHOOTER_RECT_Y = CHASSIS_Y - ARM_RECT_HEIGHT - SHOOTER_RECT_HEIGHT;
-    private static final double SHOOTER_MOTOR_X = SHOOTER_RECT_X - SHOOTER_MOTOR_RADIUS / 2.0;
-    private static final double SHOOTER_MOTOR_Y = SHOOTER_RECT_Y + (SHOOTER_RECT_HEIGHT / 2);
-    private static final double INTAKE_MOTOR_X = SHOOTER_RECT_X + SHOOTER_RECT_WIDTH + INTAKE_MOTOR_RADIUS / 2.0;
-    private static final double INTAKE_MOTOR_Y = SHOOTER_RECT_Y + (SHOOTER_RECT_HEIGHT / 2);
+    private static final double CHASSIS_Y = 9;
+    private static final double PIVOT_MOTOR_X = 3.5;
+    private static final double PIVOT_MOTOR_Y = 9;
+    private static final double ARM_RECT_X = 3;
+    private static final double ARM_RECT_Y = 3;
+    private static final double SHOOTER_RECT_X = 2;
+    private static final double SHOOTER_RECT_Y = 2;
+    private static final double SHOOTER_MOTOR_X = 1.75;
+    private static final double SHOOTER_MOTOR_Y = 2.5;
+    private static final double INTAKE_MOTOR_X = 8.25;
+    private static final double INTAKE_MOTOR_Y = 2.5;
     private static final double PIVOT_ANGLE_GOAL_X = ARM_RECT_X;
     private static final double PIVOT_ANGLE_GOAL_Y = ARM_RECT_Y;
 
@@ -49,7 +50,7 @@ public class SuperStructureController {
 
     @FXML
     private Pane m_pane;
-    
+
     @FXML
     private Rectangle m_chassis;
 
@@ -71,12 +72,9 @@ public class SuperStructureController {
     @FXML
     private Rectangle m_pivotAngleGoal;
 
-
-
     private Rotate m_armRectRotation;
 
     private Rotate m_pivotAngleGoalRotation;
-
 
     @FXML
     public void initialize() {
@@ -128,19 +126,43 @@ public class SuperStructureController {
         m_pivotAngleGoal.setHeight(PIVOT_ANGLE_GOAL_HEIGHT);
         m_pivotAngleGoal.setWidth(PIVOT_ANGLE_GOAL_WIDTH);
 
+        m_armRectRotation = new Rotate();
+        m_armRectRotation.pivotXProperty().bind(Bindings.createObjectBinding(() -> m_armRect.getX() + ARM_RECT_WIDTH, m_armRect.xProperty()));
+        m_armRectRotation.pivotYProperty().bind(Bindings.createObjectBinding(() -> m_armRect.getY() + ARM_RECT_HEIGHT, m_armRect.yProperty()));
+        m_armRect.getTransforms().add(m_armRectRotation);
+        m_shooterRect.getTransforms().add(m_armRectRotation);
+        m_intakeMotor.getTransforms().add(m_armRectRotation);
+        m_shooterMotor.getTransforms().add(m_armRectRotation);
 
-
-    m_armRectRotation = new Rotate();
-    m_armRect.getTransforms().add(m_armRectRotation);
-
-    m_pivotAngleGoalRotation = new Rotate();
-    m_pivotAngleGoal.getTransforms().add(m_pivotAngleGoalRotation);
+        m_pivotAngleGoalRotation = new Rotate();
+        m_pivotAngleGoalRotation.pivotXProperty().bind(Bindings.createObjectBinding(() -> m_armRect.getX() + ARM_RECT_WIDTH, m_armRect.xProperty()));
+        m_pivotAngleGoalRotation.pivotYProperty().bind(Bindings.createObjectBinding(() -> m_armRect.getY() + ARM_RECT_HEIGHT, m_armRect.yProperty()));
+        m_pivotAngleGoal.getTransforms().add(m_pivotAngleGoalRotation);
 
     }
 
 
     public void updateSuperStructure(SuperStructureData superStructureData) {
-        // TODO implement
+
+        m_armRectRotation.setAngle(90 - superStructureData.getPivotMotorAngle());
+        m_pivotAngleGoalRotation.setAngle(90 - superStructureData.getGoalAngle());
+
+        m_pivotMotor.setFill(Utils.getMotorColor(superStructureData.getPivotMotorPercentage()));
+        m_shooterMotor.setFill(Utils.getMotorColor(superStructureData.getShooterMotorPercentage()));
+        m_intakeMotor.setFill(Utils.getMotorColor(superStructureData.getIntakeMotorPercentage()));
+
+        if (superStructureData.isHasGamePiece()) {
+            m_shooterRect.setFill(Color.ORANGE);
+        }
+        else {
+            m_shooterRect.setFill(Color.TRANSPARENT);
+        }
+
+        if (superStructureData.getGoalAngle() == Double.MIN_VALUE) {
+            m_pivotAngleGoal.setStroke(Color.TRANSPARENT);
+        } else {
+            m_pivotAngleGoal.setStroke(Color.BLACK);
+        }
     }
 
 
