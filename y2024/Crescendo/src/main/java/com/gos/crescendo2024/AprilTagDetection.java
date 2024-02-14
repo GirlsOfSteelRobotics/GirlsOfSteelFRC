c22094a28165c8f10056f00114ba1289d1ee95ad
rachelkim <tinymousekim@gmail.com>
2024-02-13 20:19:48 -0500

    Parallel is deadline

--- y2024/Crescendo/src/main/java/com/gos/crescendo2024/AprilTagDetection.java
+++ y2024/Crescendo/src/main/java/com/gos/crescendo2024/AprilTagDetection.java
@@ -49,4 +49,28 @@ public class AprilTagDetection {
     public void updateAprilTagSimulation(Pose2d chassisLocation) {
         m_visionSim.update(chassisLocation);
     }
+    public Matrix<N3, N1> getEstimationStdDevs(Pose2d estimatedPose) {
+        var estStdDevs = kSingleTagStdDevs;
+        var targets = getLatestResult().getTargets();
+        int numTags = 0;
+        double avgDist = 0;
+        for (var tgt : targets) {
+            var tagPose = photonEstimator.getFieldTags().getTagPose(tgt.getFiducialId());
+            if (tagPose.isEmpty()) continue;
+            numTags++;
+            avgDist +=
+                tagPose.get().toPose2d().getTranslation().getDistance(estimatedPose.getTranslation());
+        }
+        if (numTags == 0) return estStdDevs;
+        avgDist /= numTags;
+        // Decrease std devs if multiple targets are visible
+        if (numTags > 1) estStdDevs = kMultiTagStdDevs;
+        // Increase std devs based on (average) distance
+        if (numTags == 1 && avgDist > 4)
+            estStdDevs = VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
+        else estStdDevs = estStdDevs.times(1 + (avgDist * avgDist / 30));
+
+        return estStdDevs;
+    }
+
 }
