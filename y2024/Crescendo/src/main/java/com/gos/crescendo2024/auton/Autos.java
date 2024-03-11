f8020d2e463d82774e5d1862ac53ce4f7f71a89d
PJ Reiniger <pj.reiniger@gmail.com>
2024-03-11 17:41:02 -0400

    Make new auto lights (#509)

--- y2024/Crescendo/src/main/java/com/gos/crescendo2024/auton/Autos.java
+++ y2024/Crescendo/src/main/java/com/gos/crescendo2024/auton/Autos.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 
 public final class Autos {
@@ -28,39 +29,39 @@ public final class Autos {
 
     public enum AutoModes {
         //four note auto
-        FOUR_NOTE_012("FourNoteAmpSide012Choreo", StartPosition.STARTING_LOCATION_AMP_SIDE, 4),
-        FOUR_NOTE_034("FourNoteAmpSide034Choreo", StartPosition.STARTING_LOCATION_AMP_SIDE, 4),
+        FOUR_NOTE_012("FourNoteAmpSide012Choreo", StartPosition.STARTING_LOCATION_AMP_SIDE, List.of(0, 1, 2)),
+        FOUR_NOTE_034("FourNoteAmpSide034Choreo", StartPosition.STARTING_LOCATION_AMP_SIDE, List.of(0, 3, 4)),
         // Three Note
-        THREE_NOTE_12("ThreeNoteMiddle12Choreo", StartPosition.STARTING_LOCATION_MIDDLE, 3),
-        THREE_NOTE_76("ThreeNotesSourceSide76Choreo", StartPosition.STARTING_LOCATION_SOURCE_SIDE, 3),
+        THREE_NOTE_12("ThreeNoteMiddle12Choreo", StartPosition.STARTING_LOCATION_MIDDLE, List.of(1, 2)),
+        THREE_NOTE_76("ThreeNotesSourceSide76Choreo", StartPosition.STARTING_LOCATION_SOURCE_SIDE, List.of(7, 6)),
         //two note autos - Amp Side
         //two note autos - middle
-        TWO_NOTE_MIDDLE_1("TwoNoteMiddle1Choreo", StartPosition.STARTING_LOCATION_MIDDLE, 2),
-        TWO_NOTE_MIDDLE_2("TwoNoteMiddle2Choreo", StartPosition.STARTING_LOCATION_MIDDLE, 2),
+        TWO_NOTE_MIDDLE_1("TwoNoteMiddle1Choreo", StartPosition.STARTING_LOCATION_MIDDLE, List.of(1)),
+        TWO_NOTE_MIDDLE_2("TwoNoteMiddle2Choreo", StartPosition.STARTING_LOCATION_MIDDLE, List.of(2)),
         //two note autos - Source Side
-        TWO_NOTE_RIGHT_6("TwoNoteSourceSide6Choreo", StartPosition.STARTING_LOCATION_SOURCE_SIDE, 2),
-        TWO_NOTE_RIGHT_7("TwoNoteSourceSide7Choreo", StartPosition.STARTING_LOCATION_SOURCE_SIDE, 2),
+        TWO_NOTE_RIGHT_6("TwoNoteSourceSide6Choreo", StartPosition.STARTING_LOCATION_SOURCE_SIDE, List.of(6)),
+        TWO_NOTE_RIGHT_7("TwoNoteSourceSide7Choreo", StartPosition.STARTING_LOCATION_SOURCE_SIDE, List.of(7)),
         //just shoot preload
-        PRELOAD_AND_SHOOT("OneNoteJustShoot", StartPosition.CURRENT_LOCATION, 1),
+        PRELOAD_ONLY("OneNoteJustShoot", StartPosition.CURRENT_LOCATION, List.of()),
         // Preload and drive
-        PRELOAD_AND_LEAVE_WING("OneNoteSourceSideAndLeaveWingChoreo", StartPosition.CURRENT_LOCATION, 1),
+        PRELOAD_AND_LEAVE_WING("OneNoteSourceSideAndLeaveWingChoreo", StartPosition.CURRENT_LOCATION, List.of()),
         // No shooting
-        LEAVE_WING("NoNoteLeaveWingChoreo", StartPosition.CURRENT_LOCATION, 0);
+        LEAVE_WING("NoNoteLeaveWingChoreo", StartPosition.CURRENT_LOCATION, List.of());
 
 
 
         public final String m_modeName;
         public final StartPosition m_location;
-        public final int m_numNotes;
+        public final List<Integer> m_notes;
 
-        AutoModes(String modeName, StartPosition location, int numNotes) {
+        AutoModes(String modeName, StartPosition location, List<Integer> notes) {
             m_modeName = modeName;
             m_location = location;
-            m_numNotes = numNotes;
+            m_notes = notes;
         }
     }
 
-    private static final AutoModes DEFAULT_MODE = AutoModes.PRELOAD_AND_SHOOT;
+    private static final AutoModes DEFAULT_MODE = AutoModes.PRELOAD_ONLY;
 
     private final SendableChooser<AutoModes> m_autonChooser;
 
--- y2024/Crescendo/src/main/java/com/gos/crescendo2024/led_patterns/subpatterns/AutoModePattern.java
+++ y2024/Crescendo/src/main/java/com/gos/crescendo2024/led_patterns/subpatterns/AutoModePattern.java
@@ -1,8 +1,6 @@
 package com.gos.crescendo2024.led_patterns.subpatterns;
 
 import com.gos.crescendo2024.auton.Autos;
-import com.gos.lib.led.LEDFlash;
-import com.gos.lib.led.LEDMovingPixel;
 import com.gos.lib.led.LEDPattern;
 import com.gos.lib.led.LEDPatternLookup;
 import com.gos.lib.led.LEDSolidColor;
@@ -13,11 +11,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AutoModePattern {
-    private final LEDPatternLookup<Autos.AutoModes> m_autoModePattern;
     private final LEDPatternLookup<Autos.StartPosition> m_startPositionPattern;
+    private final Map<Integer, LEDPattern> m_notesBeingAcquiredPattern;
 
     public AutoModePattern(AddressableLEDBuffer buffer, int startIndex, int numLeds) {
-        m_autoModePattern = new LEDPatternLookup<>(buffer, createAutonMap(buffer, startIndex, numLeds / 2));
+        m_notesBeingAcquiredPattern = createAquiredNotesMap(buffer, startIndex, numLeds / 2);
         m_startPositionPattern = new LEDPatternLookup<>(buffer, createStartPosMap(buffer, startIndex + numLeds / 2, numLeds / 2));
     }
 
@@ -30,28 +28,31 @@ public class AutoModePattern {
         return posMap;
     }
 
-    private Map<Autos.AutoModes, LEDPattern> createAutonMap(AddressableLEDBuffer buffer, int startIndex, int numLeds) {
-        Map<Autos.AutoModes, LEDPattern> autonMap = new HashMap<>();
-        autonMap.put(Autos.AutoModes.LEAVE_WING, new LEDSolidColor(buffer, startIndex, startIndex + numLeds, Color.kDenim));
-        autonMap.put(Autos.AutoModes.PRELOAD_AND_LEAVE_WING, new LEDFlash(buffer, startIndex, startIndex +  numLeds, 1.0, Color.kGreen));
-        autonMap.put(Autos.AutoModes.FOUR_NOTE_012, new LEDFlash(buffer, startIndex, startIndex +   numLeds, 1.0, Color.kRed));
-        autonMap.put(Autos.AutoModes.PRELOAD_AND_SHOOT, new LEDMovingPixel(buffer, startIndex, startIndex +  numLeds, Color.kWhite));
-        autonMap.put(Autos.AutoModes.TWO_NOTE_MIDDLE_1, new LEDFlash(buffer, startIndex, startIndex + numLeds, 2.0, Color.kBlanchedAlmond));
-        autonMap.put(Autos.AutoModes.TWO_NOTE_MIDDLE_2, new LEDFlash(buffer, startIndex, startIndex + numLeds, 2.0, Color.kPaleVioletRed));
-        autonMap.put(Autos.AutoModes.TWO_NOTE_RIGHT_6, new LEDFlash(buffer, startIndex, startIndex + numLeds, 2.0, Color.kBrown));
-        autonMap.put(Autos.AutoModes.TWO_NOTE_RIGHT_7, new LEDFlash(buffer, startIndex, startIndex + numLeds, 2.0, Color.kDarkGray));
+    private Map<Integer, LEDPattern> createAquiredNotesMap(AddressableLEDBuffer buffer, int startIndex, int numLeds) {
+        int ledsPerPosition = numLeds / 8;
+        Map<Integer, LEDPattern> autonMap = new HashMap<>();
+        autonMap.put(0, new LEDSolidColor(buffer, startIndex + 0 * ledsPerPosition, startIndex + 0 * ledsPerPosition + ledsPerPosition, Color.kViolet));
+        autonMap.put(1, new LEDSolidColor(buffer, startIndex + 1 * ledsPerPosition, startIndex + 1 * ledsPerPosition + ledsPerPosition, Color.kBlue));
+        autonMap.put(2, new LEDSolidColor(buffer, startIndex + 2 * ledsPerPosition, startIndex + 2 * ledsPerPosition + ledsPerPosition, Color.kGreen));
+        autonMap.put(3, new LEDSolidColor(buffer, startIndex + 3 * ledsPerPosition, startIndex + 3 * ledsPerPosition + ledsPerPosition, Color.kYellow));
+        autonMap.put(4, new LEDSolidColor(buffer, startIndex + 4 * ledsPerPosition, startIndex + 4 * ledsPerPosition + ledsPerPosition, Color.kOrange));
+        autonMap.put(5, new LEDSolidColor(buffer, startIndex + 5 * ledsPerPosition, startIndex + 5 * ledsPerPosition + ledsPerPosition, Color.kRed));
+        autonMap.put(6, new LEDSolidColor(buffer, startIndex + 6 * ledsPerPosition, startIndex + 6 * ledsPerPosition + ledsPerPosition, Color.kPink));
+        autonMap.put(7, new LEDSolidColor(buffer, startIndex + 7 * ledsPerPosition, startIndex + 7 * ledsPerPosition + ledsPerPosition, Color.kAliceBlue));
 
         return autonMap;
     }
 
     public void writeAutoModePattern(Autos.AutoModes autoMode) {
-        m_autoModePattern.setKey(autoMode);
         m_startPositionPattern.setKey(autoMode.m_location);
-        if (m_autoModePattern.hasKey(autoMode)) {
-            m_autoModePattern.writeLeds();
-        }
         if (m_startPositionPattern.hasKey(autoMode.m_location)) {
             m_startPositionPattern.writeLeds();
+        }
+
+        for (int notePosition : autoMode.m_notes) {
+            if (m_notesBeingAcquiredPattern.containsKey(notePosition)) {
+                m_notesBeingAcquiredPattern.get(notePosition).writeLeds();
+            }
         }
     }
 
