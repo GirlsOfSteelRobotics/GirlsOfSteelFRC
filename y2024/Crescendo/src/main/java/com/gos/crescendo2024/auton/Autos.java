a674d4796d6d92e0437a671b5de72c358a75d214
GosProgrammers <girlsofsteelrobotics@gmail.com>
2024-03-16 06:35:14 -0700

    Update autos

--- y2024/Crescendo/src/main/java/com/gos/crescendo2024/auton/Autos.java
+++ y2024/Crescendo/src/main/java/com/gos/crescendo2024/auton/Autos.java
@@ -31,9 +31,11 @@ public final class Autos {
         //four note auto
         FOUR_NOTE_012("FourNoteAmpSide012Choreo", StartPosition.STARTING_LOCATION_AMP_SIDE, List.of(0, 1, 2)),
         FOUR_NOTE_034("FourNoteAmpSide034Choreo", StartPosition.STARTING_LOCATION_AMP_SIDE, List.of(0, 3, 4)),
+        FOUR_NOTE_045("FourNoteAmpSide045Choreo", StartPosition.STARTING_LOCATION_AMP_SIDE, List.of(0, 4, 5)),
         // Three Note
         THREE_NOTE_12("ThreeNoteMiddle12Choreo", StartPosition.STARTING_LOCATION_MIDDLE, List.of(1, 2)),
         THREE_NOTE_76("ThreeNotesSourceSide76Choreo", StartPosition.STARTING_LOCATION_SOURCE_SIDE, List.of(7, 6)),
+        THREE_NOTE_65("ThreeNotesSourceSide65Choreo", StartPosition.STARTING_LOCATION_SOURCE_SIDE, List.of(6, 5)),
         //two note autos - Amp Side
         //two note autos - middle
         TWO_NOTE_MIDDLE_1("TwoNoteMiddle1Choreo", StartPosition.STARTING_LOCATION_MIDDLE, List.of(1)),
@@ -42,11 +44,11 @@ public final class Autos {
         TWO_NOTE_RIGHT_6("TwoNoteSourceSide6Choreo", StartPosition.STARTING_LOCATION_SOURCE_SIDE, List.of(6)),
         TWO_NOTE_RIGHT_7("TwoNoteSourceSide7Choreo", StartPosition.STARTING_LOCATION_SOURCE_SIDE, List.of(7)),
         //just shoot preload
-        PRELOAD_ONLY("OneNoteJustShoot", StartPosition.CURRENT_LOCATION, List.of()),
+        ONE_NOTE_JUST_SHOOT("OneNoteJustShoot", StartPosition.CURRENT_LOCATION, List.of()),
         // Preload and drive
-        PRELOAD_AND_LEAVE_WING("OneNoteSourceSideAndLeaveWingChoreo", StartPosition.CURRENT_LOCATION, List.of()),
+        ONE_NOTE_AND_LEAVE_WING("OneNoteSourceSideAndLeaveWingChoreo", StartPosition.CURRENT_LOCATION, List.of()),
         // No shooting
-        LEAVE_WING("NoNoteLeaveWingChoreo", StartPosition.CURRENT_LOCATION, List.of());
+        NO_NOTE_LEAVE_WING("NoNoteLeaveWingChoreo", StartPosition.CURRENT_LOCATION, List.of());
 
 
 
@@ -61,7 +63,7 @@ public final class Autos {
         }
     }
 
-    private static final AutoModes DEFAULT_MODE = AutoModes.PRELOAD_ONLY;
+    private static final AutoModes DEFAULT_MODE = AutoModes.ONE_NOTE_JUST_SHOOT;
 
     private final SendableChooser<AutoModes> m_autonChooser;
 
