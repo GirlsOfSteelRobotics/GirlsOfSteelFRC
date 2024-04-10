55b496e964876670fdb00baf10bf99390cbf3ace
egreenbaum15 <114195108+egreenbaum15@users.noreply.github.com>
2024-04-10 17:44:55 -0400

    Driver Practice (#538)
    * end of 4/8 testing for speaker lookup table with final intake.
    
    * Auto testing
    
    * Steal shooting polygon for drive testing
    
    * Lint
    
    * Drive practice
    
    * Lint
    
    ---------
    
    Co-authored-by: GosProgrammers <girlsofsteelrobotics@gmail.com>

--- y2024/Crescendo/src/main/java/com/gos/crescendo2024/commands/SpeakerAimAndShootCommand.java
+++ y2024/Crescendo/src/main/java/com/gos/crescendo2024/commands/SpeakerAimAndShootCommand.java
@@ -9,6 +9,8 @@ import com.gos.crescendo2024.subsystems.ShooterSubsystem;
 import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -18,17 +20,21 @@ import java.util.function.Supplier;
 
 public final class SpeakerAimAndShootCommand extends Command {
     private static final double DEFAULT_INTAKE_TIME = 1;
+    private static final double EMERGENCY_RELEASE_TIME = 2.5;
 
     private final ArmPivotSubsystem m_armPivotSubsystem;
     private final ChassisSubsystem m_chassisSubsystem;
     private final IntakeSubsystem m_intakeSubsystem;
     private final ShooterSubsystem m_shooterSubsystem;
     private final Timer m_intakeTimer;
+    private final Timer m_emergencyReleaseTimer;
 
     private final DoubleSupplier m_armAngleGoalSupplier;
     private final DoubleSupplier m_shooterRpmGoalSupplier;
     private final Supplier<Pose2d> m_robotPoseProvider;
     private boolean m_runIntake;
+
+    private final NetworkTableEntry m_didEmergencyTimerElapse;
 
     private final double m_intakeTime;
 
@@ -47,6 +53,7 @@ public final class SpeakerAimAndShootCommand extends Command {
         this.m_intakeSubsystem = intakeSubsystem;
         this.m_shooterSubsystem = shooterSubsystem;
         this.m_intakeTimer = new Timer();
+        this.m_emergencyReleaseTimer = new Timer();
 
         m_armAngleGoalSupplier = armAngleGoalSupplier;
         m_shooterRpmGoalSupplier = shooterRpmGoalSupplier;
@@ -54,6 +61,8 @@ public final class SpeakerAimAndShootCommand extends Command {
         m_debouncer = new Debouncer(.2);
 
         m_intakeTime = runIntakeTime;
+
+        m_didEmergencyTimerElapse = NetworkTableInstance.getDefault().getEntry("AutoShootDidEmergencyRelease");
 
         addRequirements(this.m_armPivotSubsystem, this.m_chassisSubsystem, this.m_intakeSubsystem, this.m_shooterSubsystem);
     }
@@ -139,7 +148,12 @@ public final class SpeakerAimAndShootCommand extends Command {
     public void initialize() {
         m_intakeTimer.reset();
         m_intakeTimer.stop();
+
+        m_emergencyReleaseTimer.reset();
+        m_emergencyReleaseTimer.start();
         m_runIntake = false;
+
+        m_didEmergencyTimerElapse.setBoolean(false);
     }
 
 
@@ -152,7 +166,10 @@ public final class SpeakerAimAndShootCommand extends Command {
         m_shooterSubsystem.setPidRpm(m_shooterRpmGoalSupplier.getAsDouble());
 
         boolean isReadyToShoot = m_armPivotSubsystem.isArmAtGoal() && m_chassisSubsystem.isAngleAtGoal() && m_shooterSubsystem.isShooterAtGoal();
-        if (m_debouncer.calculate(isReadyToShoot)) {
+        if (m_debouncer.calculate(isReadyToShoot) || m_emergencyReleaseTimer.hasElapsed(EMERGENCY_RELEASE_TIME)) {
+            if (m_emergencyReleaseTimer.hasElapsed(EMERGENCY_RELEASE_TIME)) {
+                m_didEmergencyTimerElapse.setBoolean(true);
+            }
             m_runIntake = true;
             m_intakeTimer.start();
         }
