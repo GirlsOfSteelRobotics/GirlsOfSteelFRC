306a436ab1f9b4ad833b971755f799c4992b156d
GosProgrammers <girlsofsteelrobotics@gmail.com>
2024-04-09 21:40:24 -0400

    Lint

--- y2024/Crescendo/src/main/java/com/gos/crescendo2024/SpeakerLookupTable.java
+++ y2024/Crescendo/src/main/java/com/gos/crescendo2024/SpeakerLookupTable.java
@@ -17,18 +17,20 @@ public class SpeakerLookupTable {
         //m_table.put(2.99, 37.0);
         //m_table.put(3.42, 40.0);
         //m_table.put(3.87, 41.0);
-        //m_table.put(5.62, 35.5);    // 3/28 statichooter testing
+        //m_table.put(5.62, 35.5);    // 3/28 static shooter testing
 
-//        m_table.put(1.2, 16.0);
-//        m_table.put(1.93, 20.0);
-//        m_table.put(2.37, 26.0);
-//        m_table.put(2.75, 29.0);
-//        m_table.put(3.25, 33.0);
-//        m_table.put(3.75, 35.0);
-//        m_table.put(4.22, 39.0);
-//        m_table.put(4.61, 40.00);
-//        m_table.put(5.53, 41.0);
+        // Wooden Intake
+        // m_table.put(1.2, 16.0);
+        // m_table.put(1.93, 20.0);
+        // m_table.put(2.37, 26.0);
+        // m_table.put(2.75, 29.0);
+        // m_table.put(3.25, 33.0);
+        // m_table.put(3.75, 35.0);
+        // m_table.put(4.22, 39.0);
+        // m_table.put(4.61, 40.00);
+        // m_table.put(5.53, 41.0);
 
+        // Final Shooter - 4/9
         m_table.put(1.26, 16.0);
         m_table.put(1.91, 24.0);
         m_table.put(2.5, 30.0);
@@ -37,9 +39,6 @@ public class SpeakerLookupTable {
         m_table.put(3.87, 39.0);
         m_table.put(4.6, 42.0);
         m_table.put(5.11, 41.7);
-
-
-
     }
 
     public double getAngleTable(double distance) {
