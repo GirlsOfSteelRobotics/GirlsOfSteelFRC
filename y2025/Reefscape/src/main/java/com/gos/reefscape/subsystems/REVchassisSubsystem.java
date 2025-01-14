a4567372762e907c71bc6d447e28268a4ef8c081
gosprogrammers <girlsofsteelrobotics@gmail.com>
2025-01-14 18:20:22 -0500

    tuned swerve and drive motors :) (#573)
    * started coding pose estimation on shuffleboard but does not work yet :(
    
    * started tuning drive and steer. drive is good. steer is interesting.
    
    * tuned swerve drive
    
    * linting

--- y2025/Reefscape/src/main/java/com/gos/reefscape/subsystems/REVchassisSubsystem.java
+++ y2025/Reefscape/src/main/java/com/gos/reefscape/subsystems/REVchassisSubsystem.java
@@ -3,12 +3,18 @@ package com.gos.reefscape.subsystems;
 
 import com.ctre.phoenix6.hardware.Pigeon2;
 import com.gos.reefscape.Constants;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import org.snobotv2.module_wrappers.phoenix6.Pigeon2Wrapper;
 import org.snobotv2.sim_wrappers.SwerveModuleSimWrapper;
@@ -19,28 +25,31 @@ import java.util.List;
 public class REVchassisSubsystem extends SubsystemBase {
     public static final double WHEEL_BASE = Units.inchesToMeters(25);
     public static final double TRACK_WIDTH = Units.inchesToMeters(25);
-    public static final double MAX_TRANSLATION_SPEED = Units.feetToMeters(20.1);
+    public static final double MAX_TRANSLATION_SPEED = 4; // Units.feetToMeters(20.1);
     public static final double MAX_ROTATION_SPEED = Units.degreesToRadians(540);
     private final RevSwerveModule m_backLeft;
     private final RevSwerveModule m_backRight;
     private final RevSwerveModule m_frontLeft;
     private final RevSwerveModule m_frontRight;
+    private final SwerveDriveOdometry m_odometry;
+
 
     private final Pigeon2 m_gyro;
 
     private final RevSwerveModule[] m_modules;
 
     private final SwerveDriveKinematics m_kinematics;
+    private final Field2d m_field;
 
 
     // Simulation
     private SwerveSimWrapper m_simulator;
 
     public REVchassisSubsystem() {
-        m_backLeft = new RevSwerveModule(Constants.BACK_LEFT_CANCODER_ID, Constants. BACK_LEFT_DRIVE_MOTOR_ID, Constants.BACK_LEFT_STEER_MOTOR_ID);
-        m_backRight = new RevSwerveModule(Constants.BACK_RIGHT_CANCODER_ID, Constants.BACK_RIGHT_DRIVE_MOTOR_ID, Constants. BACK_RIGHT_STEER_MOTOR_ID);
-        m_frontLeft = new RevSwerveModule(Constants. FRONT_LEFT_CANCODER_ID, Constants.FRONT_LEFT_DRIVE_MOTOR_ID, Constants.FRONT_LEFT_STEER_MOTOR_ID);
-        m_frontRight = new RevSwerveModule(Constants. FRONT_RIGHT_CANCODER_ID, Constants.FRONT_RIGHT_DRIVE_MOTOR_ID, Constants. FRONT_RIGHT_STEER_MOTOR_ID);
+        m_backLeft = new RevSwerveModule("BL", Constants.BACK_LEFT_CANCODER_ID, Constants. BACK_LEFT_DRIVE_MOTOR_ID, Constants.BACK_LEFT_STEER_MOTOR_ID);
+        m_backRight = new RevSwerveModule("BR", Constants.BACK_RIGHT_CANCODER_ID, Constants.BACK_RIGHT_DRIVE_MOTOR_ID, Constants. BACK_RIGHT_STEER_MOTOR_ID);
+        m_frontLeft = new RevSwerveModule("FL", Constants. FRONT_LEFT_CANCODER_ID, Constants.FRONT_LEFT_DRIVE_MOTOR_ID, Constants.FRONT_LEFT_STEER_MOTOR_ID);
+        m_frontRight = new RevSwerveModule("FR", Constants. FRONT_RIGHT_CANCODER_ID, Constants.FRONT_RIGHT_DRIVE_MOTOR_ID, Constants. FRONT_RIGHT_STEER_MOTOR_ID);
 
         m_gyro = new Pigeon2(Constants.PIGEON_ID);
 
@@ -53,6 +62,15 @@ public class REVchassisSubsystem extends SubsystemBase {
             new Translation2d(-WHEEL_BASE / 2, -TRACK_WIDTH / 2)
         );
 
+        m_odometry = new SwerveDriveOdometry(
+            m_kinematics, m_gyro.getRotation2d(),
+            new SwerveModulePosition[] {
+                m_frontLeft.getPosition(),
+                m_frontRight.getPosition(),
+                m_backLeft.getPosition(),
+                m_backRight.getPosition()
+            }, new Pose2d(0, 0, new Rotation2d()));
+
         if (RobotBase.isSimulation()) {
             List<SwerveModuleSimWrapper> moduleSims = List.of(
                 m_frontLeft.getSimWrapper(),
@@ -61,6 +79,8 @@ public class REVchassisSubsystem extends SubsystemBase {
                 m_backRight.getSimWrapper());
             m_simulator = new SwerveSimWrapper(WHEEL_BASE, TRACK_WIDTH, 64.0, 1.0, moduleSims, new Pigeon2Wrapper(m_gyro));
         }
+        m_field = new Field2d();
+        SmartDashboard.putData(m_field);
     }
 
     public void setChassisSpeed(ChassisSpeeds chassisSpeed) {
@@ -72,7 +92,7 @@ public class REVchassisSubsystem extends SubsystemBase {
 
     public void setModuleStates(SwerveModuleState... desiredStates) {
         for (int i = 0; i < 4; i++) {
-            m_modules[i].  drive(desiredStates[i].speedMetersPerSecond, desiredStates[i].angle.getDegrees());
+            m_modules[i].drive(desiredStates[i]);
 
         }
     }
@@ -80,6 +100,23 @@ public class REVchassisSubsystem extends SubsystemBase {
     public void swerveDrive(double xVelocity, double yVelocity, double rotationalVelocity) {
         ChassisSpeeds chassisSpeeds = new ChassisSpeeds(xVelocity, yVelocity, rotationalVelocity);
         setChassisSpeed(chassisSpeeds);
+    }
+
+    @Override
+    public void periodic() {
+        var gyroAngle = m_gyro.getRotation2d();
+        // Update the pose
+        m_odometry.update(gyroAngle,
+            new SwerveModulePosition[] {
+                m_frontLeft.getPosition(), m_frontRight.getPosition(),
+                m_backLeft.getPosition(), m_backRight.getPosition()
+            });
+        m_field.setRobotPose(m_odometry.getPoseMeters());
+
+        m_backLeft.periodic();
+        m_backRight.periodic();
+        m_frontRight.periodic();
+        m_frontLeft.periodic();
     }
 
     @Override
--- y2025/Reefscape/src/main/java/com/gos/reefscape/subsystems/RevSwerveModule.java
+++ y2025/Reefscape/src/main/java/com/gos/reefscape/subsystems/RevSwerveModule.java
@@ -1,22 +1,33 @@
 package com.gos.reefscape.subsystems;
 
 import com.ctre.phoenix6.hardware.CANcoder;
+import com.gos.lib.properties.pid.PidProperty;
+import com.gos.lib.rev.properties.pid.RevPidPropertyBuilder;
 import com.gos.lib.rev.swerve.config.RevSwerveModuleConstants;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.ClosedLoopSlot;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.simulation.swerve.SwerveModuleSim;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.snobotv2.module_wrappers.rev.RevEncoderSimWrapper;
 import org.snobotv2.module_wrappers.rev.RevMotorControllerSimWrapper;
 import org.snobotv2.sim_wrappers.SwerveModuleSimWrapper;
 
 public class RevSwerveModule {
-    private static final double GEAR_REDUCTION = 5.9;
+    private static final double GEAR_REDUCTION_DRIVE = 5.36;
+    private static final double GEAR_REDUCTION_STEER = 18.75;
 
+    private final String m_name;
 
     private final CANcoder m_absoluteEncoder;
     private final RelativeEncoder m_driveEncoder;
@@ -28,12 +39,49 @@ public class RevSwerveModule {
 
     private SwerveModuleSimWrapper m_simWrapper;
 
+    private final PidProperty m_drivePidProperties;
+    private final PidProperty m_steerPidProperties;
 
 
-    public RevSwerveModule(int absoluteEncoderID, int motorDriveID, int motorSteerID) {
+
+    public RevSwerveModule(String name, int absoluteEncoderID, int motorDriveID, int motorSteerID) {
+        m_name = name;
+
         m_absoluteEncoder = new CANcoder(absoluteEncoderID);
         m_motorDrive = new SparkFlex(motorDriveID, MotorType.kBrushless);
         m_motorSteer = new SparkFlex(motorSteerID, MotorType.kBrushless);
+
+        SparkMaxConfig driveConfig = new SparkMaxConfig();
+        driveConfig.idleMode(IdleMode.kBrake);
+        driveConfig.smartCurrentLimit(60);
+        driveConfig.inverted(false);
+
+        driveConfig.encoder.positionConversionFactor(1 / GEAR_REDUCTION_DRIVE);
+        driveConfig.encoder.velocityConversionFactor(1 / GEAR_REDUCTION_DRIVE / 60);
+
+
+
+
+        SparkMaxConfig steerConfig = new SparkMaxConfig();
+        steerConfig.idleMode(IdleMode.kBrake);
+        steerConfig.smartCurrentLimit(60);
+        steerConfig.inverted(false);
+
+        steerConfig.encoder.positionConversionFactor(1 / GEAR_REDUCTION_STEER);
+        steerConfig.encoder.velocityConversionFactor(1 / GEAR_REDUCTION_STEER / 60);
+
+        m_drivePidProperties = new RevPidPropertyBuilder("SwerveDrivePid", false, m_motorDrive, driveConfig, ClosedLoopSlot.kSlot0)
+            .addP(0)
+            .addFF(0)
+            .build();
+
+        m_steerPidProperties = new RevPidPropertyBuilder("SwerveSteerPid", false, m_motorSteer, steerConfig, ClosedLoopSlot.kSlot0)
+            .addP(0)
+            .addFF(0)
+            .build();
+
+
+
         m_driveEncoder = m_motorDrive.getEncoder();
         m_steerEncoder = m_motorSteer.getEncoder();
 
@@ -49,7 +97,7 @@ public class RevSwerveModule {
                 drivingMotor,
                 RevSwerveModuleConstants.WHEEL_DIAMETER_METERS / 2,
                 RevSwerveModuleConstants.TURNING_ENCODER_POSITION_FACTOR,
-                GEAR_REDUCTION,
+                GEAR_REDUCTION_DRIVE,
                 1.0,
                 1.8, // Seems fishy
                 1.1,
@@ -68,9 +116,16 @@ public class RevSwerveModule {
         }
     }
 
-    public void drive(double velocity, double angle) {
-        m_drivePID.setReference(velocity, ControlType.kVelocity);
-        m_steerPID.setReference(angle, ControlType.kPosition);
+    public void drive(SwerveModuleState state) {
+        state.optimize(Rotation2d.fromDegrees(getSteerAngle()));
+        m_drivePID.setReference(state.speedMetersPerSecond, ControlType.kVelocity);
+        m_steerPID.setReference(state.angle.getDegrees(), ControlType.kPosition);
+
+        SmartDashboard.putNumber(m_name + "Goal Velocity", state.speedMetersPerSecond);
+        SmartDashboard.putNumber(m_name + "Current Velocity", m_driveEncoder.getVelocity());
+
+        SmartDashboard.putNumber(m_name + "Goal Position", state.angle.getDegrees());
+        SmartDashboard.putNumber(m_name + "Current Position", m_steerEncoder.getPosition());
     }
 
     public double getAbsoluteEncoderPosition() {
@@ -88,5 +143,16 @@ public class RevSwerveModule {
 
     public SwerveModuleSimWrapper getSimWrapper() {
         return m_simWrapper;
+    }
+
+    public SwerveModulePosition getPosition() {
+        return new SwerveModulePosition(
+            m_driveEncoder.getPosition(),
+            Rotation2d.fromDegrees(m_steerEncoder.getPosition()));
+    }
+
+    public void periodic() {
+        m_steerPidProperties.updateIfChanged();
+        m_drivePidProperties.updateIfChanged();
     }
 }
