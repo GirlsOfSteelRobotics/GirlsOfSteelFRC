056639cb63e92d2447e33dfd4296c7366800fbac
GosProgrammers <girlsofsteelrobotics@gmail.com>
2025-01-23 19:01:13 -0500

    lint

--- y2025/Reefscape/src/main/java/com/gos/reefscape/RobotContainer.java
+++ y2025/Reefscape/src/main/java/com/gos/reefscape/RobotContainer.java
@@ -15,7 +15,6 @@ import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.simulation.DriverStationSim;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
