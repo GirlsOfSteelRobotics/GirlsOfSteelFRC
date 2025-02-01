58efc252aca3bd166a88e140a95f9f0c5d7712a5
gosprogrammers <girlsofsteelrobotics@gmail.com>
2025-02-01 10:17:52 -0500

    Cleanup paths (#616)
    * Cleanup path names
    
    * Fixup names
    
    * More renaming
    
    * Update script
    
    * Lint
    
    * Fixup and lint
    
    * Whoops

--- y2025/Reefscape/src/main/java/com/gos/reefscape/ChoreoPoses.java
+++ y2025/Reefscape/src/main/java/com/gos/reefscape/ChoreoPoses.java
@@ -5,17 +5,17 @@ import edu.wpi.first.math.geometry.Rotation2d;
 
 public class ChoreoPoses {
     public static final Pose2d C = new Pose2d(3.606261730194092, 3.079380989074707, Rotation2d.fromRadians(-2.1662063752267593));
-    public static final Pose2d CENTER_STARTING_POSITION = new Pose2d(7.832268238067627, 4.001668930053711, Rotation2d.fromRadians(0.0));
     public static final Pose2d E = new Pose2d(4.955280303955078, 2.8178365230560303, Rotation2d.fromRadians(-1.0471975511965976));
-    public static final Pose2d EF_ALGAE = new Pose2d(5.147095680236816, 2.9247190952301025, Rotation2d.fromRadians(-1.0516503687020464));
-    public static final Pose2d GH_ALGAE = new Pose2d(5.739912986755371, 3.987903594970703, Rotation2d.fromRadians(0.0));
+    public static final Pose2d EF = new Pose2d(5.147095680236816, 2.9247190952301025, Rotation2d.fromRadians(-1.0516503687020464));
+    public static final Pose2d GH = new Pose2d(5.739912986755371, 3.987903594970703, Rotation2d.fromRadians(0.0));
     public static final Pose2d H = new Pose2d(5.739912986755371, 4.194386005401611, Rotation2d.fromRadians(0.0));
+    public static final Pose2d HUMAN_PLAYER_LEFT = new Pose2d(1.3718515634536743, 7.273766994476318, Rotation2d.fromRadians(2.279422598922567));
+    public static final Pose2d HUMAN_PLAYER_RIGHT = new Pose2d(1.5276724100112915, 0.642888605594635, Rotation2d.fromRadians(-2.248504688139378));
     public static final Pose2d J = new Pose2d(4.943519115447998, 5.226694107055664, Rotation2d.fromRadians(1.0370881817864992));
     public static final Pose2d L = new Pose2d(3.618795394897461, 4.982372760772705, Rotation2d.fromRadians(2.095591826496163));
-    public static final Pose2d LEFT_HUMAN_PLAYER_STATION = new Pose2d(1.3718515634536743, 7.273766994476318, Rotation2d.fromRadians(2.279422598922567));
-    public static final Pose2d LEFT_STARTING_POSITION = new Pose2d(7.84056282043457, 6.1349568367004395, Rotation2d.fromRadians(0.0));
     public static final Pose2d PROCESSOR = new Pose2d(6.35289192199707, 0.45048725605010986, Rotation2d.fromRadians(1.5707963267948966));
-    public static final Pose2d RIGHT_HUMAN_PLAYER_STATION = new Pose2d(1.5276724100112915, 0.642888605594635, Rotation2d.fromRadians(-2.248504688139378));
-    public static final Pose2d RIGHT_STARTING_POSITION = new Pose2d(7.901095867156982, 1.8680174350738523, Rotation2d.fromRadians(0.0));
+    public static final Pose2d STARTING_POS_CENTER = new Pose2d(7.832268238067627, 4.001668930053711, Rotation2d.fromRadians(0.0));
+    public static final Pose2d STARTING_POS_RIGHT = new Pose2d(7.901095867156982, 1.8680174350738523, Rotation2d.fromRadians(0.0));
+    public static final Pose2d STARTING_POS_LEFT = new Pose2d(7.84056282043457, 6.1349568367004395, Rotation2d.fromRadians(0.0));
 
 }
