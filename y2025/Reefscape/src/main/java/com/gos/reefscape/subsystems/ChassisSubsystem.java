9eac493d6cf91d6b4f56010cf634a180c8c0022e
GosProgrammers <girlsofsteelrobotics@gmail.com>
2025-02-18 20:38:31 -0500

    merge stuff

--- y2025/Reefscape/src/main/java/com/gos/reefscape/subsystems/ChassisSubsystem.java
+++ y2025/Reefscape/src/main/java/com/gos/reefscape/subsystems/ChassisSubsystem.java
@@ -2,11 +2,9 @@ package com.gos.reefscape.subsystems;
 
 import static com.gos.lib.pathing.PathPlannerUtils.followChoreoPath;
 import static edu.wpi.first.units.Units.*;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
-
 import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.configs.SlotConfigs;
 import com.ctre.phoenix6.hardware.CANcoder;
@@ -16,7 +14,6 @@ import com.ctre.phoenix6.swerve.SwerveModule;
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveModuleConstants;
 import com.ctre.phoenix6.swerve.SwerveRequest;
-
 import com.gos.lib.pathing.TunablePathConstraints;
 import com.gos.lib.phoenix6.alerts.BasePhoenix6Alerts;
 import com.gos.lib.phoenix6.alerts.PigeonAlerts;
@@ -37,9 +34,7 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.config.PIDConstants;
 import com.pathplanner.lib.config.RobotConfig;
 import com.pathplanner.lib.controllers.PPHolonomicDriveController;
-
 import edu.wpi.first.math.Matrix;
-import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.numbers.N1;
@@ -52,9 +47,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.Subsystem;
-
 import org.littletonrobotics.frc2025.FieldConstants;
-import org.photonvision.EstimatedRobotPose;
 
 
 /**
