a64c6c84ea32127987c743c3852860ad73221051
GosProgrammers <girlsofsteelrobotics@gmail.com>
2025-03-03 21:31:05 -0500

    lint

--- y2025/Reefscape/src/main/java/com/gos/reefscape/subsystems/ElevatorSubsystem.java
+++ y2025/Reefscape/src/main/java/com/gos/reefscape/subsystems/ElevatorSubsystem.java
@@ -24,7 +24,6 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.simulation.DIOSim;
 import edu.wpi.first.wpilibj.simulation.ElevatorSim;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.RelativeEncoder;
 import org.snobotv2.module_wrappers.BaseDigitalInputWrapper;
