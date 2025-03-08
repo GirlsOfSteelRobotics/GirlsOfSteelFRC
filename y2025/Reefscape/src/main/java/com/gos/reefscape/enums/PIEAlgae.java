af30194f68424db7b6de041c9cde7ddc3d5ebe35
GosProgrammers <girlsofsteelrobotics@gmail.com>
2025-03-08 17:44:50 -0500

    Lint

--- y2025/Reefscape/src/main/java/com/gos/reefscape/enums/PIEAlgae.java
+++ y2025/Reefscape/src/main/java/com/gos/reefscape/enums/PIEAlgae.java
@@ -4,8 +4,7 @@ public enum PIEAlgae {
     FETCH_ALGAE_2(0.45, -167),
     FETCH_ALGAE_3(0.68, -167),
     SCORE_INTO_NET(1.5, -100),
-    SCORE_INTO_PROCESSOR(0, -203
-    );
+    SCORE_INTO_PROCESSOR(0, -203);
 
     public PIESetpoint m_setpoint;
 
