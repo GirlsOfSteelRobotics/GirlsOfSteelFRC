a192fb50233ce45072e546861b5bee24aeada11b
PJ Reiniger <pj.reiniger@gmail.com>
2026-01-06 19:01:04 -0500

    PJ Codelab

---
+++ codelabs/gitflow/src/main/java/com/gos/codelabs/gitflow/subsystems/PrimCodelab2027.java
@@ -0,0 +1,10 @@
+package com.gos.codelabs.gitflow.subsystems;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class PrimCodelab2027 extends SubsystemBase {
+    public PrimCodelab2027() {
+        System.out.println("PJ is cheating a merge conflict");
+    }
+}
