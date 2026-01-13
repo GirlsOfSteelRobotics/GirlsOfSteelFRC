7d9b51803a4156b9cdc673f9e2aa772ed6c92c65
gosprogrammers <girlsofsteelrobotics@gmail.com>
2026-01-12 19:03:45 -0500

    Add 2026 project (#673)
    * Add 2026 project
    
    * lint

---
+++ y2026/Rebuilt/src/test/java/com/gos/rebuilt/RobotContainerTest.java
@@ -0,0 +1,10 @@
+package com.gos.rebuilt;
+
+import org.junit.jupiter.api.Test;
+
+public class RobotContainerTest {
+    @Test
+    public void testStartup() {
+        new RobotContainer();
+    }
+}
