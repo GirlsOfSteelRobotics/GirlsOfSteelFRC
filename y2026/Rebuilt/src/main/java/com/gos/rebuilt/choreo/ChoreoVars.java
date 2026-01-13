a79b4ae577569b6904aafb8898defedc82e2d35a
GosProgrammers <girlsofsteelrobotics@gmail.com>
2026-01-13 17:22:28 -0500

    Add default choreo file

---
+++ y2026/Rebuilt/src/main/java/com/gos/rebuilt/choreo/ChoreoVars.java
@@ -0,0 +1,16 @@
+package com.gos.rebuilt.choreo;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.*;
+
+/**
+ * Generated file containing variables defined in Choreo.
+ * DO NOT MODIFY THIS FILE YOURSELF; instead, change these values
+ * in the Choreo GUI.
+ */
+public final class ChoreoVars {
+
+    private ChoreoVars() {}
+}
