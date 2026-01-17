1cb96ed720d54eda75d26f1e4bd203359a6c583d
Jinjin <xiangjin@andrew.cmu.edu>
2026-01-17 13:00:32 -0500

    fixed pizza sim

--- y2026/Rebuilt/src/main/java/com/gos/rebuilt/subsystems/PizzaSubsystem.java
+++ y2026/Rebuilt/src/main/java/com/gos/rebuilt/subsystems/PizzaSubsystem.java
@@ -6,10 +6,20 @@ import com.gos.rebuilt.Constants;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.system.LinearSystem;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.snobotv2.module_wrappers.rev.RevEncoderSimWrapper;
+import org.snobotv2.module_wrappers.rev.RevMotorControllerSimWrapper;
+import org.snobotv2.sim_wrappers.FlywheelSimWrapper;
+import org.snobotv2.sim_wrappers.ISimWrapper;
 
 public class PizzaSubsystem extends SubsystemBase {
 
@@ -17,10 +27,22 @@ public class PizzaSubsystem extends SubsystemBase {
     private final GosDoubleProperty m_pizzaSpeed = new GosDoubleProperty(Constants.DEFAULT_CONSTANT_PROPERTIES, "pizzaSpeed", 1);
     private final RelativeEncoder m_pizzaEncoder;
 
+    private ISimWrapper m_pizzaSimulator;
+
     public PizzaSubsystem() {
 
         m_pizzaMotor = new SparkFlex(Constants.PIZZA_MOTOR, MotorType.kBrushless);
         m_pizzaEncoder = m_pizzaMotor.getEncoder();
+        if (RobotBase.isSimulation()) {
+            DCMotor gearbox = DCMotor.getNeo550(2);
+            LinearSystem<N1, N1, N1> plant =
+                LinearSystemId.createFlywheelSystem(gearbox, 0.01, 1.0);
+            FlywheelSim shooterFlywheelSim = new FlywheelSim(plant, gearbox);
+            this.m_pizzaSimulator = new FlywheelSimWrapper(
+                shooterFlywheelSim,
+                new RevMotorControllerSimWrapper(this.m_pizzaMotor, gearbox),
+                RevEncoderSimWrapper.create(this.m_pizzaMotor));
+        }
     }
 
     public double getRPM() {
@@ -42,6 +64,8 @@ public class PizzaSubsystem extends SubsystemBase {
         m_pizzaMotor.stopMotor();
     }
 
+
+
     public void addPizzaDebugCommands() {
         ShuffleboardTab tab = Shuffleboard.getTab("Pizza");
         tab.add(createPizzaFeedCommand());
@@ -55,5 +79,11 @@ public class PizzaSubsystem extends SubsystemBase {
     public Command createPizzaReverseCommand() {
         return runEnd(this::reverse, this::stop).withName("Reverse the pizza 😱");
     }
+
+    @Override
+    public void simulationPeriodic() {
+        m_pizzaSimulator.update();
+    }
+
 }
 
