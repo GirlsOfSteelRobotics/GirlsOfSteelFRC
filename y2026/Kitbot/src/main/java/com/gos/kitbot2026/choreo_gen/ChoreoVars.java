f2e73d4b362e51c19b008db562f1796a1e0e272f
PJ Reiniger <pj.reiniger@gmail.com>
2026-01-17 10:20:48 -0500

    Add project for kitbot (#689)

---
+++ y2026/Kitbot/src/main/java/com/gos/kitbot2026/choreo_gen/ChoreoVars.java
@@ -0,0 +1,11 @@
+package com.gos.kitbot2026.choreo_gen;
+
+/**
+ * Generated file containing variables defined in Choreo.
+ * DO NOT MODIFY THIS FILE YOURSELF; instead, change these values
+ * in the Choreo GUI.
+ */
+public final class ChoreoVars {
+
+    private ChoreoVars() {}
+}
