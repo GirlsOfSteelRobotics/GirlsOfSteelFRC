c6d39e65cb950c4fd0995042ea30efc60133f39c
GosProgrammers <girlsofsteelrobotics@gmail.com>
2026-02-19 19:10:22 -0500

    Lint

--- libraries/GirlsOfSteelLibPathing/src/main/java/com/gos/lib/pathing/MaybeFlippedTranslation3d.java
+++ libraries/GirlsOfSteelLibPathing/src/main/java/com/gos/lib/pathing/MaybeFlippedTranslation3d.java
@@ -10,12 +10,12 @@ public class MaybeFlippedTranslation3d {
     private final Translation3d m_redTranslation;
 
     public MaybeFlippedTranslation3d(double x, double y, double z) {
-        m_blueTranslation = new Translation3d(x,y,z);
+        m_blueTranslation = new Translation3d(x, y, z);
         m_redTranslation = ChoreoAllianceFlipUtil.flip(m_blueTranslation);
     }
 
     public Translation3d getTranslation() {
-        if(GetAllianceUtil.isBlueAlliance()) {
+        if (GetAllianceUtil.isBlueAlliance()) {
             return m_blueTranslation;
         } else {
             return m_redTranslation;
