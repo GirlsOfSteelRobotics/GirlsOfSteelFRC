dd7707237f0378cb37dd2e659b0021e23ee5c190
GosProgrammers <girlsofsteelrobotics@gmail.com>
2026-03-30 20:52:29 -0400

    Cleanup autos

--- y2026/Rebuilt/src/main/java/com/gos/rebuilt/autos/AutoFactory.java
+++ y2026/Rebuilt/src/main/java/com/gos/rebuilt/autos/AutoFactory.java
@@ -10,24 +10,26 @@ public class AutoFactory {
 
     public AutoFactory(ChassisSubsystem chassisSubsystem, CombinedCommand combinedCommand) {
         m_chooser = new SendableChooser<>();
-        m_chooser.addOption("Start Left Depot", new StartLeftDepot(chassisSubsystem, combinedCommand));
-        m_chooser.addOption("Start Left Preload", new StartLeftPreload(chassisSubsystem, combinedCommand));
-        m_chooser.addOption("Start Middle Shoot Middle", new StartMiddleShootMiddle(chassisSubsystem, combinedCommand));
-        m_chooser.addOption("Start Right Preload", new StartRightPreload(chassisSubsystem, combinedCommand));
-        m_chooser.addOption("Start Right Outpost", new StartRightOutpost(chassisSubsystem, combinedCommand));
-        m_chooser.addOption("Start Right Preload Outpost", new StartRightPreloadOutpost(chassisSubsystem, combinedCommand));
-        m_chooser.addOption("Right -> Trench -> Center", new RightTrenchCenter(chassisSubsystem, combinedCommand));
-        m_chooser.addOption("Left -> Trench -> Center", new LeftTrenchCenter(chassisSubsystem, combinedCommand));
-        m_chooser.addOption("Left -> Trench -> Center -> Depot", new LeftTrenchDepot(chassisSubsystem, combinedCommand));
-        m_chooser.addOption("Right -> Trench -> Center -> Depot", new RightTrenchDepot(chassisSubsystem, combinedCommand));
-        m_chooser.addOption("Middle -> Depot -> Outpost", new MiddleDepotOutpostShoot(chassisSubsystem, combinedCommand));
-        m_chooser.addOption("Start Right Preload Center", new RightPreloadTrenchCenter(chassisSubsystem, combinedCommand));
-        m_chooser.addOption("Start Left Preload Center", new LeftPreloadTrenchCenter(chassisSubsystem, combinedCommand));
+        m_chooser.addOption("Left -> Preload", new StartLeftPreload(chassisSubsystem, combinedCommand));
+        m_chooser.addOption("Left -> Trench -> Collect", new LeftTrenchCenter(chassisSubsystem, combinedCommand));
+        m_chooser.addOption("Left -> Trench -> Collect -> Bump -> Shoot", new LeftTrenchCenterBump(chassisSubsystem, combinedCommand));
+        m_chooser.addOption("Left  -> Preload -> Trench", new LeftPreloadTrenchCenter(chassisSubsystem, combinedCommand));
 
-        m_chooser.addOption("Left -> Depot -> Shoot", new ShootLeftDepot(chassisSubsystem, combinedCommand));
-        m_chooser.addOption("Middle -> Shoot -> Depot -> Shoot", new MiddleShootDepot(chassisSubsystem, combinedCommand));
-        m_chooser.addOption("Left -> Trench -> Center -> Bump -> Shoot", new LeftTrenchCenterBump(chassisSubsystem, combinedCommand));
+        m_chooser.addOption("Middle -> Preload", new StartMiddleShootMiddle(chassisSubsystem, combinedCommand));
+
+        m_chooser.addOption("Right -> Preload", new StartRightPreload(chassisSubsystem, combinedCommand));
+        m_chooser.addOption("Right -> Trench -> Collect -> Trench", new RightTrenchCenter(chassisSubsystem, combinedCommand));
+        m_chooser.addOption("Right -> Preload -> Trench", new RightPreloadTrenchCenter(chassisSubsystem, combinedCommand));
         m_chooser.addOption("Right -> Trench -> Center -> Bump -> Shoot", new RightTrenchCenterBump(chassisSubsystem, combinedCommand));
+
+        //m_chooser.addOption("Start Right Outpost", new StartRightOutpost(chassisSubsystem, combinedCommand));
+        //m_chooser.addOption("Start Right Preload Outpost", new StartRightPreloadOutpost(chassisSubsystem, combinedCommand));
+        // m_chooser.addOption("Start Left Depot", new StartLeftDepot(chassisSubsystem, combinedCommand));
+        // m_chooser.addOption("Left -> Depot -> Shoot", new ShootLeftDepot(chassisSubsystem, combinedCommand));
+        //m_chooser.addOption("Middle -> Shoot -> Depot -> Shoot", new MiddleShootDepot(chassisSubsystem, combinedCommand));
+        //  m_chooser.addOption("Left -> Trench -> Center -> Depot", new LeftTrenchDepot(chassisSubsystem, combinedCommand));
+        // m_chooser.addOption("Right -> Trench -> Center -> Depot", new RightTrenchDepot(chassisSubsystem, combinedCommand));
+        //   m_chooser.addOption("Middle -> Depot -> Outpost", new MiddleDepotOutpostShoot(chassisSubsystem, combinedCommand));
         SmartDashboard.putData("Autos", m_chooser);
 
     }
