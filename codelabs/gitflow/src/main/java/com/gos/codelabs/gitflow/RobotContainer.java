bce9c8e175c2d51c4e00b04ad82f57fa9b87ee1b
Casey <puppiescasey@gmail.com>
2026-09-17 19:50:55 -0400

    bianca stuff

--- codelabs/gitflow/src/main/java/com/gos/codelabs/gitflow/RobotContainer.java
+++ codelabs/gitflow/src/main/java/com/gos/codelabs/gitflow/RobotContainer.java
@@ -18,6 +18,7 @@ import com.gos.codelabs.gitflow.subsystems.PJCodelab2020Part2Subsystem;
 import com.gos.codelabs.gitflow.subsystems.PJCodelab2021Part1Subsystem;
 import com.gos.codelabs.gitflow.subsystems.PrimCodelab2027;
 import com.gos.codelabs.gitflow.subsystems.PrimCodelab2026;
+import com.gos.codelabs.gitflow.subsystems.bianca;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -55,6 +56,7 @@ public class RobotContainer {
 
     private KatherineCodeLabe2022Part1Subsystem m_katherineCodeLabe2022Part1;
     private AnuvaCodelab2022Part1Subsystem m_anuvaCodelab2022Part1;
+    private bianca m_bianca;
 
     ///////////////////////////////////////
     // Don't touch things below here
---
+++ codelabs/gitflow/src/main/java/com/gos/codelabs/gitflow/subsystems/bianca.java
@@ -0,0 +1,7 @@
+package com.gos.codelabs.gitflow.subsystems;
+
+public class bianca {
+    public bianca(){
+        System.out.println("bianca says hello world in 2026");
+    }
+}
