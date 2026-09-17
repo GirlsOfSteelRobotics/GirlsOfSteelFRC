d77730ebb694718ae45b079badb10e9f6e0cea94
FruitLongSalad <miazhao2006@gmail.com>
2026-09-17 19:10:45 -0400

    mia and gitanjali codelabs

--- codelabs/gitflow/src/main/java/com/gos/codelabs/gitflow/RobotContainer.java
+++ codelabs/gitflow/src/main/java/com/gos/codelabs/gitflow/RobotContainer.java
@@ -18,6 +18,7 @@ import com.gos.codelabs.gitflow.subsystems.PJCodelab2020Part2Subsystem;
 import com.gos.codelabs.gitflow.subsystems.PJCodelab2021Part1Subsystem;
 import com.gos.codelabs.gitflow.subsystems.PrimCodelab2027;
 import com.gos.codelabs.gitflow.subsystems.PrimCodelab2026;
+import com.gos.codelabs.gitflow.subsystems.mia_gitanjali;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -50,7 +51,7 @@ public class RobotContainer {
     private EllieSubsystem m_ellieSubsystem;
     private PrimCodelab2027 m_prim;
     private PrimCodelab2026 m_primCodelab2026;
-
+    private mia_gitanjali m_mia_Gitanjali;
     private AshleyCodelab2022Part1Subsystem m_ashleyCodelab2022Part1;
 
     private KatherineCodeLabe2022Part1Subsystem m_katherineCodeLabe2022Part1;
---
+++ codelabs/gitflow/src/main/java/com/gos/codelabs/gitflow/subsystems/mia_gitanjali.java
@@ -0,0 +1,8 @@
+package com.gos.codelabs.gitflow.subsystems;
+
+public class mia_gitanjali {
+
+    public mia_gitanjali() {
+        System.out.println("hello world in 2026 part one!");
+    }
+}
